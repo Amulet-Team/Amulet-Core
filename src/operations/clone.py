8efff2fe14b83fab4bbbc81ff2f699d6939f76be
Naor Volkovich <naor2013@icloud.com>
2018-09-01 18:01:23 +0300

    Updated the clone operation to work with the latest changes

--- src/operations/clone.py
+++ src/operations/clone.py
@@ -10,8 +10,8 @@ class Clone(Operation):
     def run_operation(self, world):
         if self.source_box.shape != self.target_box.shape:
             raise Exception("The shape of the selections needs to be the same")
-        source_generator = world.get_selections_from_slices(*self.source_box.to_slice())
-        target_generator = world.get_selections_from_slices(*self.target_box.to_slice())
+        source_generator = world.get_blocks(*self.source_box.to_slice())
+        target_generator = world.get_blocks(*self.target_box.to_slice())
         for target_selection in target_generator:
             source_selection = next(source_generator)
             target_selection.blocks = source_selection.blocks
