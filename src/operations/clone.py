491b7e9398f1a4c36b9f14aa54b3ed42d4a1baf7
Naor Volkovich <naor2013@icloud.com>
2018-09-03 23:57:30 +0300

    Minor changes to the Clone operation

--- src/operations/clone.py
+++ src/operations/clone.py
@@ -1,20 +1,5 @@
-from api.box import SubBox, SelectionBox
+from api.box import SelectionBox
 from api.operation import Operation
-
-
-class Clone2(Operation):
-    def __init__(self, source_box: SubBox, target_box: SubBox):
-        self.source_box = source_box
-        self.target_box = target_box
-
-    def run_operation(self, world):
-        if self.source_box.shape != self.target_box.shape:
-            raise Exception("The shape of the selections needs to be the same")
-        source_generator = world.get_sub_chunks(*self.source_box.to_slice())
-        target_generator = world.get_sub_chunks(*self.target_box.to_slice())
-        for target_selection in target_generator:
-            source_selection = next(source_generator)
-            target_selection.blocks = source_selection.blocks
 
 
 class Clone(Operation):
@@ -25,14 +10,15 @@ class Clone(Operation):
 
     def run_operation(self, world):
         if len(self.source_box) != len(self.target_box):
-            raise Exception("Source Box and Target Box must have the same amount of subboexes")
+            raise Exception("Source Box and Target Box must have the same amount of subboxes")
 
         for source, target in zip(self.source_box.subboxes(), self.target_box.subboxes()):
             if source.shape != target.shape:
                 raise Exception("The shape of the selections needs to be the same")
+
+        for source, target in zip(self.source_box.subboxes(), self.target_box.subboxes()):
             source_generator = world.get_sub_chunks(*source.to_slice())
             target_generator = world.get_sub_chunks(*target.to_slice())
-            for target_selection in target_generator:
-                source_selection = next(source_generator)
+            for source_selection, target_selection in zip(source_generator, target_generator):
                 target_selection.blocks = source_selection.blocks
 
