6332bbe4a5e9f92f036dbc3b8e1b975da2d336f0
Podshot <ben.gothard3@gmail.com>
2018-09-30 15:39:11 -0400

    Trying to fix some issues with the ReadTheDocs site #2

--- docs_source/conf.py
+++ docs_source/conf.py
@@ -15,8 +15,11 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("."))
-sys.path.insert(0, os.path.abspath(os.path.join("..", "src")))
+on_rtd = os.environ.get("READTHEDOCS") == 'True'
+
+if not on_rtd:
+    sys.path.insert(0, os.path.abspath("."))
+    sys.path.insert(0, os.path.abspath(os.path.join("..", "src")))
 
 
 # -- Project information -----------------------------------------------------
