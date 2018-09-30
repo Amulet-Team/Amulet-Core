5c48a56bcd771461d206288bc6abc7ced88a6fe8
Adrian Freund <git@freundtech.com>
2018-10-01 00:14:21 +0200

    Fixed order of arguments on assertEquals
    This makes the order consistant across the code and brings it in line
    with the error messages, which list the first parameter as expected and
    second as actual.

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -12,12 +12,12 @@ class DefinitionBasedLoaderTestCase(unittest.TestCase):
 
     def test_identifing(self):
         name, _format = self.loader.identify(get_world_path("1.12.2 World"))
-        self.assertEqual(name, "1_12")
-        self.assertEqual(_format, "anvil")
+        self.assertEqual("1_12", name)
+        self.assertEqual("anvil", _format)
 
         name, _format = self.loader.identify(get_world_path("1.13 World"))
-        self.assertEqual(name, "1_13")
-        self.assertEqual(_format, "anvil2")
+        self.assertEqual("1_13", name)
+        self.assertEqual("anvil2", _format)
 
     def test_loading(self):
         world_obj = self.loader.load_world(get_world_path("1.12.2 World"))
