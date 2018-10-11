2659ec765b7bc83cf161fd0f69ea7dce20a54b80
Podshot <ben.gothard3@gmail.com>
2018-10-10 22:07:21 -0400

    BlockManager now supports internal ID <-> Block object

--- src/api/blocks.py
+++ src/api/blocks.py
@@ -3,6 +3,9 @@ from __future__ import annotations
 import re
 from typing import Any, Dict, Union
 
+import numpy
+import time
+
 
 class MalformedBlockstateException(Exception):
     pass
@@ -17,18 +20,18 @@ class Block:
 
     def __init__(
         self,
-        resource_location: str,
+        namespace: str,
         base_name: str,
         properties: Dict[str, Union[str, bool, int]],
     ):
-        self._resource_location = resource_location
+        self._namespace = namespace
         self._base_name = base_name
         self._properties = properties
         self._blockstate = self._gen_blockstate()
 
     @property
-    def resource_location(self) -> str:
-        return self._resource_location
+    def namespace(self) -> str:
+        return self._namespace
 
     @property
     def base_name(self) -> str:
@@ -43,7 +46,7 @@ class Block:
         return self._blockstate
 
     def _gen_blockstate(self) -> str:
-        blockstate = f"{self._resource_location}:{self._base_name}"
+        blockstate = f"{self._namespace}:{self._base_name}"
         if self._properties:
             props = [f"{key}={value}" for key, value in self._properties.items()]
             blockstate += f"[{','.join(props)}]"
@@ -54,8 +57,12 @@ class Block:
         match = Block.blockstate_regex.match(blockstate)
         namespace = match.group("namespace") or "minecraft"
         base_name = match.group("base_name")
+
         if match.group("property_name") is not None:
             properties = {match.group("property_name"): match.group("property_value")}
+        else:
+            properties = {}
+
         properties_string = match.group("properties")
         if properties_string is not None:
             properties_match = Block.parameters_regex.finditer(properties_string)
@@ -81,32 +88,44 @@ class Block:
 
 class BlockManager:
     def __init__(self):
-        self._name_to_block_map: Dict[str, Block] = {}
+        self._index_to_block = numpy.array([], dtype=Block)
         self._block_to_index_map: Dict[Block, int] = {}
 
-    def __getitem__(self, item: Union[Block, str]) -> int:
+    def __getitem__(self, item: Union[Block, str, int]) -> int:
         if isinstance(item, str):
             item = self.get_block(item)
+
+        if isinstance(item, int):
+            return self._index_to_block[item]
+
         return self._block_to_index_map[item]
 
     def get_block(self, block: str) -> Block:
-        if block in self._name_to_block_map:
-            return self._name_to_block_map[block]
+        if block in self._block_to_index_map:
+            return self._index_to_block[self._block_to_index_map[block]]
 
-        self._name_to_block_map[block] = b = Block.get_from_blockstate(block)
+        b = Block.get_from_blockstate(block)
         self._block_to_index_map[b] = len(self._block_to_index_map)
+        self._index_to_block = numpy.append(self._index_to_block, [b])
+
         return b
 
 
 if __name__ == "__main__":
-    b = Block.get_from_blockstate("minecraft:air")
-    d = {b: 0}
-    print(b in d)
-    print("minecraft:air" in d)
+    b = Block.get_from_blockstate("minecraft:unknown")
 
     manager = BlockManager()
+    start1 = time.time()
     i1 = manager["minecraft:air"]
+    end1 = time.time()
     i2 = manager["minecraft:stone"]
+
+    start2 = time.time()
     i3 = manager["minecraft:air"]
+    end2 = time.time()
+    print(i1, i2, i3)
+    print(f"Creation: {end1 - start1}")
+    print(f"__getitem__: {end2 - start2}")
+
+    block = manager[4]
     i4 = manager[b]
-    print(i1, i2, i3, i4)
