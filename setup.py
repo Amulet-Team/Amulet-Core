0560c0e335b2f781b011c85cd7fd6a9672dac5ab
Podshot <ben.gothard3@gmail.com>
2019-07-05 12:16:42 -0400

    Exclude command_line package for now, get version data from version.py, and require the necessary 3rd-party modules

--- setup.py
+++ setup.py
@@ -3,23 +3,30 @@ import os
 
 from setuptools import setup, find_packages
 
-# import version
-
 import numpy
 
 include_dirs = [numpy.get_include()]
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
 
-packs = find_packages("src", include=["amulet*"])
+from amulet import version
 
-print(packs)
+packs = find_packages(
+    "src", include=["amulet*"], exclude=["*.command_line", "*.command_line.*"]
+)
+
+requirements_fp = open("./requirements.txt")
+
+required_packages = requirements_fp.readlines()
+
+requirements_fp.close()
 
 setup(
     name="amulet",
-    version="0.0.0",
+    version=".".join(map(str, version.VERSION_NUMBER)),
     packages=packs,
     package_dir={"": "src"},
     include_dirs=include_dirs,
     include_package_data=True,
+    install_requires=required_packages,
 )
