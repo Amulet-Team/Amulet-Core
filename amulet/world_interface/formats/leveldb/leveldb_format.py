94f28891a555075c54cb6d245a560862f57a6635
BluCodeGH <22122986+BluCodeGH@users.noreply.github.com>
2019-10-23 20:47:19 -0400

    Add bedrock loading support

---
+++ amulet/world_interface/formats/leveldb/leveldb_format.py
@@ -0,0 +1,60 @@
+from __future__ import annotations
+
+import os
+import struct
+from typing import Tuple
+
+import amulet_nbt as nbt
+
+from amulet.utils.format_utils import check_all_exist
+from amulet.world_interface.formats import Format
+from amulet.world_interface import interfaces
+from amulet.world_interface.leveldb import LevelDB
+
+
+class LevelDBFormat(Format):
+    def __init__(self, *args):
+        super().__init__(*args)
+        with open(os.path.join(self._directory, "level.dat"), "rb") as f:
+            self.root_tag = nbt.load(buffer=f.read()[8:])
+        self._db = LevelDB(os.path.join(self._directory, "db"))
+
+    def save(self):
+        raise NotImplementedError
+
+    def close(self):
+        self._db.close()
+
+    def _max_world_version(self) -> Tuple:
+        return 'leveldb', [t.value for t in self.root_tag.value["MinimumCompatibleClientVersion"]]
+
+    def _get_raw_chunk_data(self, cx, cz) -> Tuple[int, int, LevelDB]:
+        """
+        Return the interface key and data to interface with given chunk coordinates.
+
+        :param cx: The x coordinate of the chunk.
+        :param cz: The z coordinate of the chunk.
+        :return: The interface key for the get_interface method and the data to interface with.
+        """
+        return (cx, cz, self._db)
+
+    def _get_interface(self, max_world_version, raw_chunk_data=None) -> interfaces.Interface:
+        if raw_chunk_data is not None:
+            cx, cz = raw_chunk_data[:2]
+            chunk_key_base = struct.pack("<ii", cx, cz)
+            chunk_version = self._db.get(chunk_key_base + b"v")[0]
+            key = ("leveldb", chunk_version)
+        else:
+            key = max_world_version
+        return interfaces.get_interface(key)
+
+    @staticmethod
+    def is_valid(directory):
+        print(directory)
+        if not check_all_exist(directory, "db", "level.dat", "levelname.txt"):
+            return False
+
+        return True
+
+
+FORMAT_CLASS = LevelDBFormat
---
+++ amulet/world_interface/interfaces/leveldb/leveldb_interface.py
@@ -0,0 +1,110 @@
+from __future__ import annotations
+
+from typing import List, Union, Tuple
+
+import struct
+import numpy
+import amulet_nbt
+
+from amulet.api.block import Block
+from amulet.api.chunk import Chunk
+from amulet.world_interface.interfaces import Interface
+from amulet.world_interface.leveldb import LevelDB
+from amulet.world_interface import translators
+from amulet.utils.world_utils import get_smallest_dtype
+
+
+class LevelDBInterface(Interface):
+    @staticmethod
+    def is_valid(key):
+        if key[0] != "leveldb":
+            return False
+        if key[1] not in [10, 13, 14, 15]:
+            return False
+        return True
+
+    def decode(self, data: Tuple[int, int, LevelDB]) -> Tuple[Chunk, numpy.ndarray]:
+        cx, cz, db = data
+        # chunk_key_base = struct.pack("<ii", cx, cz)
+
+        subchunks = []
+        for i in range(16):
+            try:
+                key = struct.pack("<iicB", cx, cz, b"/", i)
+                subchunks.append(db.get(key))
+            except KeyError:
+                subchunks.append(None)
+
+        blocks, palette = self._load_subchunks(subchunks)
+        entities = None
+        tile_entities = None
+        return Chunk(cx, cz, blocks, entities, tile_entities), palette
+
+    def _load_subchunks(self, subchunks):
+        blocks = numpy.zeros((16, 256, 16), dtype=numpy.uint32)
+        palette = [Block(namespace="minecraft", base_name="air", properties={"block_data": 0})]
+        for y, data in enumerate(subchunks):
+            if data is None:
+                continue
+            version, numStorages, data = data[0], data[1], data[2:]
+            block_data, data = self._load_blocks(data)
+            palette_data = self._load_palette(data)
+            for i, block in enumerate(palette_data):
+                namespace, base_name = block["name"].value.split(":", 1)
+                if "states" in block: #1.13 format
+                    properties = block["properties"].value
+                else:
+                    properties = {"block_data": str(block["val"].value)}
+                palette_data[i] = Block(namespace=namespace, base_name=base_name, properties=properties)
+            y *= 16
+            blocks[:, y:y+16, :] = block_data + len(palette)
+            palette += palette_data
+
+        palette, inverse = numpy.unique(palette, return_inverse=True)
+        blocks = inverse[blocks]
+
+        return blocks.astype(f"uint{get_smallest_dtype(blocks)}"), palette
+
+    # These arent actual blocks, just ids pointing to the palette.
+    def _load_blocks(self, data):
+        # Ignore LSB of data (its a flag) and get compacting level
+        bitsPerBlock, data = data[0] >> 1, data[1:]
+        blocksPerWord = 32 // bitsPerBlock  # Word = 4 bytes, basis of compacting.
+        numWords = -(-4096 // blocksPerWord)  # Ceiling divide is inverted floor divide
+
+        blockWords, data = (
+            struct.unpack("<" + "I" * numWords, data[: 4 * numWords]),
+            data[4 * numWords :],
+        )
+        blocks = numpy.empty(4096, dtype=numpy.uint32)
+        for i, word in enumerate(blockWords):
+            for j in range(blocksPerWord):
+                block = word & (
+                    (1 << bitsPerBlock) - 1
+                )  # Mask out number of bits for one block
+                word >>= bitsPerBlock  # For next iteration
+                if i * blocksPerWord + j < 4096:  # Safety net for padding at end.
+                    blocks[i * blocksPerWord + j] = block
+        blocks = blocks.reshape(16, 16, 16).swapaxes(1, 2)
+        return blocks, data
+
+    # NBT encoded block names (with minecraft:) and data values.
+    def _load_palette(self, data):
+        palette_len, data = struct.unpack("<I", data[:4])[0], data[4:]
+        palette, offset = amulet_nbt.load(buffer=data, compressed=False, count=palette_len, offset=True)
+        return palette
+
+    def encode(self, chunk: Chunk, palette: numpy.ndarray) -> Tuple[int, int, LevelDB]:
+        raise NotImplementedError
+
+    def get_translator(self, max_world_version, data: Tuple[int, int, LevelDB] = None) -> translators.Translator:
+        if data is None:
+            return translators.get_translator(max_world_version)
+        else:
+            cx, cz, db = data
+            chunk_key_base = struct.pack("<ii", cx, cz)
+            chunk_version = db.get(chunk_key_base + b"v")[0]
+            return translators.get_translator(("leveldb", chunk_version))
+
+
+INTERFACE_CLASS = LevelDBInterface
