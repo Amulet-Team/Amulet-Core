d010ca02409822994877c12fd435cd349ad67f93
BluCodeGH <22122986+BluCodeGH@users.noreply.github.com>
2019-10-25 09:43:16 -0400

    Convert loader to from tabs to spaces.

--- amulet/world_interface/loader.py
+++ amulet/world_interface/loader.py
@@ -10,98 +10,98 @@ from amulet.api.errors import LoaderNoneMatched
 
 
 class Loader:
-	def __init__(self, object_type: str, directory: str, supported_meta_version, supported_version, create_instance=True):
-		self._object_type = object_type
-		self._directory = directory
-		self._supported_meta_version = supported_meta_version
-		self._supported_version = supported_version
-		self._loaded: Dict[str, Any] = {}
-		self._is_loaded = False
-		self._create_instance = create_instance
+    def __init__(self, object_type: str, directory: str, supported_meta_version, supported_version, create_instance=True):
+        self._object_type = object_type
+        self._directory = directory
+        self._supported_meta_version = supported_meta_version
+        self._supported_version = supported_version
+        self._loaded: Dict[str, Any] = {}
+        self._is_loaded = False
+        self._create_instance = create_instance
 
-	def _find(self):
-		"""Load all objects from the object directory"""
+    def _find(self):
+        """Load all objects from the object directory"""
 
-		directories = glob.iglob(os.path.join(self._directory, "*", ""))
-		for d in directories:
-			meta_path = os.path.join(d, f"{self._object_type}.meta")
-			if not os.path.exists(meta_path):
-				continue
+        directories = glob.iglob(os.path.join(self._directory, "*", ""))
+        for d in directories:
+            meta_path = os.path.join(d, f"{self._object_type}.meta")
+            if not os.path.exists(meta_path):
+                continue
 
-			with open(meta_path) as fp:
-				meta = json.load(fp)
+            with open(meta_path) as fp:
+                meta = json.load(fp)
 
-			if meta["meta_version"] != self._supported_meta_version:
-				print(
-					f'[Error] Couldn\'t enable {self._object_type} located in "{d}" due to unsupported meta version'
-				)
-				continue
+            if meta["meta_version"] != self._supported_meta_version:
+                print(
+                    f'[Error] Couldn\'t enable {self._object_type} located in "{d}" due to unsupported meta version'
+                )
+                continue
 
-			if meta[self._object_type][f"{self._object_type}_version"] != self._supported_version:
-				print(
-					f"[Error] Couldn't enable {self._object_type} \"{meta[self._object_type]['id']}\" due to unsupported {self._object_type} version"
-				)
-				continue
+            if meta[self._object_type][f"{self._object_type}_version"] != self._supported_version:
+                print(
+                    f"[Error] Couldn't enable {self._object_type} \"{meta[self._object_type]['id']}\" due to unsupported {self._object_type} version"
+                )
+                continue
 
-			spec = importlib.util.spec_from_file_location(
-				meta[self._object_type]["entry_point"],
-				os.path.join(d, meta[self._object_type]["entry_point"] + ".py"),
-			)
-			modu = importlib.util.module_from_spec(spec)
-			spec.loader.exec_module(modu)
+            spec = importlib.util.spec_from_file_location(
+                meta[self._object_type]["entry_point"],
+                os.path.join(d, meta[self._object_type]["entry_point"] + ".py"),
+            )
+            modu = importlib.util.module_from_spec(spec)
+            spec.loader.exec_module(modu)
 
-			if not hasattr(modu, f"{self._object_type.upper()}_CLASS"):
-				print(
-					f"[Error] {self._object_type} \"{meta[self._object_type]['id']}\" is missing the {self._object_type.upper()}_CLASS attribute"
-				)
-				continue
+            if not hasattr(modu, f"{self._object_type.upper()}_CLASS"):
+                print(
+                    f"[Error] {self._object_type} \"{meta[self._object_type]['id']}\" is missing the {self._object_type.upper()}_CLASS attribute"
+                )
+                continue
 
-			c = getattr(modu, f"{self._object_type.upper()}_CLASS")
-			if self._create_instance:
-				self._loaded[meta[f"{self._object_type}"]["id"]] = c()
-			else:
-				self._loaded[meta[f"{self._object_type}"]["id"]] = c
+            c = getattr(modu, f"{self._object_type.upper()}_CLASS")
+            if self._create_instance:
+                self._loaded[meta[f"{self._object_type}"]["id"]] = c()
+            else:
+                self._loaded[meta[f"{self._object_type}"]["id"]] = c
 
-			if __debug__:
-				print(
-					f"[Debug] Enabled {self._object_type} \"{meta[self._object_type]['id']}\", version {meta[self._object_type]['wrapper_version']}"
-				)
+            if __debug__:
+                print(
+                    f"[Debug] Enabled {self._object_type} \"{meta[self._object_type]['id']}\", version {meta[self._object_type]['wrapper_version']}"
+                )
 
-		self._is_loaded = True
+        self._is_loaded = True
 
-	def reload(self):
-		"""Reloads all objects"""
-		self._loaded.clear()
-		self._find()
+    def reload(self):
+        """Reloads all objects"""
+        self._loaded.clear()
+        self._find()
 
-	def get_all(self) -> AbstractSet[str]:
-		"""
-		:return: The identifiers of all loaded objects
-		"""
-		if not self._is_loaded:
-			self._find()
-		return self._loaded.keys()
+    def get_all(self) -> AbstractSet[str]:
+        """
+        :return: The identifiers of all loaded objects
+        """
+        if not self._is_loaded:
+            self._find()
+        return self._loaded.keys()
 
-	def get(self, identifier: Any) -> Any:
-		"""
-		Given an ``identifier`` will find a valid class and return it
+    def get(self, identifier: Any) -> Any:
+        """
+        Given an ``identifier`` will find a valid class and return it
 
-		:param identifier: The identifier for the desired loaded object
-		:return: The class for the object
-		"""
-		object_id = self.identify(identifier)
-		return self._loaded[object_id]
+        :param identifier: The identifier for the desired loaded object
+        :return: The class for the object
+        """
+        object_id = self.identify(identifier)
+        return self._loaded[object_id]
 
-	def identify(self, identifier: Any) -> str:
+    def identify(self, identifier: Any) -> str:
 
-		if not self._is_loaded:
-			self._find()
+        if not self._is_loaded:
+            self._find()
 
-		for object_name, object_instance in self._loaded.items():
-			if object_instance.is_valid(identifier):
-				return object_name
+        for object_name, object_instance in self._loaded.items():
+            if object_instance.is_valid(identifier):
+                return object_name
 
-		raise LoaderNoneMatched(f"Could not find a matching {self._object_type}")
+        raise LoaderNoneMatched(f"Could not find a matching {self._object_type}")
 
-	def get_by_id(self, object_id: str):
-		return self._loaded[object_id]
+    def get_by_id(self, object_id: str):
+        return self._loaded[object_id]
