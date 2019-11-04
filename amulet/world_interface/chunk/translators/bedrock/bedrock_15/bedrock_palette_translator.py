f18031cd791d5b09abbe6f02280b7b0425959567
gentlegiantJGC <james_clare1@yahoo.co.uk>
2019-11-04 12:57:42 +0000

    Renamed translators to end _translator
    This is consistent with interfaces and makes finding files easier

--- amulet/world_interface/chunk/translators/bedrock/bedrock_15/bedrock_palette_translator.py
+++ amulet/world_interface/chunk/translators/bedrock/bedrock_15/bedrock_palette_translator.py
@@ -5,7 +5,7 @@ from typing import Union, Tuple
 from amulet.world_interface.chunk.translators import Translator
 
 
-class Bedrock_15_Translator(Translator):
+class BedrockPaletteTranslator(Translator):
     def _translator_key(
         self, version_number: Tuple[int, int, int]
     ) -> Tuple[str, Tuple[int, int, int]]:
@@ -20,4 +20,4 @@ class Bedrock_15_Translator(Translator):
         return True
 
 
-TRANSLATOR_CLASS = Bedrock_15_Translator
+TRANSLATOR_CLASS = BedrockPaletteTranslator
