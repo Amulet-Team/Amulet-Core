20630be311d203c704f3c863b87e0e5e4eccb85a
gentlegiantJGC <james_clare1@yahoo.co.uk>
2019-11-07 17:48:59 +0000

    Added interfaces for leveldb chunk versions 10-15

---
+++ amulet/world_interface/chunk/interfaces/leveldb/leveldb_15/leveldb_15_interface.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+from amulet.world_interface.chunk.interfaces.leveldb.base_leveldb_interface import BaseLevelDBInterface
+
+
+class LevelDB15Interface(BaseLevelDBInterface):
+    def __init__(self):
+        BaseLevelDBInterface.__init__(self)
+
+        self.features["chunk_version"] = 15,
+        # self.features["finalised_state"] = ,
+        # self.features["data_2d"] = ,
+        self.features["entities"] = "32list",
+        self.features["block_entities"] = "31list",
+        self.features["terrain"] = "2fnpalette"
+
+    @staticmethod
+    def is_valid(key):
+        if key[0] != "leveldb":
+            return False
+        if key[1] != 15:
+            return False
+        return True
+
+
+INTERFACE_CLASS = LevelDB15Interface
