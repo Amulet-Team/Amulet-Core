862165a196e20f10044d8fa51c74c7dd37cc163e
Podshot <ben.gothard3@gmail.com>
2019-12-05 15:29:39 -0500

    Specify egg names in requirements.txt to help resolve package dependencies

--- setup.py
+++ setup.py
@@ -2,15 +2,20 @@ from setuptools import setup, find_packages
 
 from amulet import version
 
+
+def get_egg_name_from_git_uri(uri):
+    if uri.startswith("git+") or uri.startswith("https:"):
+        return uri[uri.index("#egg=") + len("#egg=") :]
+    return uri
+
+
 packs = find_packages(
     include=["amulet*"], exclude=["*.command_line", "*.command_line.*"]
 )
 
 with open("./requirements.txt") as requirements_fp:
     required_packages = [
-        line
-        for line in requirements_fp.readlines()
-        if not line.startswith("git+") and not line.startswith("https:")
+        get_egg_name_from_git_uri(line) for line in requirements_fp.readlines()
     ]
 
 setup(
@@ -19,5 +24,6 @@ setup(
     packages=packs,
     include_package_data=True,
     install_requires=required_packages,
+    setup_requires=required_packages,
     dependency_links=["https://github.com/Amulet-Team/Amulet-NBT"],
 )
