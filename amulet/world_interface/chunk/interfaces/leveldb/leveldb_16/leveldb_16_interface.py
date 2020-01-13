e1600e7d9c920e7aaf76110d72e03f29ac74e30a
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-01-13 13:28:26 +0000

    Added an interface for chunk version 16

---
+++ amulet/world_interface/chunk/interfaces/leveldb/leveldb_16/leveldb_16_interface.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+
+from amulet.world_interface.chunk.interfaces.leveldb.leveldb_15.leveldb_15_interface import (
+    LevelDB15Interface,
+)
+
+
+class LevelDB16Interface(LevelDB15Interface):
+    def __init__(self):
+        LevelDB15Interface.__init__(self)
+
+        self.features["chunk_version"] = 16
+
+
+INTERFACE_CLASS = LevelDB16Interface
