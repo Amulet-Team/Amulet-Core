054063e912de63aa685efde4dc7d3d3960e371a2
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2020-02-08 11:08:47 +0000

    Added interfaces for versions back to v4
    Entity format might be wrong in the old versions but these are not implemented yet

---
+++ amulet/world_interface/chunk/interfaces/leveldb/leveldb_5/interface.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+from amulet.world_interface.chunk.interfaces.leveldb.leveldb_4.interface import LevelDB4Interface
+
+
+class LevelDB5Interface(LevelDB4Interface):
+    def __init__(self):
+        LevelDB4Interface.__init__(self)
+
+        self.features["chunk_version"] = 5
+
+
+INTERFACE_CLASS = LevelDB5Interface
--- amulet/world_interface/chunk/interfaces/leveldb/leveldb_6/interface.py
+++ amulet/world_interface/chunk/interfaces/leveldb/leveldb_6/interface.py
@@ -1,28 +1,13 @@
 from __future__ import annotations
 
-from amulet.world_interface.chunk.interfaces.leveldb.base_leveldb_interface import (
-    BaseLevelDBInterface,
-)
+from amulet.world_interface.chunk.interfaces.leveldb.leveldb_5.interface import LevelDB5Interface
 
 
-class LevelDB6Interface(BaseLevelDBInterface):
+class LevelDB6Interface(LevelDB5Interface):
     def __init__(self):
-        BaseLevelDBInterface.__init__(self)
+        LevelDB5Interface.__init__(self)
 
         self.features["chunk_version"] = 6
-        self.features["finalised_state"] = "int0-2"
-
-        self.features["data_2d"] = 'unused_height512|biome256'
-
-        self.features["block_entities"] = "31list"
-        self.features["block_entity_format"] = "str-id"
-        self.features["block_entity_coord_format"] = "xyz-int"
-
-        self.features["entities"] = "32list"
-        self.features["entity_format"] = "int-id"
-        self.features["entity_coord_format"] = "Pos-list-float"
-
-        self.features["terrain"] = "2farray"
 
 
 INTERFACE_CLASS = LevelDB6Interface
