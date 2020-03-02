30cb227c29c3165aa5a94d8ff7d07edf974d2105
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-03-02 13:59:36 +0000

    Rewritten fill and replace to work with the new world format

--- amulet/operations/fill.py
+++ amulet/operations/fill.py
@@ -5,17 +5,21 @@ from typing import TYPE_CHECKING
 
 from amulet.api.selection import SelectionBox
 from amulet.api.block import Block
+from amulet import log
 
 if TYPE_CHECKING:
     from amulet.api.world import World
 
 
-def fill(world: "World", target_box: SelectionBox, fill_block: Block):
+def fill(world: "World", target_box: SelectionBox, options: dict):
+    fill_block = options.get('fill_block', None)
+    if not isinstance(fill_block, Block):
+        log.error('Fill operation was not given a Block object')
+        return
+    fill_block: Block
     internal_id = world.palette.get_add_block(fill_block)
 
     for target in target_box.subboxes():
         block_generator = world.get_sub_chunks(*target.to_slice())
         for selection in block_generator:
-            selection.blocks = numpy.full(
-                selection.blocks.shape, internal_id, selection.blocks.dtype
-            )
+            selection.blocks[:] = internal_id
--- amulet/operations/replace.py
+++ amulet/operations/replace.py
@@ -4,6 +4,7 @@ from typing import List, TYPE_CHECKING
 
 from amulet.api.selection import SelectionBox
 from amulet.api.block import Block
+from amulet import log
 
 if TYPE_CHECKING:
     from amulet.api.world import World
@@ -12,11 +13,26 @@ if TYPE_CHECKING:
 def replace(
     world: "World",
     selection_box: SelectionBox,
-    original_blocks: List[Block],
-    replacement_blocks: List[Block],
+    options: dict
 ):
+    original_blocks = options.get('original_blocks', None)
+    if not isinstance(original_blocks, list) and all(isinstance(block, Block) for block in original_blocks):
+        log.error('Replace operation was not given a list of source Block objects')
+        return
+
+    replacement_blocks = options.get('replacement_blocks', None)
+    if not isinstance(replacement_blocks, list) and all(isinstance(block, Block) for block in replacement_blocks):
+        log.error('Replace operation was not given a list of destination Block objects')
+        return
+    original_blocks: List[Block]
+    replacement_blocks: List[Block]
+
     if len(original_blocks) != len(replacement_blocks):
-        raise Exception
+        if len(replacement_blocks) == 1:
+            replacement_blocks = replacement_blocks * len(original_blocks)
+        else:
+            log.error('Replace operation must be given the same number of destination blocks as source blocks')
+
     original_internal_ids = map(world.palette.get_add_block, original_blocks)
     replacement_internal_ids = map(world.palette.get_add_block, replacement_blocks)
 
@@ -26,6 +42,4 @@ def replace(
             for original_id, replacement_id in zip(
                 original_internal_ids, replacement_internal_ids
             ):
-                blocks = selection.blocks.copy()
-                blocks[blocks == original_id] = replacement_id
-                selection.blocks = blocks
+                selection.blocks[selection.blocks == original_id] = replacement_id
