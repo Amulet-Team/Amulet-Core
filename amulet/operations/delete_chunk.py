141da215d62faa9f561a7d7bf9514d7260715d3c
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-03-04 11:14:19 +0000

    Simplified delete_chunk operation

--- amulet/operations/delete_chunk.py
+++ amulet/operations/delete_chunk.py
@@ -6,13 +6,5 @@ if TYPE_CHECKING:
 
 
 def delete_chunk(world: "World", source_box: Selection):
-    already_deleted_chunks = set()
-    for subbox in source_box.subboxes():
-        sub_chunks = world.get_sub_chunks(*subbox.to_slice())
-        for sub_chunk in sub_chunks:
-            chunk_coords = sub_chunk.parent_coordinates
-            if chunk_coords in already_deleted_chunks:
-                continue
-            world.get_chunk(*chunk_coords)
-            world.delete_chunk(*chunk_coords)
-            already_deleted_chunks.add(chunk_coords)
+    for chunk, _ in world.get_chunk_slices(source_box):
+        world.delete_chunk(*chunk.coordinates)
