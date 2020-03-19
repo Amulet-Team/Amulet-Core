7b649f5fffe4d83e8c9cbe6abf1a45a2942ca5eb
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-03-19 11:19:40 +0000

    Removed old blocks array

--- amulet/api/chunk/blocks.py
+++ amulet/api/chunk/blocks.py
@@ -1,24 +1,5 @@
-import numpy
-import weakref
-
-from amulet.api.chunk.chunk_array import ChunkArray
 from amulet.api.chunk.partial_array import PartialNDArray
 
-from typing import TYPE_CHECKING
 
-if TYPE_CHECKING:
-    from amulet.api.chunk import Chunk
-
-
-class Blocks(ChunkArray):
-    def __new__(cls, parent_chunk: "Chunk", input_array=None):
-        if input_array is None:
-            input_array = numpy.zeros((16, 256, 16), dtype=numpy.int)
-        obj = numpy.asarray(input_array).view(cls)
-        obj._parent_chunk = weakref.ref(parent_chunk)
-        return obj
-
-
-class Blocks2(PartialNDArray):
+class Blocks(PartialNDArray):
     pass
-
