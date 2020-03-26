3ad2a480cd5098f34a292800a3813ccac3d70876
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-03-26 19:19:15 +0000

    Modified paste operation to extend the block palette
    The structure may have a different block pallete to the world

--- amulet/operations/paste.py
+++ amulet/operations/paste.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from typing import TYPE_CHECKING
+import numpy
 
 from amulet.api.structure import Structure
 
@@ -10,7 +11,9 @@ if TYPE_CHECKING:
 
 def paste(world: "World", dimension: int, structure: Structure, dst: dict):
     dst_location = (dst.get('x', 0), dst.get('y', 0), dst.get('z', 0))
+    gab = numpy.vectorize(world.palette.get_add_block)
+    lut = gab(structure.palette.blocks())
     for src_chunk, src_slices, _, (dst_cx, dst_cz), dst_slices, _ in structure.get_moved_chunk_slices(dst_location):
         dst_chunk = world.get_chunk(dst_cx, dst_cz, dimension)
-        dst_chunk.blocks[dst_slices] = src_chunk.blocks[src_slices]
+        dst_chunk.blocks[dst_slices] = lut[src_chunk.blocks[src_slices]]
         dst_chunk.changed = True
