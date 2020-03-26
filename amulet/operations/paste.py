50262c74e54887f273018e68e4cff1bc954ee1c8
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-03-26 18:52:07 +0000

    Removed source from paste operation

--- amulet/operations/paste.py
+++ amulet/operations/paste.py
@@ -2,14 +2,13 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from amulet.api.selection import Selection
 from amulet.api.structure import Structure
 
 if TYPE_CHECKING:
     from amulet.api.world import World
 
 
-def paste(world: "World", dimension: int, source: Selection, structure: Structure, dst: dict):
+def paste(world: "World", dimension: int, structure: Structure, dst: dict):
     dst_location = (dst.get('x', 0), dst.get('y', 0), dst.get('z', 0))
     for src_chunk, src_slices, _, (dst_cx, dst_cz), dst_slices, _ in structure.get_moved_chunk_slices(dst_location):
         dst_chunk = world.get_chunk(dst_cx, dst_cz, dimension)
