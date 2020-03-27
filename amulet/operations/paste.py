2edf7d3b7fc3c4033fbe7725672dbaa941fc04ca
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-03-27 10:47:03 +0000

    Fixed paste operation so it can create chunks if they do not exist

--- amulet/operations/paste.py
+++ amulet/operations/paste.py
@@ -4,6 +4,8 @@ from typing import TYPE_CHECKING
 import numpy
 
 from amulet.api.structure import Structure
+from amulet.api.errors import ChunkLoadError, ChunkDoesNotExist
+from amulet.api.chunk import Chunk
 
 if TYPE_CHECKING:
     from amulet.api.world import World
@@ -14,6 +16,13 @@ def paste(world: "World", dimension: int, structure: Structure, dst: dict):
     gab = numpy.vectorize(world.palette.get_add_block)
     lut = gab(structure.palette.blocks())
     for src_chunk, src_slices, _, (dst_cx, dst_cz), dst_slices, _ in structure.get_moved_chunk_slices(dst_location):
-        dst_chunk = world.get_chunk(dst_cx, dst_cz, dimension)
+        try:
+            dst_chunk = world.get_chunk(dst_cx, dst_cz, dimension)
+        except ChunkDoesNotExist:
+            dst_chunk = Chunk(dst_cx, dst_cz)
+            world.put_chunk(dst_chunk, dimension)
+        except ChunkLoadError:
+            continue
+
         dst_chunk.blocks[dst_slices] = lut[src_chunk.blocks[src_slices]]
         dst_chunk.changed = True
