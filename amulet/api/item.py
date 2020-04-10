995ebfb8333337120011771f7a80a60669f98e88
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-04-10 10:40:44 +0100

    Blocked out an item and block item class
    These are subclasses of TAG_Compound so that they can be stored in NBT

---
+++ amulet/api/item.py
@@ -0,0 +1,67 @@
+import amulet_nbt
+
+
+class Item(amulet_nbt.TAG_Compound):
+    """
+    {
+        "namespace": TAG_String,
+        "base_name": TAG_String,
+        "metadata": TAG_Compound
+    }
+    """
+    def __init__(self, namespace: str, base_name: str, metadata: dict = None):
+        super().__init__(
+            {
+                "namespace": amulet_nbt.TAG_String(namespace),
+                "base_name": amulet_nbt.TAG_String(base_name),
+                "metadata": amulet_nbt.TAG_Compound(metadata or {})
+            }
+        )
+
+    @property
+    def namespace(self) -> str:
+        return self.value["namespace"].value
+
+    @property
+    def base_name(self) -> str:
+        return self.value["base_name"].value
+
+    @property
+    def metadata(self) -> amulet_nbt.TAG_Compound:
+        return self.value["metadata"].value
+
+
+class BlockItem(amulet_nbt.TAG_Compound):
+    """
+    {
+        "namespace": TAG_String,
+        "base_name": TAG_String,
+        "properties": TAG_Compound
+        "metadata": TAG_Compound
+    }
+    """
+    def __init__(self, namespace: str, base_name: str, properties: dict = None, metadata: dict = None):
+        super().__init__(
+            {
+                "namespace": amulet_nbt.TAG_String(namespace),
+                "base_name": amulet_nbt.TAG_String(base_name),
+                "properties": amulet_nbt.TAG_Compound(properties or {}),
+                "metadata": amulet_nbt.TAG_Compound(metadata or {})
+            }
+        )
+
+    @property
+    def namespace(self) -> str:
+        return self.value["namespace"].value
+
+    @property
+    def base_name(self) -> str:
+        return self.value["base_name"].value
+
+    @property
+    def properties(self) -> amulet_nbt.TAG_Compound:
+        return self.value["properties"].value
+
+    @property
+    def metadata(self) -> amulet_nbt.TAG_Compound:
+        return self.value["metadata"].value
