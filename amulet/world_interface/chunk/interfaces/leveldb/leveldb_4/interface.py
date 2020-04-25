e5defd05a5b3c6665430fcdbb2f0567a5b20b5aa
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-04-25 09:11:17 +0100

    Added a Bedrock v3 interface

--- amulet/world_interface/chunk/interfaces/leveldb/leveldb_4/interface.py
+++ amulet/world_interface/chunk/interfaces/leveldb/leveldb_4/interface.py
@@ -1,28 +1,15 @@
 from __future__ import annotations
 
-from amulet.world_interface.chunk.interfaces.leveldb.base_leveldb_interface import (
-    BaseLevelDBInterface,
+from amulet.world_interface.chunk.interfaces.leveldb.leveldb_3.interface import (
+    LevelDB3Interface,
 )
 
 
-class LevelDB4Interface(BaseLevelDBInterface):
+class LevelDB4Interface(LevelDB3Interface):
     def __init__(self):
-        BaseLevelDBInterface.__init__(self)
+        LevelDB3Interface.__init__(self)
 
         self.features["chunk_version"] = 4
-        self.features["finalised_state"] = "int0-2"
-
-        self.features["data_2d"] = "unused_height512|biome256"
-
-        self.features["block_entities"] = "31list"
-        self.features["block_entity_format"] = "str-id"
-        self.features["block_entity_coord_format"] = "xyz-int"
-
-        self.features["entities"] = "32list"
-        self.features["entity_format"] = "int-id"
-        self.features["entity_coord_format"] = "Pos-list-float"
-
-        self.features["terrain"] = "2farray"
 
 
 INTERFACE_CLASS = LevelDB4Interface
