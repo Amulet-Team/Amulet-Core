233e78a44d7ea6738b92de4c33f578babff1efb4
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-04-30 10:18:46 +0100

    Added a block subclass that also stores version number
    This will be used so that the bedrock palette just stores a block rather than a tuple

---
+++ amulet/api/wrapper/block.py
@@ -0,0 +1,21 @@
+from typing import Dict, Union, Iterable
+from amulet.api.block import Block
+from amulet.api.wrapper.chunk.translator import VersionIdentifierType
+import amulet_nbt
+
+
+class MetaBlock(Block):
+    def __init__(
+        self,
+        namespace: str,
+        base_name: str,
+        properties: Dict[str, amulet_nbt.BaseValueType] = None,
+        extra_blocks: Union[Block, Iterable[Block]] = None,
+        version: VersionIdentifierType = None
+    ):
+        super().__init__(namespace, base_name, properties, extra_blocks)
+        self._version = version
+
+    @property
+    def version(self) -> VersionIdentifierType:
+        return self._version
