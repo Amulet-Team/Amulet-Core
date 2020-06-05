4e67daa435ffa33dfdc5f4d67ecb6171c920d6a8
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-06-05 16:19:14 +0100

    Improved paste operation and added multi-paste support

--- amulet/operations/fill.py
+++ amulet/operations/fill.py
@@ -4,7 +4,7 @@ from typing import TYPE_CHECKING
 
 from amulet.api.selection import SelectionGroup
 from amulet.api.block import Block
-from amulet.api.data_types import Dimension
+from amulet.api.data_types import Dimension, OperationReturnType
 from amulet import log
 
 if TYPE_CHECKING:
@@ -13,7 +13,7 @@ if TYPE_CHECKING:
 
 def fill(
     world: "World", dimension: Dimension, target_box: SelectionGroup, fill_block: Block
-):
+) -> OperationReturnType:
     if not isinstance(fill_block, Block):
         raise Exception("Fill operation was not given a Block object")
     internal_id = world.palette.get_add_block(fill_block)
--- amulet/operations/paste.py
+++ amulet/operations/paste.py
@@ -1,23 +1,37 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Sequence
 import numpy
 
 from amulet.api.structure import Structure
 from amulet.api.errors import ChunkLoadError, ChunkDoesNotExist
 from amulet.api.chunk import Chunk
-from amulet.api.data_types import Dimension
+from amulet.api.data_types import Dimension, BlockCoordinates
 
 if TYPE_CHECKING:
     from amulet.api.world import World
 
 
-def paste(world: "World", dimension: Dimension, structure: Structure, options: dict):
-    dst_location = options.get("location", (0, 0, 0))
-    copy_air = options.get("copy_air", True)
+def paste(
+        world: "World",
+        dimension: Dimension,
+        structure: Structure,
+        locations: Sequence[BlockCoordinates],
+        copy_air=True
+):
+    for _ in paste_iter(world, dimension, structure, locations, copy_air):
+        pass
+
+
+def paste_iter(
+        world: "World",
+        dimension: Dimension,
+        structure: Structure,
+        locations: Sequence[BlockCoordinates],
+        copy_air=True
+):
     gab = numpy.vectorize(world.palette.get_add_block)
     lut = gab(structure.palette.blocks())
-    offset = -structure.selection.min + dst_location
     non_air = None
     if not copy_air:
         non_air = numpy.array(
@@ -27,52 +41,55 @@ def paste(world: "World", dimension: Dimension, structure: Structure, options: d
             ]
         )
 
-    iter_count = len(list(structure.get_moved_chunk_slices(dst_location)))
-    count = 0
+    for location_index, dst_location in enumerate(locations):
+        offset = -structure.selection.min + dst_location
 
-    for (
-        src_chunk,
-        src_slices,
-        src_box,
-        (dst_cx, dst_cz),
-        dst_slices,
-        dst_box,
-    ) in structure.get_moved_chunk_slices(dst_location):
-        try:
-            dst_chunk = world.get_chunk(dst_cx, dst_cz, dimension)
-        except ChunkDoesNotExist:
-            dst_chunk = Chunk(dst_cx, dst_cz)
-            world.put_chunk(dst_chunk, dimension)
-        except ChunkLoadError:
-            continue
-        remove_block_entities = []
-        for block_entity_location in dst_chunk.block_entities.keys():
-            if block_entity_location in dst_box:
-                if copy_air:
-                    remove_block_entities.append(block_entity_location)
-                else:
-                    chunk_block_entity_location = (
-                        numpy.array(block_entity_location) - offset
-                    )
-                    chunk_block_entity_location[[0, 2]] %= 16
-                    if non_air[src_chunk.blocks[tuple(chunk_block_entity_location)]]:
+        iter_count = len(list(structure.get_moved_chunk_slices(dst_location)))
+        count = 0
+
+        for (
+            src_chunk,
+            src_slices,
+            src_box,
+            (dst_cx, dst_cz),
+            dst_slices,
+            dst_box,
+        ) in structure.get_moved_chunk_slices(dst_location):
+            try:
+                dst_chunk = world.get_chunk(dst_cx, dst_cz, dimension)
+            except ChunkDoesNotExist:
+                dst_chunk = Chunk(dst_cx, dst_cz)
+                world.put_chunk(dst_chunk, dimension)
+            except ChunkLoadError:
+                continue
+            remove_block_entities = []
+            for block_entity_location in dst_chunk.block_entities.keys():
+                if block_entity_location in dst_box:
+                    if copy_air:
                         remove_block_entities.append(block_entity_location)
-        for block_entity_location in remove_block_entities:
-            del dst_chunk.block_entities[block_entity_location]
-        for block_entity_location, block_entity in src_chunk.block_entities.items():
-            if block_entity_location in src_box:
-                dst_chunk.block_entities.insert(
-                    block_entity.new_at_location(*offset + block_entity_location)
-                )
+                    else:
+                        chunk_block_entity_location = (
+                            numpy.array(block_entity_location) - offset
+                        )
+                        chunk_block_entity_location[[0, 2]] %= 16
+                        if non_air[src_chunk.blocks[tuple(chunk_block_entity_location)]]:
+                            remove_block_entities.append(block_entity_location)
+            for block_entity_location in remove_block_entities:
+                del dst_chunk.block_entities[block_entity_location]
+            for block_entity_location, block_entity in src_chunk.block_entities.items():
+                if block_entity_location in src_box:
+                    dst_chunk.block_entities.insert(
+                        block_entity.new_at_location(*offset + block_entity_location)
+                    )
 
-        if not copy_air:
-            dst_blocks_copy = dst_chunk.blocks[dst_slices]
-            mask = non_air[src_chunk.blocks[src_slices]]
-            dst_blocks_copy[mask] = lut[src_chunk.blocks[src_slices]][mask]
-            dst_chunk.blocks[dst_slices] = dst_blocks_copy
-        else:
-            dst_chunk.blocks[dst_slices] = lut[src_chunk.blocks[src_slices]]
-        dst_chunk.changed = True
+            if not copy_air:
+                dst_blocks_copy = dst_chunk.blocks[dst_slices]
+                mask = non_air[src_chunk.blocks[src_slices]]
+                dst_blocks_copy[mask] = lut[src_chunk.blocks[src_slices]][mask]
+                dst_chunk.blocks[dst_slices] = dst_blocks_copy
+            else:
+                dst_chunk.blocks[dst_slices] = lut[src_chunk.blocks[src_slices]]
+            dst_chunk.changed = True
 
-        count += 1
-        yield 100 * count / iter_count
+            count += 1
+            yield (count / iter_count) * (location_index/len(locations))
