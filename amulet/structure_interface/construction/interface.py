4986e49c143abded07e548b486d71c5085a77d41
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-06-22 15:59:05 +0100

    Fixed overflowing issue when importing construction files

--- amulet/structure_interface/construction/interface.py
+++ amulet/structure_interface/construction/interface.py
@@ -34,7 +34,7 @@ class ConstructionInterface(Interface):
                 sz = section.sz - ((section.sz >> 4) << 4)
                 chunk.blocks[
                     sx : sx + shapex, sy : sy + shapey, sz : sz + shapez,
-                ] = section.blocks + len(palette)
+                ] = section.blocks.astype(numpy.uint32) + len(palette)
                 chunk.block_entities.update(section.block_entities)
             chunk.entities.extend(section.entities)
             palette += section.palette
