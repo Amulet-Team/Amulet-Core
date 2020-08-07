fd6c9b09ed9333286bc57c76ab41e2eb6796055d
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-08-07 09:11:08 +0100

    Fixed an issue with glob

--- amulet/world_interface/loader.py
+++ amulet/world_interface/loader.py
@@ -31,7 +31,7 @@ class Loader:
         """Load all objects from the object directory"""
 
         directories = glob.iglob(
-            os.path.join(self._directory, "**", ""), recursive=True
+            glob.escape(os.path.join(self._directory, "**", "")), recursive=True
         )
         for d in directories:
             meta_path = os.path.join(d, f"{self._object_type}.meta")
