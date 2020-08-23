40d176e94d8c45351c2d848c361e196ea30f453d
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-08-23 11:59:23 +0100

    Modified Blocks to use the new array class

--- amulet/api/chunk/blocks.py
+++ amulet/api/chunk/blocks.py
@@ -1,37 +1,29 @@
 import numpy
 from typing import Iterable
 
-from amulet.api.partial_array.partial_array import PartialNDArray
+from amulet.api.partial_3d_array import Partial3DArray
 
 
-class Blocks(PartialNDArray):
-    _size_x = 16
-    _section_size_y = 16
-    _size_z = 16
-    _default_min_section = 0
-    _default_max_section = 16
-    _flat_array = numpy.zeros((_size_x, _size_z), dtype=bool)
-
-    def _check_type(self, other) -> bool:
-        return isinstance(other, Blocks)
+class Blocks(Partial3DArray):
+    def __init__(self):
+        super().__init__(
+            numpy.uint32,
+            0,
+            (16, 16, 16),
+            (None, None, None),
+            (None, None, None),
+            (None, None, None),
+        )
 
     @property
     def sub_chunks(self) -> Iterable[int]:
         """An iterable of the sub-chunk indexes that exist"""
         return self.sections
 
-    def get_create_sub_chunk(self, cy: int) -> numpy.ndarray:
-        """Get the section ndarray for a given section index. Create if it does not exist.
-        :param cy: The section y index
-        :return: Numpy array for this section
-        """
-        return self.get_create_section(cy)
-
     def get_sub_chunk(self, cy: int) -> numpy.ndarray:
         """Get the section ndarray for a given section index.
         :param cy: The section y index
         :return: Numpy array for this section
-        :raises: KeyError if no section exists with this index
         """
         return self.get_section(cy)
 
