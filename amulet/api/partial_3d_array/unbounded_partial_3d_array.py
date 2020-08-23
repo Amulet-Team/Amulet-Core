c81969ce0b3da3912ac9bbf315f0185d5caa3689
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-08-23 15:17:51 +0100

    Added the start of the getitem method to the unbounded partial array

--- amulet/api/partial_3d_array/unbounded_partial_3d_array.py
+++ amulet/api/partial_3d_array/unbounded_partial_3d_array.py
@@ -3,6 +3,7 @@ import numpy
 
 from .base_partial_3d_array import BasePartial3DArray
 from .bounded_partial_3d_array import BoundedPartial3DArray
+from .util import sanitise_slice, to_slice, unpack_slice
 
 
 class UnboundedPartial3DArray(BasePartial3DArray):
@@ -17,8 +18,7 @@ class UnboundedPartial3DArray(BasePartial3DArray):
     the defined sections need to be held in memory.
 
     This class also implements an API that resembles that of the numpy.ndarray but it is not directly compatible with numpy.
-    Chained indexing does not currently work as __getitem__ returns a copy of the array data rather than a view into it.
-    To use numpy fully the section data will need to be directly used.
+    This class implements methods to access and directly modify the underlying numpy arrays to give finer control.
     """
 
     def __init__(
@@ -39,7 +39,7 @@ class UnboundedPartial3DArray(BasePartial3DArray):
             sections=sections
         )
         self._default_min_y = - default_section_counts[0] * self.section_shape[1]
-        self._default_max_y = - default_section_counts[1] * self.section_shape[1]
+        self._default_max_y = default_section_counts[1] * self.section_shape[1]
 
     @property
     def sections(self) -> Iterable[int]:
@@ -73,3 +73,64 @@ class UnboundedPartial3DArray(BasePartial3DArray):
             self.create_section(cy)
         return self._sections[cy]
 
+    @overload
+    def __getitem__(self, slices: Tuple[int, int, int]) -> int:
+        ...
+
+    @overload
+    def __getitem__(
+            self, slices: Tuple[Union[int, slice], Union[int, slice], Union[int, slice]]
+    ) -> BoundedPartial3DArray:
+        ...
+
+    def __getitem__(self, item):
+        if isinstance(item, tuple):
+            if len(item) != 3:
+                raise Exception(f"Tuple item must be of length 3, got {len(item)}")
+            if all(isinstance(i, (int, numpy.integer)) for i in item):
+                cy = item[1] // self.section_shape[1]
+                if cy in self:
+                    return int(
+                        self._sections[cy][
+                            (item[0], item[1] % self.section_shape[1], item[2])
+                        ]
+                    )
+                else:
+                    return 0
+
+            elif all(
+                    isinstance(i, (int, numpy.integer, slice)) for i in item
+            ):
+                start_x, stop_x, step_x = sanitise_slice(*unpack_slice(to_slice(item[0])), self.size_x)
+                start_y, stop_y, step_y = unpack_slice(to_slice(item[1]))
+                start_z, stop_z, step_z = sanitise_slice(*unpack_slice(to_slice(item[2])), self.size_z)
+
+                # cap at the normal limits
+                if step_y > 0:
+                    if start_y is None:
+                        start_y = self._default_min_y
+                    if stop_y is None:
+                        stop_y = self._default_max_y
+                else:
+                    if start_y is None:
+                        start_y = self._default_max_y
+                    if stop_y is None:
+                        stop_y = self._default_min_y
+
+                return BoundedPartial3DArray.from_partial_array(
+                    self,
+                    (start_x, start_y, start_z),
+                    (stop_x, stop_y, stop_z),
+                    (step_x, step_y, step_z)
+                )
+            else:
+                raise Exception(f"Unsupported tuple {item} for getitem")
+
+        elif isinstance(item, numpy.ndarray):
+            raise NotImplementedError(
+                "numpy.ndarray is not currently supported as a slice input"
+            )
+        else:
+            raise Exception(
+                f"{item.__class__.__name__}({item}) is not a supported input for __getitem__"
+            )
