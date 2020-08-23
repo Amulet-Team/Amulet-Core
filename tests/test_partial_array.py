483765eb10bc1cacbd71169bb3e9b742998764a8
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-08-23 15:19:10 +0100

    Extended the partial array tests

--- tests/test_partial_array.py
+++ tests/test_partial_array.py
@@ -1,8 +1,10 @@
 import unittest
 import numpy
 import random
+import math
 
 from amulet.api.partial_3d_array.util import get_sliced_array_size, sanitise_slice, unsanitise_slice
+from amulet.api.partial_3d_array import UnboundedPartial3DArray
 
 
 class PartialArrayTestCase(unittest.TestCase):
@@ -42,3 +44,30 @@ class PartialArrayTestCase(unittest.TestCase):
                     arr[slice(start3, stop3, step3)]
                 )
             )
+
+    def test_array(self):
+        class Partial3DArray16(UnboundedPartial3DArray):
+            def __init__(self):
+                super().__init__(
+                    numpy.uint32,
+                    0,
+                    (16, 16, 16),
+                    (0, 16)
+                )
+
+        partial = Partial3DArray16()
+        self.assertEqual(partial.shape, (16, math.inf, 16))
+        self.assertEqual(partial[:, :, :].shape, (16, 256, 16))
+        self.assertEqual(partial[:, -100:500, :].shape, (16, 600, 16))
+        self.assertEqual(partial[0, 0, 0], 0)
+        self.assertEqual(partial[:, 0, :].shape, (16, 1, 16))
+        self.assertEqual(partial[0, :, 0].shape, (1, 256, 1))
+
+        # partial[:, :16, :] = 1
+
+        # print(partial[:, 0, :].shape)
+
+        # partial[:, 17, :] = partial[:, 0, :]
+
+        # print(partial.get_section(0))
+        # print(partial.get_section(1))
