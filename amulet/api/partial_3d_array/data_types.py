7b73655d058614fd126405591ae29e12a383de32
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-08-30 17:13:16 +0100

    Added some more data types

--- amulet/api/partial_3d_array/data_types.py
+++ amulet/api/partial_3d_array/data_types.py
@@ -2,6 +2,8 @@ from typing import Union, Tuple
 import numpy
 
 IntSlicesType = Tuple[int, int, int]
+UnpackedSliceType = Tuple[int, int, int]
+UnpackedSlicesType = Tuple[UnpackedSliceType, UnpackedSliceType, UnpackedSliceType]
 SliceSlicesType = Tuple[slice, slice, slice]
 SingleFlexibleSliceType = Union[int, numpy.integer, slice]
 FlexibleSlicesType = Tuple[
