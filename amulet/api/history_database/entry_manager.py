d25d5ea761b199e0856e5862ac83386a3696ab77
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-09-24 12:32:12 +0100

    renamed entry to entry_manager and cleaned up a bit

--- amulet/api/history_database/entry_manager.py
+++ amulet/api/history_database/entry_manager.py
@@ -1,13 +1,15 @@
-from typing import List, Optional, Any
+from typing import List
+
+from .data_types import EntryType
 
 
-class BaseEntry:
-    """A class to define the base API for an entry.
+class BaseEntryManager:
+    """A class to define the base API for an entry manager.
     An entry in this case is any object that can change and needs its change history tracking."""
     __slots__ = ("_revisions", "_current_revision_index", "_saved_revision_index")
 
-    def __init__(self, initial_state: Optional[Any]):
-        self._revisions: List[Optional[Any]] = []  # the data for each revision
+    def __init__(self, initial_state: EntryType):
+        self._revisions: List[EntryType] = []  # the data for each revision
         self._current_revision_index: int = 0  # the index into the above for the current data
         self._saved_revision_index: int = 0  # the index into the above for the saved version
         self._store_entry(initial_state)
@@ -17,7 +19,7 @@ class BaseEntry:
         """Has this entry changed since the last save."""
         return self._current_revision_index != self._saved_revision_index
 
-    def put_new_entry(self, entry: Optional[Any]):
+    def put_new_entry(self, entry: EntryType):
         """Add a new entry to the database and increment the index."""
         if len(self._revisions) > self._current_revision_index + 1:
             # if there are upstream revisions delete them
@@ -25,7 +27,7 @@ class BaseEntry:
         self._store_entry(entry)
         self._current_revision_index += 1
 
-    def _store_entry(self, entry: Optional[Any]):
+    def _store_entry(self, entry: EntryType):
         """Store the entry data as required."""
         raise NotImplementedError
 
@@ -49,11 +51,25 @@ class BaseEntry:
         """Let the class know that the current revision has been saved."""
         self._saved_revision_index = self._current_revision_index
 
+    @property
+    def is_deleted(self) -> bool:
+        return self._revisions[self._current_revision_index] is None
 
-class RAMEntry(BaseEntry):
-    """A class to hold data about an entry in RAM."""
-    def _store_entry(self, entry: Optional[Any]):
+
+class RAMEntry(BaseEntryManager):
+    """A class to hold data about an entries history in RAM."""
+    def _store_entry(self, entry: EntryType):
         self._revisions.append(entry)
 
     def get_current_entry(self):
         return self._revisions[self._current_revision_index]
+
+
+class DiskEntry(BaseEntryManager):
+    """A class to hold data about an entries history on disk.
+    Revision indexes are still stored in RAM."""
+    def _store_entry(self, entry: EntryType):
+        raise NotImplementedError
+
+    def get_current_entry(self):
+        raise NotImplementedError
