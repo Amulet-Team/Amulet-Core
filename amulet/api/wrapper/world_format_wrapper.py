3fcc1813ac864f4295cf399067af6db7506d591f
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-10-10 14:38:25 +0100

    Added the not implemented methods to world_format_wrapper

--- amulet/api/wrapper/world_format_wrapper.py
+++ amulet/api/wrapper/world_format_wrapper.py
@@ -1,7 +1,9 @@
 import os
 import warnings
+from typing import Any, Generator, Optional, List, Tuple
 
-from amulet import log, IMG_DIRECTORY
+from amulet import IMG_DIRECTORY, ChunkCoordinates, SelectionGroup
+from amulet.api.data_types import Dimension, PlatformType, VersionNumberAny
 from .format_wrapper import FormatWrapper
 
 missing_world_icon = os.path.abspath(
@@ -48,8 +50,53 @@ class WorldFormatWrapper(FormatWrapper):
         """The path to the world icon"""
         return self._world_image_path
 
+    @staticmethod
+    def is_valid(path: str) -> bool:
+        raise NotImplementedError
+
+    @property
+    def valid_formats(self) -> Tuple[Tuple[PlatformType, bool, bool], ...]:
+        raise NotImplementedError
+
     @property
     def can_add_dimension(self) -> bool:
         """Can external code register a new dimension.
         If False register_dimension will have no effect."""
         return True
+
+    @property
+    def dimensions(self) -> List[Dimension]:
+        raise NotImplementedError
+
+    def register_dimension(self, dimension_internal: Any, dimension_name: Dimension):
+        raise NotImplementedError
+
+    def _get_interface_key(self, raw_chunk_data) -> Any:
+        raise NotImplementedError
+
+    def _create_and_open(self, platform: PlatformType, version: VersionNumberAny, selection: Optional[SelectionGroup] = None):
+        raise NotImplementedError
+
+    def _open(self):
+        raise NotImplementedError
+
+    def _save(self):
+        raise NotImplementedError
+
+    def _close(self):
+        raise NotImplementedError
+
+    def unload(self):
+        raise NotImplementedError
+
+    def all_chunk_coords(self, dimension: Dimension) -> Generator[ChunkCoordinates, None, None]:
+        raise NotImplementedError
+
+    def _delete_chunk(self, cx: int, cz: int, dimension: Dimension):
+        raise NotImplementedError
+
+    def _put_raw_chunk_data(self, cx: int, cz: int, data: Any, dimension: Dimension):
+        raise NotImplementedError
+
+    def _get_raw_chunk_data(self, cx: int, cz: int, dimension: Dimension) -> Any:
+        raise NotImplementedError
