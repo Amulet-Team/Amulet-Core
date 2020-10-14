4890ca8e6d1e0e637c2b5b61bfa2cc8bd3af041b
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-10-14 09:58:04 +0100

    Added a structure class to world
    This will help differentiate structures from worlds

---
+++ amulet/api/world/structure.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from .chunk_world import ChunkWorld
+
+if TYPE_CHECKING:
+    from amulet.api.wrapper import StructureFormatWrapper
+
+
+class Structure(ChunkWorld):
+    """
+    Class that handles editing of any structure format via an separate and flexible data format
+    """
+
+    def __init__(
+        self, directory: str, structure_wrapper: "StructureFormatWrapper", temp_dir: str = None
+    ):
+        super().__init__(directory, structure_wrapper, temp_dir)
