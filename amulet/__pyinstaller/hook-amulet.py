668062d840abb925dcc31ddec3ca63b19c7330bc
gentlegiantJGC <james_clare1@yahoo.co.uk>
2021-02-24 10:06:27 +0000

    Reformatted

--- amulet/__pyinstaller/hook-amulet.py
+++ amulet/__pyinstaller/hook-amulet.py
@@ -9,15 +9,21 @@ from amulet import operations
 
 AMULET_PATH = amulet.__path__[0]
 
-hiddenimports = [
-    name for _, name, _ in pkgutil.walk_packages(level.__path__, level.__name__ + ".")
-] + [
-    name for _, name, _ in pkgutil.walk_packages(operations.__path__, operations.__name__ + ".")
-] + [
-    "amulet.api.structure"
-]
+hiddenimports = (
+    [name for _, name, _ in pkgutil.walk_packages(level.__path__, level.__name__ + ".")]
+    + [
+        name
+        for _, name, _ in pkgutil.walk_packages(
+            operations.__path__, operations.__name__ + "."
+        )
+    ]
+    + ["amulet.api.structure"]
+)
 
-datas = collect_data_files("PyMCTranslate", excludes=["__pyinstaller", "json", "*.pyc", "*.py", "*.dll", "*.so", "*.dylib"])
+datas = collect_data_files(
+    "PyMCTranslate",
+    excludes=["__pyinstaller", "json", "*.pyc", "*.py", "*.dll", "*.so", "*.dylib"],
+)
 
 if sys.platform == "linux":
     binaries = [
