26098f08f0a5239a945f906002efdeba9cf17293
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2021-04-25 11:24:13 +0100

    Cleaned up entity list code and typing

--- amulet/api/chunk/entity_list.py
+++ amulet/api/chunk/entity_list.py
@@ -1,15 +1,15 @@
 from collections import UserList
-from typing import Iterable, Generator
-import copy
+from typing import Iterable
 from amulet.api.entity import Entity
 
 
 class ChunkList(UserList):
     def __init__(self, iterable: Iterable = ()):
-        super(ChunkList, self).__init__(list(iterable))
+        super().__init__(list(iterable))
 
     def _check_all_types(self, value):
-        [self._check_type(val) for val in value]
+        for val in value:
+            self._check_type(val)
 
     def _check_type(self, value):
         raise NotImplementedError
@@ -20,30 +20,39 @@ class ChunkList(UserList):
         super().append(value)
 
     def copy(self) -> "ChunkList":
-        return copy.deepcopy(self)
+        raise NotImplementedError
 
-    def extend(self, iterable: Iterable[Entity]) -> None:
         """ Extend list by appending elements from the iterable. """
+    def extend(self, iterable: Iterable):
         self._check_all_types(iterable)
         super().extend(iterable)
 
-    def insert(self, index: int, value) -> None:
         """ Insert value before index. """
+    def insert(self, index: int, value):
         self._check_type(value)
         super().insert(index, value)
 
     def __repr__(self):
         if not self:
             return "[]"
-        return "".join(f"\n\t{str(obj)}" for obj in self) + "\n"
+        return "[" + "".join(f"\n\t{str(obj)}" for obj in self) + "\n]"
 
 
 class EntityList(ChunkList):
-    def _check_type(self, value):
-        assert isinstance(value, Entity)
 
-    def __iter__(self) -> Generator[Entity, None, None]:
-        yield from super().__iter__()
+    def __init__(self, entities: Iterable[Entity] = ()):
+        for e in entities:
+            self._check_type(e)
+        super().__init__(entities)
+
+    def _check_type(self, value):
+        assert isinstance(value, Entity), f"{value} is not an instance of Entity"
+
+    def copy(self) -> "EntityList":
+        return EntityList(self)
+
+    def __iter__(self) -> Iterable[Entity]:
+        yield from self.data
 
     def __repr__(self) -> str:
         """ Return repr(self). """
