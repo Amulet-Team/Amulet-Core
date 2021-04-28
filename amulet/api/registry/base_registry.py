d1d9a2b369b1f907cf4f1dec3fddacbb6e4b3c8a
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2021-04-28 15:46:35 +0100

    Made the base registry abstract

--- amulet/api/registry/base_registry.py
+++ amulet/api/registry/base_registry.py
@@ -1,26 +1,34 @@
+from abc import ABC, abstractmethod
 from typing import Union, Tuple, Generator, Iterable, Any
 
 BaseType = Any
 
 
-class BaseRegistry:
+class BaseRegistry(ABC):
+    @abstractmethod
     def __len__(self):
         raise NotImplementedError
 
+    @abstractmethod
     def __contains__(self, item: Union[int, BaseType]):
         raise NotImplementedError
 
+    @abstractmethod
     def __iter__(self) -> Iterable[BaseType]:
         raise NotImplementedError
 
+    @abstractmethod
     def values(self) -> Tuple[BaseType]:
         raise NotImplementedError
 
+    @abstractmethod
     def items(self) -> Generator[Tuple[int, BaseType], None, None]:
         raise NotImplementedError
 
+    @abstractmethod
     def __getitem__(self, item):
         raise NotImplementedError
 
+    @abstractmethod
     def register(self, item: BaseType) -> int:
         raise NotImplementedError
