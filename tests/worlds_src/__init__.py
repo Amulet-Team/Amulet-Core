1bf5ea4bad966b7958393f556d1e98f902105472
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2021-05-26 14:54:14 +0100

    Added a modded 1.12 world

--- tests/worlds_src/__init__.py
+++ tests/worlds_src/__init__.py
@@ -5,3 +5,5 @@ bedrock_vanilla_1_16 = "bedrock_vanilla_1_16"
 java_vanilla_1_13 = "java_vanilla_1_13"
 java_vanilla_1_12_2 = "java_vanilla_1_12_2"
 
+# java forge worlds
+java_forge_1_12_2_twilight_forest = "java_forge_1_12_2_twilight_forest"
