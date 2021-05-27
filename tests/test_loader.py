2cc8c8ec9093b0d016f89c76a7214702406d9415
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2021-05-27 11:03:58 +0100

    Reformatted

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -11,19 +11,27 @@ from . import worlds_src
 class DefinitionBasedLoaderTestCase(unittest.TestCase):
     @unittest.skip
     def test_identify(self):
-        numerical_java_format = load_format(get_world_path(worlds_src.java_vanilla_1_12_2))
+        numerical_java_format = load_format(
+            get_world_path(worlds_src.java_vanilla_1_12_2)
+        )
         self.assertIsInstance(numerical_java_format, AnvilFormat)
 
-        blockstate_java_format = load_format(get_world_path(worlds_src.java_vanilla_1_13))
+        blockstate_java_format = load_format(
+            get_world_path(worlds_src.java_vanilla_1_13)
+        )
         self.assertIsInstance(blockstate_java_format, AnvilFormat)
 
     def test_loading(self):
-        numerical_java_world = load_level(create_temp_world(worlds_src.java_vanilla_1_12_2))
+        numerical_java_world = load_level(
+            create_temp_world(worlds_src.java_vanilla_1_12_2)
+        )
         self.assertIsInstance(numerical_java_world, World)
         numerical_java_world.close()
         clean_temp_world(worlds_src.java_vanilla_1_12_2)
 
-        blocksate_java_world = load_level(create_temp_world(worlds_src.java_vanilla_1_13))
+        blocksate_java_world = load_level(
+            create_temp_world(worlds_src.java_vanilla_1_13)
+        )
         self.assertIsInstance(blocksate_java_world, World)
         blocksate_java_world.close()
         clean_temp_world(worlds_src.java_vanilla_1_13)
