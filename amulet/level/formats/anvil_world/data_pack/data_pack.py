e688909b2f8738cf6a43a9228a4f3297641bd32e
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2021-06-02 14:30:06 +0100

    Added the first version of the data pack loading logic

--- amulet/level/formats/anvil_world/data_pack/data_pack.py
+++ amulet/level/formats/anvil_world/data_pack/data_pack.py
@@ -1,9 +1,9 @@
 from abc import ABC, abstractmethod
-from typing import Iterable
+from typing import Iterable, BinaryIO
 import os
 import json
 from zipfile import ZipFile
-import io
+import re
 
 
 class BaseWrapper(ABC):
@@ -34,6 +34,16 @@ class BaseWrapper(ABC):
         """
         raise NotImplementedError
 
+    def all_files_match(self, match: str) -> Iterable[str]:
+        """
+        The relative paths of all files contained within that match the given regex.
+
+        :param match: The regex string to match again.
+        :return: An iterable of file paths that match the given regex.
+        """
+        re_match = re.compile(match)
+        return tuple(path for path in self.all_files if re_match.fullmatch(path))
+
     @abstractmethod
     def has_file(self, relative_path: str) -> bool:
         """
@@ -45,7 +55,7 @@ class BaseWrapper(ABC):
         raise NotImplementedError
 
     @abstractmethod
-    def open(self, relative_path: str, mode="rb", **kwargs) -> io.BytesIO:
+    def open(self, relative_path: str, **kwargs) -> BinaryIO:
         """
         Get the contents of the file.
 
@@ -107,8 +117,8 @@ class DirWrapper(BaseWrapper):
     def has_file(self, relative_path: str) -> bool:
         return os.path.isfile(os.path.join(self.path, relative_path))
 
-    def open(self, relative_path: str, mode="rb", **kwargs) -> io.BytesIO:
-        return open(os.path.join(self.path, relative_path), mode, **kwargs)
+    def open(self, relative_path: str, **kwargs) -> BinaryIO:
+        return open(os.path.join(self.path, relative_path), "rb", **kwargs)
 
     def close(self):
         pass
@@ -118,15 +128,24 @@ def _open_wrapper(path: str) ->BaseWrapper:
     for cls in (ZipWrapper, DirWrapper):
         if cls.is_valid(path):
             return cls(path)
-    raise ValueError(f"The given path {path} is not valid.")
+    raise FileNotFoundError(f"The given path {path} is not valid.")
 
 
 class DataPack:
+    """The DataPack class wraps a single data pack."""
+
     def __init__(self, path: str):
-        assert self.is_valid(path), "The given path is not a valid data pack."
+        self._path = path
+        self._wrapper = _open_wrapper(path)
+        self._is_valid = self.is_wrapper_valid(self._wrapper)
+
+    @property
+    def path(self) -> str:
+        """The path to the data."""
+        return self._path
 
     @staticmethod
-    def is_valid(path: str):
+    def is_path_valid(path: str):
         """
         Check if the given path is a valid data pack.
 
@@ -134,6 +153,16 @@ class DataPack:
         :return: True if the path is a valid data pack, False otherwise.
         """
         wrapper = _open_wrapper(path)
+        is_valid = DataPack.is_wrapper_valid(wrapper)
+        wrapper.close()
+        return is_valid
+
+    @property
+    def is_valid(self) -> bool:
+        return self._is_valid
+
+    @staticmethod
+    def is_wrapper_valid(wrapper: BaseWrapper):
         if wrapper.has_file("pack.mcmeta"):
             with wrapper.open("pack.mcmeta") as m:
                 try:
@@ -143,7 +172,46 @@ class DataPack:
                 else:
                     if isinstance(meta_file, dict) and isinstance(meta_file.get("pack", {}).get("pack_format", None), int):
                         # TODO: check the actual value
-                        wrapper.close()
                         return True
-        wrapper.close()
         return False
+
+    @property
+    def all_files(self) -> Iterable[str]:
+        """
+        The relative paths of all files contained within.
+
+        :return: An iterable of paths.
+        """
+        return self._wrapper.all_files
+
+    def all_files_match(self, match: str) -> Iterable[str]:
+        """
+        The relative paths of all files contained within that match the given regex.
+
+        :param match: The regex string to match again.
+        :return: An iterable of file paths that match the given regex.
+        """
+        return self._wrapper.all_files_match(match)
+
+    def has_file(self, relative_path: str) -> bool:
+        """
+        Does the requested file exist.
+
+        :param relative_path:
+        :return:
+        """
+        return self._wrapper.has_file(relative_path)
+
+    def open(self, relative_path: str) -> BinaryIO:
+        """
+        Get the contents of the file.
+
+        :param relative_path:
+        :return:
+        """
+        return self._wrapper.open(relative_path)
+
+    @abstractmethod
+    def close(self):
+        """Close the contents."""
+        self._wrapper.close()
