ba971c5a747839795750962c89fc15140078aede
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2022-01-23 14:36:06 +0000

    Added a CLI controlled toggle for temporary entity support
    This is False by default but if enabled will allow entities to be stored to the temporary location in the chunk

--- amulet/__init__.py
+++ amulet/__init__.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 
 from ._version import get_versions
 
@@ -7,6 +8,20 @@ del get_versions
 
 
 entity_support = False
+temp_entity_support = False
+
+
+def _parse_args():
+    global temp_entity_support
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--temp-entity-support", dest="temp_entity_support", action="store_true")
+    args = parser.parse_known_args()
+    temp_entity_support = args.temp_entity_support
+
+
+_parse_args()
+
+
 IMG_DIRECTORY = os.path.join(os.path.dirname(__file__), "img")
 
 from .api import *
