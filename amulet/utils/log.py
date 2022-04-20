1c77e9f0680693e2b852771e15285e9ecf9507ed
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2022-04-20 10:24:05 +0100

    Modified the logger setup
    The default handler will only be added if no handler has been bound yet.
    Removed the file handler

--- amulet/utils/log.py
+++ amulet/utils/log.py
@@ -1,41 +1,33 @@
 import logging
 import sys
-import os
+
+"""
+By default the logger only logs to the console.
+If you wish to modify the handlers you should
+1) Get the logger
+2) Remove and close all existing handlers
+3) Set up the logger how you desire
+
+Step 2 may not be required if you set up the logger before this module is imported
+but there is no harm in doing it and if you don't everything may get logged to the console twice.
+"""
+
 
 log = logging.getLogger("amulet")
 
 
 def _init_logging():
-    log.setLevel(logging.DEBUG if "amulet-debug" in sys.argv else logging.INFO)
+    if not log.handlers:
+        # if no handlers have been bound then set up a default one
+        log.setLevel(logging.DEBUG if "amulet-debug" in sys.argv else logging.INFO)
 
-    formatter = logging.Formatter(
-        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-    )
+        formatter = logging.Formatter(
+            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        )
 
-    # TODO: remove the file logger from here so that the application has to set it up
-    os.makedirs("./logs", exist_ok=True)
-    file_handler = logging.FileHandler("./logs/amulet_core.log", "w", encoding="utf-8")
-    file_handler.setFormatter(formatter)
-
-    console_handler = logging.StreamHandler()
-    console_handler.setFormatter(formatter)
-
-    log.addHandler(file_handler)
-    log.addHandler(console_handler)
+        console_handler = logging.StreamHandler()
+        console_handler.setFormatter(formatter)
+        log.addHandler(console_handler)
 
 
 _init_logging()
-
-
-def factory_reset():
-    """
-    Disable the handlers implemented here and reset to the factory state.
-    This exists so that calling code can implement the handlers as desired.
-    It is suggested that libraries should not implement their own handlers however this library can be
-    used on its own so having a console handler set up by default is beneficial in that case.
-    """
-    while log.hasHandlers():
-        handler = log.handlers[0]
-        log.removeHandler(handler)
-        handler.close()
-    log.setLevel(logging.NOTSET)
