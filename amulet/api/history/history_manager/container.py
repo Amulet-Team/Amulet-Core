ac446749f5584930a3cba60e3e49254650955a28
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2022-05-31 13:59:03 +0100

    Improved thread safety in history container

--- amulet/api/history/history_manager/container.py
+++ amulet/api/history/history_manager/container.py
@@ -1,5 +1,6 @@
 from abc import abstractmethod
 from typing import List, Generic, TypeVar
+import threading
 
 from amulet.api.history.base.history_manager import AbstractHistoryManager
 
@@ -15,12 +16,14 @@ class AbstractContainerHistoryManager(AbstractHistoryManager, Generic[SnapshotT]
     _last_save_snapshot: int
     # if the user saves, undoes and does a new operation a save branch will be lost. This is the number of changes on that branch
     _branch_save_count: int
+    _lock: threading.RLock
 
     def __init__(self):
         self._snapshots = []
         self._snapshot_index = -1
         self._last_save_snapshot = -1
         self._branch_save_count = 0
+        self._lock = threading.RLock()
 
     @abstractmethod
     def _check_snapshot(self, snapshot: SnapshotT):
@@ -32,27 +35,29 @@ class AbstractContainerHistoryManager(AbstractHistoryManager, Generic[SnapshotT]
         :param snapshot: The snapshot data to save.
         :return: True if the snapshot was created, false otherwise.
         """
-        self._check_snapshot(snapshot)
-        if snapshot:
-            if self._last_save_snapshot > self._snapshot_index:
-                # if the user has undone changes and made more changes things get a bit messy
-                # This fixes the property storing the number of changes since the last save.
-                self._branch_save_count += (
-                    self._last_save_snapshot - self._snapshot_index
-                )
-                self._last_save_snapshot = self._snapshot_index
-            self._snapshot_index += 1
-            # delete all upstream snapshots
-            del self._snapshots[self._snapshot_index :]
-            self._snapshots.append(snapshot)
-            return True
-        return False
+        with self._lock:
+            self._check_snapshot(snapshot)
+            if snapshot:
+                if self._last_save_snapshot > self._snapshot_index:
+                    # if the user has undone changes and made more changes things get a bit messy
+                    # This fixes the property storing the number of changes since the last save.
+                    self._branch_save_count += (
+                        self._last_save_snapshot - self._snapshot_index
+                    )
+                    self._last_save_snapshot = self._snapshot_index
+                self._snapshot_index += 1
+                # delete all upstream snapshots
+                del self._snapshots[self._snapshot_index :]
+                self._snapshots.append(snapshot)
+                return True
+            return False
 
     def mark_saved(self):
         """Let the class know that the current state has been saved."""
-        self._last_save_snapshot = self._snapshot_index
-        self._branch_save_count = 0
-        self._mark_saved()
+        with self._lock:
+            self._last_save_snapshot = self._snapshot_index
+            self._branch_save_count = 0
+            self._mark_saved()
 
     @abstractmethod
     def _mark_saved(self):
@@ -76,10 +81,11 @@ class AbstractContainerHistoryManager(AbstractHistoryManager, Generic[SnapshotT]
 
     def undo(self):
         """Undoes the last set of changes to the database"""
-        if self.undo_count > 0:
-            snapshot = self._snapshots[self._snapshot_index]
-            self._undo(snapshot)
-            self._snapshot_index -= 1
+        with self._lock:
+            if self.undo_count > 0:
+                snapshot = self._snapshots[self._snapshot_index]
+                self._undo(snapshot)
+                self._snapshot_index -= 1
 
     @abstractmethod
     def _undo(self, snapshot: SnapshotT):
@@ -87,10 +93,11 @@ class AbstractContainerHistoryManager(AbstractHistoryManager, Generic[SnapshotT]
 
     def redo(self):
         """Redoes the last set of changes to the database"""
-        if self.redo_count > 0:
-            self._snapshot_index += 1
-            snapshot = self._snapshots[self._snapshot_index]
-            self._redo(snapshot)
+        with self._lock:
+            if self.redo_count > 0:
+                self._snapshot_index += 1
+                snapshot = self._snapshots[self._snapshot_index]
+                self._redo(snapshot)
 
     @abstractmethod
     def _redo(self, snapshot: SnapshotT):
@@ -102,7 +109,8 @@ class AbstractContainerHistoryManager(AbstractHistoryManager, Generic[SnapshotT]
 
     def purge(self):
         """Unload all cached data. Effectively returns the class to its starting state."""
-        self._snapshots.clear()
-        self._snapshot_index: int = -1
-        self._last_save_snapshot = -1
-        self._branch_save_count = 0
+        with self._lock:
+            self._snapshots.clear()
+            self._snapshot_index: int = -1
+            self._last_save_snapshot = -1
+            self._branch_save_count = 0
