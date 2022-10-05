42a220839ad5d33305f5e26791cb33c4808f49a1
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2022-10-05 12:19:26 +0100

    Reformatted

--- amulet/libs/leveldb/__init__.py
+++ amulet/libs/leveldb/__init__.py
@@ -1,3 +1,7 @@
 import warnings
-warnings.warn("`amulet.libs.leveldb` is depreciated and will be removed in the future. It is replaced by the `leveldb` package.", DeprecationWarning)
+
+warnings.warn(
+    "`amulet.libs.leveldb` is depreciated and will be removed in the future. It is replaced by the `leveldb` package.",
+    DeprecationWarning,
+)
 from leveldb import LevelDB, LevelDBException, LevelDBEncrypted
