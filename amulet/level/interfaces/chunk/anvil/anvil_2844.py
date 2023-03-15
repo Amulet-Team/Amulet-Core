9c41a2bda1e20dcdb101481b5e37cd51862e8d99
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2023-03-15 11:32:21 +0000

    Added support for 1.19.4

--- amulet/level/interfaces/chunk/anvil/anvil_2844.py
+++ amulet/level/interfaces/chunk/anvil/anvil_2844.py
@@ -77,7 +77,7 @@ class Anvil2844Interface(ParentInterface):
 
     @staticmethod
     def minor_is_valid(key: int):
-        return 2844 <= key < 3219
+        return 2844 <= key < 3338
 
     def _get_floor_cy(self, data: ChunkDataType):
         return self.get_layer_obj(data, self.yPos, pop_last=True).py_int
