bb7757d91ffd361ef6c89a7736894f0a7804ddff
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2023-06-15 11:37:36 +0100

    Update Java status format
    We should probably re-evaluate how we are storing this data.

---
+++ amulet/level/interfaces/chunk/anvil/anvil_3463.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+from .anvil_2844 import (
+    Anvil2844Interface as ParentInterface,
+)
+from amulet.api.chunk import StatusFormats
+
+
+class Anvil3463Interface(ParentInterface):
+    def __init__(self):
+        super().__init__()
+        self._set_feature("status", StatusFormats.Java_20)
+
+    @staticmethod
+    def minor_is_valid(key: int):
+        return 3454 <= key < 3480
+
+
+export = Anvil3463Interface
