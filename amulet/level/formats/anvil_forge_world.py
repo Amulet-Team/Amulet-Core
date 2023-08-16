f4c91da04f24335eec31f1742d0e99f699b65c97
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2023-08-16 09:52:13 +0100

    Reformatted

--- amulet/level/formats/anvil_forge_world.py
+++ amulet/level/formats/anvil_forge_world.py
@@ -42,7 +42,9 @@ class AnvilForgeFormat(AnvilFormat):
 
     def _register_modded_dimensions(self):
         for region_path in glob.glob(
-            os.path.join(glob.escape(self.path), "dimensions", "*", "*", "**", "region"),
+            os.path.join(
+                glob.escape(self.path), "dimensions", "*", "*", "**", "region"
+            ),
             recursive=True,
         ):
             if not os.path.isdir(region_path):
