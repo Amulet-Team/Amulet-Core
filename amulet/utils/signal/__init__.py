dcabe771329c74339bd9d6cdeaa90876173564f3
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2023-09-24 14:27:35 +0100

    Converted signal module to a package
    This should better hide the private variables

---
+++ amulet/utils/signal/__init__.py
@@ -0,0 +1,7 @@
+from ._signal import (
+    Signal,
+    SignalInstanceConstructor,
+    set_signal_instance_constructor,
+    get_fallback_signal_instance_constructor,
+    get_pyside6_signal_instance_constructor,
+)
