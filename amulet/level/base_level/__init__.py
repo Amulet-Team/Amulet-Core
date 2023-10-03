8e2e3b25dbd5fc4bb3cc7210d76fc1024b9cd689
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2023-10-03 15:08:25 +0100

    Added __init__.py imports

---
+++ amulet/level/base_level/__init__.py
@@ -0,0 +1,15 @@
+from ._base_level import (
+    BaseLevel,
+    BaseLevelPrivate,
+    LevelT,
+    LevelDataT,
+    LevelNamespace,
+    ReadonlyMetadataNamespace,
+    MetadataNamespace,
+    RawNamespace,
+    ChunkNamespace,
+    PlayerNamespace,
+)
+from ._creatable_level import CreatableLevel
+from ._loadable_level import LoadableLevel
+from ._disk_level import DiskLevel
