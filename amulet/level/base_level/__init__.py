164184053759afb61763608f86851b055db1f938
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2023-10-05 13:59:23 +0100

    Added a method to find create arguments
    This is useful to populate a GUI without being aware of the level implementation.

--- amulet/level/base_level/__init__.py
+++ amulet/level/base_level/__init__.py
@@ -5,6 +5,17 @@ from ._base_level_namespaces.metadata import MetadataNamespace
 from ._base_level_namespaces.player import PlayerNamespace
 from ._base_level_namespaces.raw import RawNamespace
 from ._base_level_namespaces.readonly_metadata import ReadonlyMetadataNamespace
-from ._creatable_level import CreatableLevel
+from ._creatable_level import (
+    CreatableLevel,
+    StringArg,
+    BytesArg,
+    BoolArg,
+    IntArg,
+    FloatArg,
+    SequenceArg,
+    UnionArg,
+    ProtocolArg,
+    CreateArgsT,
+)
 from ._loadable_level import LoadableLevel
 from ._disk_level import DiskLevel
