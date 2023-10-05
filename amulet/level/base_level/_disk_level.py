c45d0075ddd2cf8d4cbfbd10aeb1a870427b12b8
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2023-10-05 14:00:03 +0100

    Added path property to disk level

--- amulet/level/base_level/_disk_level.py
+++ amulet/level/base_level/_disk_level.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from abc import abstractmethod
+
 from ._base_level import BaseLevel
 
 
@@ -7,3 +9,9 @@ class DiskLevel(BaseLevel):
     """A level base class for all levels with data on disk."""
 
     __slots__ = ()
+
+    @property
+    @abstractmethod
+    def path(self) -> str:
+        """The path to the level on disk."""
+        raise NotImplementedError
