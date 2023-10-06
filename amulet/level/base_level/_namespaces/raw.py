e479b38290dfdd442654b4f2f1a96963b37fae87
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2023-10-06 11:21:18 +0100

    Added methods to raw namespace

--- amulet/level/base_level/_namespaces/raw.py
+++ amulet/level/base_level/_namespaces/raw.py
@@ -1,7 +1,121 @@
 from __future__ import annotations
 
-from .namespace import LevelNamespace
+from typing import Iterable, Any, TypeVar, Generic
+from abc import ABC, abstractmethod
+
+from amulet.api.data_types import Dimension, ChunkCoordinates
+from amulet.api.chunk import Chunk
+
+from .namespace import LevelNamespace, LevelT, LevelDataT
+
+RawChunkT = TypeVar("NativeChunkT")
+NativeChunkT = TypeVar("NativeChunkT")
+PlayerIDT = TypeVar("PlayerIDT")
+RawPlayerT = TypeVar("RawPlayerT")
 
 
-class RawNamespace(LevelNamespace):
+class RawNamespace(LevelNamespace[LevelT], ABC, Generic[LevelT, LevelDataT, RawChunkT, NativeChunkT, PlayerIDT, RawPlayerT]):
+    """
+    A class with raw access to the level.
+    All of these methods directly read from or write to the level.
+    There is no way to undo changes made with these methods.
+    """
+
     __slots__ = ()
+
+    @abstractmethod
+    def all_chunk_coords(self) -> Iterable[ChunkCoordinates]:
+        """Get an iterable of all the chunk coordinates that exist in the raw level data."""
+        raise NotImplementedError
+
+    @abstractmethod
+    def has_chunk(self, dimension: Dimension, cx: int, cz: int) -> bool:
+        """Check if the chunk exists in the raw level data."""
+        raise NotImplementedError
+
+    @abstractmethod
+    def delete_chunk(self, dimension: Dimension, cx: int, cz: int):
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_raw_chunk(self, dimension: Dimension, cx: int, cz: int) -> RawChunkT:
+        """
+        Get the chunk data in its raw format.
+        This is usually the exact data that exists on disk.
+        The raw chunk format varies between each level class.
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def set_raw_chunk(self, dimension: Dimension, cx: int, cz: int, chunk: RawChunkT):
+        """Set the chunk in its raw format."""
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_native_chunk(self, dimension: Dimension, cx: int, cz: int) -> NativeChunkT:
+        """
+        Get the raw chunk data loaded into an easier to use format.
+        Block, biome and other array data should be loaded into editable arrays.
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def set_native_chunk(self, dimension: Dimension, cx: int, cz: int, chunk: NativeChunkT):
+        """Set the chunk in its native format."""
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_universal_chunk(self, dimension: Dimension, cx: int, cz: int) -> Chunk:
+        """Get the chunk in the universal format."""
+
+    @abstractmethod
+    def set_universal_chunk(self, dimension: Dimension, cx: int, cz: int, chunk: Chunk):
+        """Set the chunk in the universal format."""
+        raise NotImplementedError
+
+    @abstractmethod
+    def raw_to_native_chunk(self, chunk: RawChunkT) -> NativeChunkT:
+        raise NotImplementedError
+
+    @abstractmethod
+    def native_to_raw_chunk(self, chunk: NativeChunkT) -> RawChunkT:
+        raise NotImplementedError
+
+    @abstractmethod
+    def native_to_universal_chunk(self, chunk: NativeChunkT) -> Chunk:
+        raise NotImplementedError
+
+    @abstractmethod
+    def universal_to_native_chunk(self, chunk: Chunk) -> NativeChunkT:
+        raise NotImplementedError
+
+    @abstractmethod
+    def all_player_ids(self) -> Iterable[PlayerIDT]:
+        raise NotImplementedError
+
+    @abstractmethod
+    def has_player(self, player_id: PlayerIDT) -> bool:
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_raw_player(self, player_id: PlayerIDT) -> RawPlayerT:
+        raise NotImplementedError
+
+    @abstractmethod
+    def set_raw_player(self, player_id: PlayerIDT, player: RawPlayerT):
+        raise NotImplementedError
+
+
+class BufferedRawNamespace(RawNamespace):
+    """
+    A class with raw access to the level.
+    All of these methods directly read from or write to the level.
+    There is no way to undo changes made with these methods.
+
+    This is a special case where the methods save to an in-memory buffer and :meth:`save` saves the data to the level.
+    This is used for structure levels where the whole level must be read and written in one go.
+    """
+
+    @abstractmethod
+    def save(self):
+        raise NotImplementedError
