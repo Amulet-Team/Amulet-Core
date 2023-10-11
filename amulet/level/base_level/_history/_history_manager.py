bae016f80cabf9c140b7a45e9dc1a37caf69c793
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2023-10-11 10:39:11 +0100

    Refactored history manager
    The history manager now tracks in memory if the resource exists so that the data does not need to be loaded to check if the data exists.

--- amulet/level/base_level/_history/_history_manager.py
+++ amulet/level/base_level/_history/_history_manager.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from uuid import uuid4
 from threading import Lock
-from typing import Sequence, Protocol, TypeVar, Generic
+from typing import Sequence, Protocol, TypeVar, Generic, Mapping
 
 from amulet.utils.signal import Signal
 
@@ -23,15 +23,17 @@ class ResourceId(Protocol):
 
 
 class Resource:
-    __slots__ = ("index", "saved_index", "global_index")
+    __slots__ = ("index", "saved_index", "global_index", "exists")
 
     def __init__(self):
         # The index of the currently active revision
-        self.index: int = -1
-        # The index of the saved revision. -2 if the index no longer exists (overwritten or destroyed future)
-        self.saved_index: int = -1
+        self.index: int = 0
+        # The index of the saved revision. -1 if the index no longer exists (overwritten or destroyed future)
+        self.saved_index: int = 0
         # The global history index
-        self.global_index: int = -1
+        self.global_index: int = 0
+        # Does the resource data exist
+        self.exists: list[bool] = [True]
 
     def get_resource_key(self, uuid: bytes, resource_id: ResourceId) -> bytes:
         return b"/".join((uuid, bytes(resource_id), str(self.index).encode()))
@@ -41,8 +43,8 @@ class HistoryManagerPrivate:
     def __init__(self):
         self.lock = Lock()
         self.resources: dict[bytes, dict[ResourceId, Resource]] = {}
-        self.history: list[set[Resource]] = []
-        self.history_index = -1
+        self.history: list[set[Resource]] = [set()]
+        self.history_index = 0
         self.has_redo = False
         self.cache = GlobalDiskCache.instance()
 
@@ -55,7 +57,7 @@ class HistoryManagerPrivate:
                 for resource in layer.values():
                     if resource.saved_index > resource.index:
                         # A future index is saved that just got invalidated
-                        resource.saved_index = -2
+                        resource.saved_index = -1
 
 
 ResourceIdT = TypeVar("ResourceIdT", bound=ResourceId)
@@ -93,12 +95,12 @@ class HistoryManager:
     def undo_count(self) -> int:
         """The number of times undo can be called."""
         with self._d.lock:
-            return self._d.history_index + 1
+            return self._d.history_index
 
     def undo(self):
         """Undo the changes in the current undo bin."""
         with self._d.lock:
-            if self._d.history_index < 0:
+            if self._d.history_index <= 0:
                 raise RuntimeError
             for resource in self._d.history[self._d.history_index]:
                 resource.index -= 1
@@ -126,73 +128,122 @@ class HistoryManager:
 
 
 class HistoryManagerLayer(Generic[ResourceIdT]):
-    __slots__ = ("_d", "_uuid")
+    __slots__ = ("_d", "_uuid", "_resources")
 
     def __init__(self, _d: HistoryManagerPrivate):
         self._d = _d
         self._uuid = uuid4().bytes
-        self._d.resources[self._uuid] = {}
+        self._resources: dict[ResourceIdT, Resource] = {}
+        self._d.resources[self._uuid] = self._resources
 
-    def has_resource(self, resource_id: ResourceIdT) -> bool:
+    def resources(self) -> Sequence[ResourceIdT]:
         """
-        Check if a resource entry exists.
-        :param resource_id: The identifier for the resource in the data group e.g. b"minecraft:overworld/10/50"
+        Get all resource ids from this layer.
         :return:
         """
         with self._d.lock:
-            return resource_id in self._d.resources[self._uuid]
+            return list(self._resources)
 
-    def get_resource(self, resource_id: ResourceIdT) -> bytes:
+    def changed_resources(self) -> Sequence[ResourceIdT]:
         """
-        Get the newest resource data.
-        :param resource_id: The identifier for the resource in the data group e.g. b"minecraft:overworld/10/50"
-        :return:
-        """
-        with self._d.lock:
-            resource = self._d.resources[self._uuid][resource_id]
-            return self._d.cache[resource.get_resource_key(self._uuid, resource_id)]
-
-    def set_initial_resource(self, resource_id: ResourceIdT, data: bytes):
-        """
-        Set the data for the resource. This must only be used if the resource does not already exist.
-        :param resource_id: The identifier for the resource in the data group e.g. b"minecraft:overworld/10/50"
-        :param data: The binary data to set.
-        :return:
-        """
-        with self._d.lock:
-            layer = self._d.resources[self._uuid]
-            if resource_id in layer:
-                raise RuntimeError("Resource already exists")
-            resource = layer[resource_id] = Resource()
-            self._d.cache[resource.get_resource_key(self._uuid, resource_id)] = data
-
-    def set_resource(self, resource_id: ResourceIdT, data: bytes):
-        """
-        Set the data for the resource.
-        :param resource_id: The identifier for the resource in the data group e.g. b"minecraft:overworld/10/50"
-        :param data: The binary data to set.
-        :return:
-        """
-        with self._d.lock:
-            self._d.invalidate_future()
-            resource = self._d.resources[self._uuid][resource_id]
-            if resource.global_index != self._d.history_index:
-                resource.index += 1
-                resource.global_index = self._d.history_index
-            if resource.index == resource.saved_index:
-                resource.saved_index = -2
-            self._d.cache[resource.get_resource_key(self._uuid, resource_id)] = data
-            if self._d.history_index != -1:
-                self._d.history[self._d.history_index].add(resource)
-
-    def get_changed_resource_ids(self) -> Sequence[ResourceIdT]:
-        """
-        Get resource ids in the data layer for all resources that have changed since the last call to mark_saved.
+        Get all resource ids from this layer that have changed since the last call to mark_saved.
         :return:
         """
         with self._d.lock:
             return [
                 resource_id
-                for resource_id, resource in self._d.resources[self._uuid]
+                for resource_id, resource in self._resources
                 if resource.saved_index != resource.index
             ]
+
+    def resources_exist(self) -> Mapping[ResourceIdT, bool]:
+        """
+        Get a mapping from the resource ids to a bool stating if the data exists for that resource.
+        If false that resource has been deleted.
+        """
+        with self._d.lock:
+            return {
+                resource_id: resource.exists[resource.index]
+                for resource_id, resource in self._resources
+            }
+
+    def has_resource(self, resource_id: ResourceIdT) -> bool:
+        """
+        Check if a resource entry exists.
+        If the resource has been loaded this will be True regardless if the resource data has been deleted.
+        Use :meth:`resource_exists` to check if the resource data has been deleted.
+        Calling :meth:`get_resource` or :meth:`set_resource` when this is False will error.
+        :param resource_id: The resource identifier
+        :return:
+        """
+        with self._d.lock:
+            return resource_id in self._resources
+
+    def resource_exists(self, resource_id: ResourceIdT) -> bool:
+        """
+        A fast way to check if the resource data exists without loading it.
+        :param resource_id: The resource identifier
+        :return: True if the data exists.
+        """
+        with self._d.lock:
+            resource = self._resources[resource_id]
+            return resource.exists[resource.index]
+
+    def get_resource(self, resource_id: ResourceIdT) -> bytes:
+        """
+        Get the newest resource data.
+        :param resource_id: The resource identifier
+        :return: The binary data that was previously set. An empty bytes object for the deleted state.
+        """
+        with self._d.lock:
+            resource = self._resources[resource_id]
+            if resource.exists[resource.index]:
+                return self._d.cache[resource.get_resource_key(self._uuid, resource_id)]
+            else:
+                return b""
+
+    def set_initial_resource(self, resource_id: ResourceIdT, data: bytes):
+        """
+        Set the data for the resource.
+        This can only be used if the resource does not already exist.
+        :param resource_id: The resource identifier
+        :param data: The binary data to set. An empty bytes object for the deleted state.
+        :return:
+        """
+        with self._d.lock:
+            if resource_id in self._resources:
+                raise RuntimeError("Resource already exists")
+            resource = self._resources[resource_id] = Resource()
+            if data:
+                # Save the data to the cache if it exists
+                self._d.cache[resource.get_resource_key(self._uuid, resource_id)] = data
+            # Store a flag if it exists
+            resource.exists[resource.index] = bool(data)
+
+    def set_resource(self, resource_id: ResourceIdT, data: bytes):
+        """
+        Set the data for the resource.
+        :param resource_id: The resource identifier
+        :param data: The binary data to set. An empty bytes object for the deleted state.
+        :return:
+        """
+        with self._d.lock:
+            self._d.invalidate_future()
+            resource = self._resources[resource_id]
+            if resource.global_index != self._d.history_index:
+                # The global history index has been increased since the last change to this resource
+                # Add a new state
+                resource.index += 1
+                resource.global_index = self._d.history_index
+                resource.exists.append(False)
+            if resource.index == resource.saved_index:
+                # The saved index has been directly modified
+                resource.saved_index = -1
+            if data:
+                # Save the data to the cache if it exists
+                self._d.cache[resource.get_resource_key(self._uuid, resource_id)] = data
+            # Store a flag if it exists
+            resource.exists[resource.index] = bool(data)
+            if self._d.history_index:
+                # Add the resource to the history bin if one has been created
+                self._d.history[self._d.history_index].add(resource)
