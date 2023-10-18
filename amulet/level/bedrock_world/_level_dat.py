f61c70b78726b28ff6962ecee4a0bf76de3727e2
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2023-10-18 10:10:54 +0100

    Stripped back the bedrock level.dat class

--- amulet/level/bedrock_world/_level_dat.py
+++ amulet/level/bedrock_world/_level_dat.py
@@ -1,8 +1,6 @@
 from __future__ import annotations
 
-import os
 import struct
-import warnings
 from typing import Tuple, Union, Optional, BinaryIO
 from io import BytesIO
 
@@ -19,43 +17,18 @@ from amulet.api.errors import (
 
 
 class BedrockLevelDAT(NamedTag):
-    _path: str
     _level_dat_version: int
 
-    def __init__(
-        self, tag=None, name: str = "", path: str = None, level_dat_version: int = None
-    ):
-        if isinstance(tag, str):
-            warnings.warn(
-                "You must use BedrockLevelDAT.from_file to load from a file.",
-                FutureWarning,
+    def __init__(self, tag=None, name: str = "", level_dat_version: int = None):
+        if not isinstance(level_dat_version, int):
+            raise TypeError(
+                "path and level_dat_version must be specified when constructing a BedrockLevelDAT instance."
             )
-            super().__init__()
-            self._path = path = tag
-            self._level_dat_version = 8
-            if os.path.isfile(path):
-                self.load_from(path)
-            return
-        else:
-            if not (isinstance(path, str) and isinstance(level_dat_version, int)):
-                raise TypeError(
-                    "path and level_dat_version must be specified when constructing a BedrockLevelDAT instance."
-                )
-            super().__init__(tag, name)
-            self._path = path
-            self._level_dat_version = level_dat_version
+        super().__init__(tag, name)
+        self._level_dat_version = level_dat_version
 
     @classmethod
     def from_file(cls, path: str):
-        level_dat_version, name, tag = cls._read_from(path)
-        return cls(tag, name, path, level_dat_version)
-
-    @property
-    def path(self) -> Optional[str]:
-        return self._path
-
-    @staticmethod
-    def _read_from(path: str) -> Tuple[int, str, AbstractBaseTag]:
         with open(path, "rb") as f:
             level_dat_version = struct.unpack("<i", f.read(4))[0]
             if 4 <= level_dat_version <= 10:
@@ -67,22 +40,13 @@ class BedrockLevelDAT(NamedTag):
                     string_decoder=utf8_escape_decoder,
                 )
                 name = root_tag.name
-                value = root_tag.tag
+                tag = root_tag.tag
             else:
                 # TODO: handle other versions
                 raise ObjectReadError(
                     f"Unsupported level.dat version {level_dat_version}"
                 )
-        return level_dat_version, name, value
-
-    def load_from(self, path: str):
-        self._level_dat_version, self.name, self.tag = self._read_from(path)
-
-    def reload(self):
-        self.load_from(self.path)
-
-    def save(self, path: str = None):
-        self.save_to(path or self._path)
+        return cls(tag, name, level_dat_version)
 
     def save_to(
         self,
