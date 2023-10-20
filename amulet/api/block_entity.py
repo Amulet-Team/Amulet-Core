af972bf5c6a2c24020322051db41dc6e90f80db0
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2023-10-20 11:48:02 +0100

    Refactored entity classes

--- amulet/api/block_entity.py
+++ amulet/api/block_entity.py
@@ -1,19 +1,23 @@
 from __future__ import annotations
 import copy
-import numpy
 
 from amulet_nbt import NamedTag
 from amulet.api.data_types import BlockCoordinates
-from .abstract_base_entity import AbstractBaseEntity
 
 
-class BlockEntity(AbstractBaseEntity):
+class BlockEntity:
     """
     A class to contain all the data to define a BlockEntity.
     """
 
-    obj_name = "BlockEntity"
-    coord_types = (int, numpy.integer)
+    __slots__ = (
+        "_namespace",
+        "_base_name",
+        "_x",
+        "_y",
+        "_z",
+        "_nbt",
+    )
 
     def __init__(
         self,
@@ -34,27 +38,17 @@ class BlockEntity(AbstractBaseEntity):
         :param z: The z coordinate of the block entity
         :param nbt: The NBT stored with the block entity
         """
-        super().__init__(namespace, base_name, x, y, z, nbt)
+        self._namespace = str(namespace)
+        self._base_name = str(base_name)
+        self._x = int(x)
+        self._y = int(y)
+        self._z = int(z)
+        if not isinstance(nbt, NamedTag):
+            raise TypeError(f"nbt must be an NamedTag. Got {nbt}")
+        self._nbt = nbt
 
-    @property
-    def x(self) -> int:
-        """The x location of the BlockEntity. Read Only"""
-        return self._x
-
-    @property
-    def y(self) -> int:
-        """The y location of the BlockEntity. Read Only"""
-        return self._y
-
-    @property
-    def z(self) -> int:
-        """The z location of the BlockEntity. Read Only"""
-        return self._z
-
-    @property
-    def location(self) -> BlockCoordinates:
-        """The location of the BlockEntity. Read Only"""
-        return self._x, self._y, self._z
+    def __repr__(self):
+        return f"BlockEntity({self._namespace!r}, {self._base_name!r}, {self._x}, {self._y}, {self._z}, {self._nbt!r})"
 
     def new_at_location(self, x: int, y: int, z: int) -> BlockEntity:
         """
@@ -69,3 +63,74 @@ class BlockEntity(AbstractBaseEntity):
             z,
             copy.deepcopy(self._nbt),
         )
+
+    @property
+    def namespaced_name(self) -> str:
+        """
+        The namespace:base_name of the entity represented by the object (eg: `minecraft:chest`)
+
+        If the given namespace is an empty string it will just return the base name.
+
+        :return: The namespace:base_name of the block entity
+        """
+        return f"{self._namespace}:{self._base_name}"
+
+    @property
+    def namespace(self) -> str:
+        """
+        The namespace of the block entity represented by the BlockEntity object (eg: `minecraft`)
+
+        :return: The namespace of the block entity
+        """
+        return self._namespace
+
+    @namespace.setter
+    def namespace(self, value: str):
+        self._namespace = value
+
+    @property
+    def base_name(self) -> str:
+        """
+        The base name of the block entity represented by the BlockEntity object (eg: `chest`, `furnace`)
+
+        :return: The base name of the block entity
+        """
+        return self._base_name
+
+    @base_name.setter
+    def base_name(self, value: str):
+        self._base_name = value
+
+    @property
+    def x(self) -> int:
+        return self._x
+
+    @property
+    def y(self) -> int:
+        return self._y
+
+    @property
+    def z(self) -> int:
+        return self._z
+
+    @property
+    def location(self) -> BlockCoordinates:
+        return self._x, self._y, self._z
+
+    @property
+    def nbt(self) -> NamedTag:
+        """
+        The NBT behind the object
+
+        :getter: Get the NBT data stored in the object
+        :setter: Set the NBT data stored in the object
+
+        :return: A NamedTag
+        """
+        return self._nbt
+
+    @nbt.setter
+    def nbt(self, value: NamedTag):
+        if not isinstance(value, NamedTag):
+            raise TypeError
+        self._nbt = value
--- amulet/api/entity.py
+++ amulet/api/entity.py
@@ -1,18 +1,23 @@
 from __future__ import annotations
 
-import numpy
 from typing import Tuple, Union
 from amulet_nbt import NamedTag
-from .abstract_base_entity import AbstractBaseEntity
+from amulet.api.data_types import PointCoordinates
 
 
-class Entity(AbstractBaseEntity):
+class Entity:
     """
     A class to contain all the data to define an Entity.
     """
 
-    obj_name = "Entity"
-    coord_types = (float, numpy.floating)
+    __slots__ = (
+        "_namespace",
+        "_base_name",
+        "_x",
+        "_y",
+        "_z",
+        "_nbt",
+    )
 
     def __init__(
         self,
@@ -33,7 +38,54 @@ class Entity(AbstractBaseEntity):
         :param z: The z coordinate of the entity
         :param nbt: The NBT stored with the entity
         """
-        super().__init__(namespace, base_name, x, y, z, nbt)
+        self._namespace = str(namespace)
+        self._base_name = str(base_name)
+        self._x = float(x)
+        self._y = float(y)
+        self._z = float(z)
+        if not isinstance(nbt, NamedTag):
+            raise TypeError(f"nbt must be an NamedTag. Got {nbt}")
+        self._nbt = nbt
+
+    def __repr__(self):
+        return f"Entity({self._namespace!r}, {self._base_name!r}, {self._x}, {self._y}, {self._z}, {self._nbt!r})"
+
+    @property
+    def namespaced_name(self) -> str:
+        """
+        The namespace:base_name of the entity represented by the object (eg: `minecraft:creeper`)
+
+        If the given namespace is an empty string it will just return the base name.
+
+        :return: namespace:base_name of the entity
+        """
+        return f"{self._namespace}:{self._base_name}"
+
+    @property
+    def namespace(self) -> str:
+        """
+        The namespace of the entity represented by the Entity object (eg: `minecraft`)
+
+        :return: The namespace of the entity
+        """
+        return self._namespace
+
+    @namespace.setter
+    def namespace(self, value: str):
+        self._namespace = value
+
+    @property
+    def base_name(self) -> str:
+        """
+        The base name of the entity represented by the Entity object (eg: `creeper`, `pig`)
+
+        :return: The base name of the entity
+        """
+        return self._base_name
+
+    @base_name.setter
+    def base_name(self, value: str):
+        self._base_name = value
 
     @property
     def x(self) -> float:
@@ -63,7 +115,7 @@ class Entity(AbstractBaseEntity):
         self._z = value
 
     @property
-    def location(self) -> Tuple[float, float, float]:
+    def location(self) -> PointCoordinates:
         """The location of the Entity."""
         return self._x, self._y, self._z
 
@@ -72,3 +124,21 @@ class Entity(AbstractBaseEntity):
         self, location: Tuple[Union[int, float], Union[int, float], Union[int, float]]
     ):
         self._x, self._y, self._z = location
+
+    @property
+    def nbt(self) -> NamedTag:
+        """
+        The NBT behind the object
+
+        :getter: Get the NBT data stored in the object
+        :setter: Set the NBT data stored in the object
+
+        :return: A NamedTag
+        """
+        return self._nbt
+
+    @nbt.setter
+    def nbt(self, value: NamedTag):
+        if not isinstance(value, NamedTag):
+            raise TypeError
+        self._nbt = value
