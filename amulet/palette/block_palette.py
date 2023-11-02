96cc585cfbcf03d5b38a858354b347d2642d0602
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2023-11-02 11:23:58 +0000

    Refactored block palette
    This now takes the version range it is compatible with.
    Blocks are added through the normal methods

--- amulet/palette/block_palette.py
+++ amulet/palette/block_palette.py
@@ -3,27 +3,27 @@ from typing import Union
 from threading import Lock
 from collections.abc import Sequence
 
-from amulet.block import BlockStack, Block
-from amulet.game_version import GameVersionRange
+from amulet.block import BlockStack
+from amulet.game_version import GameVersionRange, GameVersionRangeContainer
 
 
-class BlockPalette(Sequence[BlockStack]):
+class BlockPalette(GameVersionRangeContainer, Sequence[BlockStack]):
     """
     Class to handle the mappings between Block Stack objects and their index-based internal IDs
     """
 
     def __init__(self, version_range: GameVersionRange):
         """
-        Creates a new BlockManager object
+        Creates a new BlockPalette object
         """
+        GameVersionRangeContainer.__init__(self, version_range)
         self._lock = Lock()
         self._index_to_block: list[BlockStack] = []
         self._block_to_index: dict[BlockStack, int] = {}
-        self._version_range = version_range
 
     def __len__(self):
         """
-        The number of blocks in the registry.
+        The number of block stacks in the palette.
 
         >>> block_palette: BlockPalette
         >>> len(block_palette)
@@ -36,7 +36,7 @@ class BlockPalette(Sequence[BlockStack]):
 
     def __contains__(self, item: Union[int, BlockStack]) -> bool:
         """
-        Is the given :class:`Block` already in the registry.
+        Is the given :class:`BlockStack` already in the palette.
 
         >>> block_palette: BlockPalette
         >>> block_stack: BlockStack
@@ -50,50 +50,34 @@ class BlockPalette(Sequence[BlockStack]):
         if isinstance(item, int):
             return item < len(self._index_to_block)
         elif isinstance(item, BlockStack):
-            return self.fix_block(item) in self._block_to_index
+            return item in self._block_to_index
         return False
 
-    def index_to_block(self, index: int) -> BlockStack:
+    def index_to_block_stack(self, index: int) -> BlockStack:
         """
-        Get the block at the specified palette index.
+        Get the block stack at the specified palette index.
         :param index: The index to get
-        :return: The block at that index
-        :raises KeyError if there is no block at that index.
+        :return: The block stack at that index
+        :raises KeyError if there is no block stack at that index.
         """
         return self._index_to_block[index]
 
-    def block_to_index(self, block: BlockStack) -> int:
+    def block_stack_to_index(self, block_stack: BlockStack) -> int:
         """
-        Get the index of the block in the palette.
+        Get the index of the block stack in the palette.
         If it is not in the palette already it will be added first.
-        :param block: The block to get the index of.
-        :return: The index of the block in the palette.
+        :param block_stack: The block stack to get the index of.
+        :return: The index of the block stack in the palette.
         """
-        if not isinstance(block, BlockStack):
-            raise TypeError(repr(block))
-        block = self.fix_block(block)
-        if block not in self._block_to_index:
+        if not isinstance(block_stack, BlockStack):
+            raise TypeError(repr(block_stack))
+        if not all(b.version in self.version_range for b in block_stack):
+            raise ValueError(
+                f"BlockStack {block_stack} is incompatible with {self.version_range}"
+            )
+        if block_stack not in self._block_to_index:
             with self._lock:
-                if block not in self._block_to_index:
-                    self._index_to_block.append(block)
-                    self._block_to_index[block] = len(self._index_to_block) - 1
-        return self._block_to_index[block]
-
-    def fix_block(self, block_stack: BlockStack) -> BlockStack:
-        """If a version has not been set for a block this sets the maximum version this palette supports."""
-        blocks = []
-        for block in block_stack:
-            if block.version is None:
-                blocks.append(
-                    Block(
-                        block.namespace,
-                        block.base_name,
-                        block.properties,
-                        self._version_range.max,
-                    )
-                )
-            elif block.version in self._version_range:
-                blocks.append(block)
-            else:
-                raise ValueError(block)
-        return BlockStack(*blocks)
+                if block_stack not in self._block_to_index:
+                    self._index_to_block.append(block_stack)
+                    self._block_to_index[block_stack] = len(self._index_to_block) - 1
+        return self._block_to_index[block_stack]
