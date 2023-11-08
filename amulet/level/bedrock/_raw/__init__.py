66aa21f8ebb586a7871cb5d3ad879ab3f6d07513
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2023-11-08 14:47:25 +0000

    Clean up init py

--- amulet/level/bedrock/_raw/__init__.py
+++ amulet/level/bedrock/_raw/__init__.py
@@ -1,4 +1,5 @@
-from ._level_dat import BedrockLevelDAT
-from ._level import BedrockRawLevelPrivate, BedrockRawLevel
 from ._chunk import BedrockRawChunk
+from ._dimension import BedrockRawDimension
+from ._level import BedrockRawLevelPrivate, BedrockRawLevel
+from ._level_dat import BedrockLevelDAT
 from ._typing import InternalDimension
