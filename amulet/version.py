cb7b5bb9544e515dc25e12cee858d2111ff6498c
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2023-11-11 15:47:41 +0000

    Removed unused import

--- amulet/version.py
+++ amulet/version.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 from abc import ABC, abstractmethod
 from typing import TypeVar, Generic, Any
-from collections.abc import Container
 
 
 class AbstractVersion(ABC):
