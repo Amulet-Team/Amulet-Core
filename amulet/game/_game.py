0cb387cb69508a5840e38025ff8df8dffa32d70e
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2024-02-24 16:50:45 +0000

    Added code to load the game version data

--- amulet/game/_game.py
+++ amulet/game/_game.py
@@ -3,6 +3,9 @@ from __future__ import annotations
 from functools import cache
 from typing import overload, Literal, TYPE_CHECKING
 from collections.abc import Sequence
+from threading import Lock
+import pickle
+import os
 
 from amulet.version import VersionNumber
 
@@ -12,7 +15,25 @@ if TYPE_CHECKING:
     from .bedrock import BedrockGameVersion
 
 
-_versions: dict[str, list[GameVersion]] = {}
+_versions: dict[str, list[GameVersion]] | None = None
+_lock = Lock()
+
+
+def _get_versions() -> dict[str, list[GameVersion]]:
+    global _versions
+    if _versions is None:
+        with _lock:
+            if _versions is None:
+                try:
+                    with open(os.path.join(os.path.dirname(__file__), "versions.pkl"), "rb") as pkl:
+                        _versions = pickle.load(pkl)
+                except Exception:
+                    raw_path = os.environ.get("AMULET_GAME_VERSION_RAW_PATH")
+                    if raw_path is None:
+                        raise RuntimeError("Could not find game version data.")
+                    # TODO: Load the raw data
+                    raise NotImplementedError
+    return _versions
 
 
 def game_platforms() -> list[str]:
@@ -20,7 +41,7 @@ def game_platforms() -> list[str]:
     Get a list of all the platforms there are Version classes for.
     These are currently 'java' and 'bedrock'
     """
-    return list(_versions.keys())
+    return list(_get_versions().keys())
 
 
 @overload
@@ -46,9 +67,9 @@ def game_versions(platform: str) -> Sequence[GameVersion]:
     :return: The version classes for the platform
     :raise: Raises a KeyError if the platform is not present.
     """
-    if platform not in _versions:
+    if platform not in _get_versions():
         raise KeyError(f'The requested platform "{platform}" is not present')
-    return tuple(_versions[platform])
+    return tuple(_get_versions()[platform])
 
 
 @overload
@@ -80,9 +101,9 @@ def get_game_version(platform: str, version_number: VersionNumber) -> GameVersio
     :return: The Version class for the given inputs.
     :raise: Raises a KeyError if it does not exist.
     """
-    if platform not in _versions:
+    if platform not in _get_versions():
         raise KeyError(f'The requested platform "{platform}" is not present')
-    for version in _versions[platform]:
+    for version in _get_versions()[platform]:
         if version.supports_version(platform, version_number):
             return version
     raise KeyError(f"Version {platform}, {version_number} is not supported")
