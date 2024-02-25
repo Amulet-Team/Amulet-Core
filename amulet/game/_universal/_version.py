18a35d5b8e3e66d1c1e7ccea3fe1223aa06e6da7
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2024-02-25 15:49:32 +0000

    Added from_json methods

--- amulet/game/_universal/_version.py
+++ amulet/game/_universal/_version.py
@@ -1,10 +1,17 @@
 from __future__ import annotations
+from typing import Self
+import os
+import json
 
-from amulet.game.abc import GameVersion, BiomeData, BlockData
+from amulet.game.abc import GameVersion, BiomeData, load_json_block_spec
 from amulet.version import VersionNumber
+
+from ._block import UniversalBlockData
 
 
 class UniversalVersion(GameVersion):
+    _block: UniversalBlockData
+
     def supports_version(self, platform: str, version: VersionNumber) -> bool:
         return platform == "universal"
 
@@ -21,9 +28,29 @@ class UniversalVersion(GameVersion):
         return VersionNumber(1)
 
     @property
-    def block(self) -> BlockData:
-        pass
+    def block(self) -> UniversalBlockData:
+        return self._block
 
     @property
     def biome(self) -> BiomeData:
-        pass
+        raise NotImplementedError
+
+    @classmethod
+    def from_json(cls, version_path: str) -> Self:
+        with open(os.path.join(version_path, "__init__.json")) as f:
+            init = json.load(f)
+        assert init["platform"] == "universal"
+
+        block_format = init["block_format"]
+
+        self = cls()
+
+        # Load the block specification and translations
+        block_spec = load_json_block_spec(version_path, block_format)
+
+        self._block = UniversalBlockData(
+            self,
+            block_spec,
+        )
+
+        return self
--- amulet/game/bedrock/_version.py
+++ amulet/game/bedrock/_version.py
@@ -1,25 +1,40 @@
 from __future__ import annotations
+from typing import Self
+import json
+import os
 
-from amulet.game.abc import GameVersion, BiomeData
+from amulet.game import get_game_version
+from amulet.game.abc import GameVersion, BiomeData, load_json_block_spec
+from amulet.game.abc.translate import (
+    BlockToUniversalTranslator,
+    BlockFromUniversalTranslator,
+    load_json_block_translations,
+)
 from amulet.version import VersionNumber
+
 from ._block import BedrockBlockData
 
 
 class BedrockGameVersion(GameVersion):
+    _block: BedrockBlockData
+
     def __init__(
         self,
+        min_data_version: VersionNumber,
+        max_data_version: VersionNumber,
         min_semantic_version: VersionNumber,
         max_semantic_version: VersionNumber,
-        block_data: BedrockBlockData,
     ) -> None:
+        """Do not use this."""
+        self._min_data_version = min_data_version
+        self._max_data_version = max_data_version
         self._min_semantic_version = min_semantic_version
         self._max_semantic_version = max_semantic_version
-        self._block_data = block_data
 
     def supports_version(self, platform: str, version: VersionNumber) -> bool:
-        return (
-            platform == "java"
-            and self._min_semantic_version <= version <= self._max_semantic_version
+        return platform == "bedrock" and (
+            self._min_data_version <= version <= self._max_data_version
+            or self._min_semantic_version <= version <= self._max_semantic_version
         )
 
     @property
@@ -36,8 +51,69 @@ class BedrockGameVersion(GameVersion):
 
     @property
     def block(self) -> BedrockBlockData:
-        return self._block_data
+        return self._block
 
     @property
     def biome(self) -> BiomeData:
-        pass
+        raise NotImplementedError
+
+    @classmethod
+    def from_json(cls, version_path: str) -> Self:
+        with open(os.path.join(version_path, "__init__.json")) as f:
+            init = json.load(f)
+        assert init["platform"] == "bedrock"
+        min_data_version = VersionNumber(init["data_version"])
+        max_data_version = VersionNumber(init["data_version"])  # TODO: Add max field
+        min_semantic_version = VersionNumber(init["version"])
+        max_semantic_version = VersionNumber(init["version"])  # TODO: Add max field
+
+        block_format = {
+            "pseudo-numerical": "numerical",
+            "nbt-blockstate": "blockstate"
+        }[init["block_format"]]
+        universal_version = get_game_version("universal", VersionNumber(1))
+
+        self = cls(
+            min_data_version,
+            max_data_version,
+            min_semantic_version,
+            max_semantic_version,
+        )
+
+        # Load the block specification and translations
+        block_spec = load_json_block_spec(version_path, block_format)
+        block_to_universal = load_json_block_translations(
+            version_path,
+            block_format,
+            "to_universal",
+            BlockToUniversalTranslator,
+            lambda namespace, base_name: block_spec[namespace][base_name],
+            universal_version
+        )
+        block_from_universal = load_json_block_translations(
+            version_path,
+            block_format,
+            "from_universal",
+            BlockFromUniversalTranslator,
+            universal_version.block.get_specification,
+            self
+        )
+
+        numerical_block_map = {}
+        file_path = os.path.join(version_path, "__numerical_block_map__.json")
+        if os.path.isfile(file_path):
+            with open(file_path) as f:
+                block_map: dict[str, int] = json.load(f)
+            for block_str, block_num in block_map.items():
+                namespace, base_name = block_str.split(":", 1)
+                numerical_block_map[block_num] = (namespace, base_name)
+
+        self._block = BedrockBlockData(
+            self,
+            block_spec,
+            block_to_universal,
+            block_from_universal,
+            numerical_block_map,
+        )
+
+        return self
