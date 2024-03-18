5e24aeedb2b541621a6a32966ff376e5340f0fe5
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2024-03-18 13:13:12 +0000

    Converted height to TypedProperty

--- amulet/chunk/components/height_2d.py
+++ amulet/chunk/components/height_2d.py
@@ -1,23 +1,28 @@
-from typing import Optional
 import numpy
+from numpy.typing import NDArray
+from amulet.utils.typed_property import TypedProperty
+
+
+# TODO: If numpy ever supports type hinting shape, add it here.
+HeightType = NDArray[numpy.int64]
 
 
 class Height2DComponent:
-    __height: Optional[numpy.ndarray]
+    __height: HeightType | None
 
     def __init__(self) -> None:
         self.__height = None
 
-    @property
-    def height(self) -> numpy.ndarray:
+    @TypedProperty[HeightType, HeightType | object]
+    def height(self) -> HeightType:
         if self.__height is None:
             self.__height = numpy.zeros((16, 16), dtype=numpy.int64)
         return self.__height
 
     @height.setter
-    def height(
+    def _set_height(
         self,
-        height: numpy.ndarray,
+        height: HeightType | object,
     ) -> None:
         height = numpy.asarray(height)
         if not isinstance(height, numpy.ndarray):
