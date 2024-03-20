5be7ab1a276766a2e05b4e8f43b595fc3bb4d3f4
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2024-03-20 11:33:12 +0000

    Support setting Bedrock raw chunk to None

--- amulet/level/bedrock/chunk/components/raw_chunk.py
+++ amulet/level/bedrock/chunk/components/raw_chunk.py
@@ -10,14 +10,15 @@ class RawChunkComponent:
         self.__raw_chunk = None
 
     @property
-    def raw_chunk(self) -> BedrockRawChunk:
+    def raw_chunk(self) -> BedrockRawChunk | None:
         return self.__raw_chunk
 
     @raw_chunk.setter
     def raw_chunk(
         self,
-        raw_chunk: BedrockRawChunk,
+        raw_chunk: BedrockRawChunk | None,
     ) -> None:
-        if not isinstance(raw_chunk, BedrockRawChunk):
+        if isinstance(raw_chunk, BedrockRawChunk) or raw_chunk is None:
+            self.__raw_chunk = raw_chunk
+        else:
             raise TypeError
-        self.__raw_chunk = raw_chunk
