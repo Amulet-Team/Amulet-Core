ab6d3e205de5cf74bcbff46c64e4876e9a3c62e6
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2024-03-25 09:56:49 +0000

    Update level init

--- amulet/level/__init__.py
+++ amulet/level/__init__.py
@@ -1,6 +1,8 @@
 from .abc import Level
-from .java import JavaLevel
-from .bedrock import BedrockLevel
-from .temporary_level import TemporaryLevel
 
-from .load import load_format, load_level
+from ._load import register_level_class, unregister_level_class, get_level, NoValidLevel
+
+# from .java import JavaLevel
+from .bedrock import BedrockLevel
+
+# from .temporary_level import TemporaryLevel
