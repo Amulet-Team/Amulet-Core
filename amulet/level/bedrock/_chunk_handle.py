0b6f417cc85d57d42d9cd77b9c9f65b12cd47f45
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2024-04-05 16:34:32 +0100

    Fixed issues in BedrockChunkHandle

--- amulet/level/bedrock/_chunk_handle.py
+++ amulet/level/bedrock/_chunk_handle.py
@@ -3,7 +3,7 @@ from typing import TYPE_CHECKING
 
 from amulet.level.abc import ChunkHandle
 from ..abc._chunk_handle import ChunkT
-from .chunk import BedrockChunk
+from .chunk import BedrockChunk, BedrockChunk
 
 if TYPE_CHECKING:
     from ._level import BedrockLevel
@@ -11,8 +11,9 @@ if TYPE_CHECKING:
 
 
 class BedrockChunkHandle(
-    ChunkHandle["BedrockLevel", "BedrockRawDimension", "BedrockChunk"]
+    ChunkHandle["BedrockLevel", "BedrockRawDimension", BedrockChunk]
 ):
-    def _validate_chunk(self, chunk: ChunkT) -> None:
+    @staticmethod
+    def _validate_chunk(chunk: ChunkT) -> None:
         if not isinstance(chunk, BedrockChunk):
             raise TypeError
