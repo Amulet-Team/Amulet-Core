bde90795fc7a4e140184c3d21bdd96e8b99ffed9
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2024-05-06 11:46:01 +0100

    Fix GameVersion import
    This must be imported from amulet.game.abc

--- amulet/game/__init__.py
+++ amulet/game/__init__.py
@@ -2,6 +2,6 @@
 A module to store data about the game including state enumerations and translations between different game versions.
 """
 
-from ._game import game_platforms, game_versions, get_game_version, GameVersion
+from ._game import game_platforms, game_versions, get_game_version
 from .java import JavaGameVersion
 from .bedrock import BedrockGameVersion
