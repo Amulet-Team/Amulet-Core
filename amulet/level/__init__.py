6903618048cd758c2cd95868a781dae9d99d3886
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2024-06-01 09:46:04 +0100

    Import Java level

--- amulet/level/__init__.py
+++ amulet/level/__init__.py
@@ -2,7 +2,7 @@ from .abc import Level
 
 from ._load import register_level_class, unregister_level_class, get_level, NoValidLevel
 
-# from .java import JavaLevel
+from .java import JavaLevel
 from .bedrock import BedrockLevel
 
 # from .temporary_level import TemporaryLevel
