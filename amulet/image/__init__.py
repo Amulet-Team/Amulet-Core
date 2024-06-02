e9f2612181e6f7c27610fbd39ccfe9226aa58e1c
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2024-06-02 09:12:08 +0100

    Added missing pack and missing no textures

--- amulet/image/__init__.py
+++ amulet/image/__init__.py
@@ -1,3 +1,5 @@
 import os
 
+missing_pack = os.path.abspath(os.path.join(os.path.dirname(__file__), "missing_pack.png"))
+missing_no = os.path.abspath(os.path.join(os.path.dirname(__file__), "missing_no.png"))
 missing_world = os.path.abspath(os.path.join(os.path.dirname(__file__), "missing_world.png"))
