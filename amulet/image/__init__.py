fdae90f89dab99defa516853959d1be2b93a4be9
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2024-06-02 09:12:25 +0100

    Reformatted

--- amulet/image/__init__.py
+++ amulet/image/__init__.py
@@ -1,5 +1,9 @@
 import os
 
-missing_pack = os.path.abspath(os.path.join(os.path.dirname(__file__), "missing_pack.png"))
+missing_pack = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), "missing_pack.png")
+)
 missing_no = os.path.abspath(os.path.join(os.path.dirname(__file__), "missing_no.png"))
-missing_world = os.path.abspath(os.path.join(os.path.dirname(__file__), "missing_world.png"))
+missing_world = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), "missing_world.png")
+)
