ca34f6ddfa25dbd37d15e93eee949df137880f66
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2024-06-03 10:57:52 +0100

    Refactored to a src layout

--- src/amulet/_version.py
+++ src/amulet/_version.py
@@ -43,7 +43,7 @@ def get_config():
     cfg.style = "pep440"
     cfg.tag_prefix = ""
     cfg.parentdir_prefix = "amulet-"
-    cfg.versionfile_source = "amulet/_version.py"
+    cfg.versionfile_source = "src/amulet/_version.py"
     cfg.verbose = False
     return cfg
 
