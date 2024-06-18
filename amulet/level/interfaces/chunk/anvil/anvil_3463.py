5a32a1c75b5b15eddac88e4516310015f63f6799
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2024-06-18 12:05:45 +0100

    Increase max Java version (#305)

--- amulet/level/interfaces/chunk/anvil/anvil_3463.py
+++ amulet/level/interfaces/chunk/anvil/anvil_3463.py
@@ -13,7 +13,7 @@ class Anvil3463Interface(ParentInterface):
 
     @staticmethod
     def minor_is_valid(key: int):
-        return 3454 <= key <= 3900
+        return 3454 <= key <= 4000
 
 
 export = Anvil3463Interface
