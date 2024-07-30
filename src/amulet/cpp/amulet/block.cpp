26bdfaf2191cf10a91101864d3a21b79c02752de
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2024-07-30 15:30:22 +0100

    Added properties to block

--- src/amulet/cpp/amulet/block.cpp
+++ src/amulet/cpp/amulet/block.cpp
@@ -1,6 +1,9 @@
 #include <map>
+#include <variant>
+#include <type_traits>
 
 #include <amulet/block.hpp>
+#include <amulet_nbt/nbt_encoding/binary.hpp>
 
 namespace Amulet {
     Block::Block(
@@ -8,8 +11,12 @@ namespace Amulet {
         const VersionNumber& version,
         const std::string& namespace_,
         const std::string& base_name,
-        const std::map<std::string, > properties
-    ) : PlatformVersionContainer(platform, version), namespace_(namespace_), base_name(base_name) {}
+        const std::map<std::string, PropertyValueType>& properties
+    ): 
+        PlatformVersionContainer(platform, version), 
+        namespace_(namespace_), 
+        base_name(base_name), 
+        properties(properties) {}
 
     void Block::serialise(Amulet::BinaryWriter& writer) const {
         writer.writeNumeric<std::uint8_t>(1);
@@ -18,6 +25,13 @@ namespace Amulet {
         writer.writeSizeAndBytes(namespace_);
         writer.writeSizeAndBytes(base_name);
         
+        writer.writeNumeric<std::uint64_t>(properties.size());
+        for (auto const& [key, val] : properties) {
+            writer.writeSizeAndBytes(key);
+            std::visit([&writer](auto&& tag) {
+                AmuletNBT::write_nbt(writer, "", tag);
+            }, val);
+        }
     }
     Block Block::deserialise(Amulet::BinaryReader& reader){
         auto version_number = reader.readNumeric<std::uint8_t>();
@@ -28,8 +42,29 @@ namespace Amulet {
             VersionNumber version = VersionNumber::deserialise(reader);
             std::string namespace_ = reader.readSizeAndBytes();
             std::string base_name = reader.readSizeAndBytes();
-            // TODO: properties
-            return Block(platform, version, namespace_, base_name);
+            std::uint64_t property_count;
+            std::map<std::string, PropertyValueType> properties;
+            reader.readNumericInto<std::uint64_t>(property_count);
+            for (std::uint64_t i = 0; i < property_count; i++) {
+                std::string name = reader.readSizeAndBytes();
+                AmuletNBT::NamedTag named_tag = AmuletNBT::read_nbt(reader);
+                properties[name] = std::visit([](auto&& tag) -> PropertyValueType {
+                    using T = std::decay_t<decltype(tag)>;
+                    if constexpr (
+                        std::is_same_v<T, AmuletNBT::ByteTag> ||
+                        std::is_same_v<T, AmuletNBT::ShortTag> ||
+                        std::is_same_v<T, AmuletNBT::IntTag> ||
+                        std::is_same_v<T, AmuletNBT::LongTag> ||
+                        std::is_same_v<T, AmuletNBT::StringTag>
+                    ) {
+                        return tag;
+                    }
+                    else {
+                        throw std::invalid_argument("Property tag must be Byte, Short, Int, Long or String");
+                    }
+                }, named_tag.tag_node);
+            }
+            return Block(platform, version, namespace_, base_name, properties);
         }
         default:
             throw std::invalid_argument("Unsupported version " + std::to_string(version_number));
--- src/amulet/cpp/amulet/block.hpp
+++ src/amulet/cpp/amulet/block.hpp
@@ -2,7 +2,8 @@
 #include <variant>
 
 #include <amulet/version.hpp>
-#include <amulet_nbt/tag/nbt.hpp>
+#include <amulet_nbt/tag/int.hpp>
+#include <amulet_nbt/tag/string.hpp>
 
 
 namespace Amulet {
@@ -11,22 +12,31 @@ namespace Amulet {
         AmuletNBT::ShortTag,
         AmuletNBT::IntTag,
         AmuletNBT::LongTag,
-        AmuletNBT::StringTag,
+        AmuletNBT::StringTag
     > PropertyValueType;
 
     class Block: public PlatformVersionContainer {
+        private:
+            std::string namespace_;
+            std::string base_name;
+            std::map<std::string, PropertyValueType> properties;
         public:
-            const std::string namespace_;
-            const std::string base_name;
-            const std::map<std::string, PropertyValueType> properties
+            const std::string& get_namespace() const { return namespace_; }
+            const std::string& get_base_name() const { return base_name; }
+            const std::map<std::string, PropertyValueType>& get_properties() const {
+                return properties;
+            }
 
             Block(
                 const PlatformType&,
                 const VersionNumber&,
-                const std::string&,
-                const std::string&
+                const std::string& namespace_,
+                const std::string& base_name,
+                const std::map<std::string, PropertyValueType>&
             );
+
             void serialise(Amulet::BinaryWriter&) const;
             static Block deserialise(Amulet::BinaryReader&);
+            
     };
 }
