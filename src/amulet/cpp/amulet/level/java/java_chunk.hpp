9827986bcf698b6ba3886403f88f305d7a458b9f
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2024-08-13 16:47:17 +0100

    Added all Java chunk classes

--- src/amulet/cpp/amulet/level/java/java_chunk.hpp
+++ src/amulet/cpp/amulet/level/java/java_chunk.hpp
@@ -4,11 +4,24 @@
 #include <optional>
 #include <stdexcept>
 #include <vector>
+#include <amulet/block.hpp>
+#include <amulet/biome.hpp>
 #include <amulet/chunk.hpp>
 #include <amulet/chunk_components/block_component.hpp>
+#include <amulet/level/java/chunk_components/data_version.hpp>
 
 namespace Amulet {
-	class JavaChunkNA : public ChunkComponentHelper<BlockComponent> {
+	class JavaChunkNA : public ChunkComponentHelper<
+		// JavaRawChunkComponent,
+		DataVersionComponent,
+		// LastUpdateComponent,
+		// JavaLegacyVersionComponent,
+		BlockComponent//,
+		// BlockEntityComponent,
+		// EntityComponent,
+		// Biome2DComponent,
+		// Height2DComponent,
+	> {
 	public:
 		static const std::string ChunkID;
 
@@ -16,8 +29,105 @@ namespace Amulet {
 
 		using ChunkComponentHelper::ChunkComponentHelper;
 		JavaChunkNA(
+			std::shared_ptr<BlockStack> default_block,
+			std::shared_ptr<Biome> default_biome
+		);
+	};
+
+	class JavaChunk0 : public ChunkComponentHelper<
+		// JavaRawChunkComponent,
+		DataVersionComponent,
+		// LastUpdateComponent,
+		// TerrainPopulatedComponent,
+		// LightPopulatedComponent,
+		BlockComponent//,
+		// BlockEntityComponent,
+		// EntityComponent,
+		// Biome2DComponent,
+		// Height2DComponent,
+	> {
+	public:
+		static const std::string ChunkID;
+
+		std::string chunk_id();
+
+		using ChunkComponentHelper::ChunkComponentHelper;
+		JavaChunk0(
 			std::int64_t data_version,
-			std::shared_ptr<BlockStack> default_block
+			std::shared_ptr<BlockStack> default_block,
+			std::shared_ptr<Biome> default_biome
+		);
+	};
+
+	class JavaChunk1444 : public ChunkComponentHelper<
+		// JavaRawChunkComponent,
+		DataVersionComponent,
+		// LastUpdateComponent,
+		// StatusStringComponent,
+		BlockComponent//,
+		// BlockEntityComponent,
+		// EntityComponent,
+		// Biome2DComponent,
+		// Height2DComponent,
+	> {
+	public:
+		static const std::string ChunkID;
+
+		std::string chunk_id();
+
+		using ChunkComponentHelper::ChunkComponentHelper;
+		JavaChunk1444(
+			std::int64_t data_version,
+			std::shared_ptr<BlockStack> default_block,
+			std::shared_ptr<Biome> default_biome
+		);
+	};
+
+	class JavaChunk1466 : public ChunkComponentHelper<
+		// JavaRawChunkComponent,
+		DataVersionComponent,
+		// LastUpdateComponent,
+		// StatusStringComponent,
+		BlockComponent//,
+		// BlockEntityComponent,
+		// EntityComponent,
+		// Biome2DComponent,
+		// NamedHeight2DComponent,
+	> {
+	public:
+		static const std::string ChunkID;
+
+		std::string chunk_id();
+
+		using ChunkComponentHelper::ChunkComponentHelper;
+		JavaChunk1466(
+			std::int64_t data_version,
+			std::shared_ptr<BlockStack> default_block,
+			std::shared_ptr<Biome> default_biome
+		);
+	};
+
+	class JavaChunk2203 : public ChunkComponentHelper<
+		// JavaRawChunkComponent,
+		DataVersionComponent,
+		// LastUpdateComponent,
+		// StatusStringComponent,
+		BlockComponent//,
+		// BlockEntityComponent,
+		// EntityComponent,
+		// Biome3DComponent,
+		// NamedHeight2DComponent,
+	> {
+	public:
+		static const std::string ChunkID;
+
+		std::string chunk_id();
+
+		using ChunkComponentHelper::ChunkComponentHelper;
+		JavaChunk2203(
+			std::int64_t data_version,
+			std::shared_ptr<BlockStack> default_block,
+			std::shared_ptr<Biome> default_biome
 		);
 	};
 }
