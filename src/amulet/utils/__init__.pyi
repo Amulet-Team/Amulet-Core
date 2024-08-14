9e2cb81e7e41bd199f8524df71a04a830feb76af
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2024-08-14 16:20:50 +0100

    Remove imports

--- src/amulet/utils/__init__.pyi
+++ src/amulet/utils/__init__.pyi
@@ -1,17 +1,5 @@
 from __future__ import annotations
-import typing
 from . import collections
 from . import numpy
 
-__all__: list = [
-    "block_coords_to_chunk_coords",
-    "chunk_coords_to_region_coords",
-    "region_coords_to_chunk_coords",
-    "blocks_slice_to_chunk_slice",
-    "from_nibble_array",
-    "check_all_exist",
-    "check_one_exists",
-    "load_leveldat",
-]
-
-def __getattr__(arg0: typing.Any) -> typing.Any: ...
+__all__ = ["collections", "numpy"]
--- src/amulet/utils/_init_utils.cpp
+++ src/amulet/utils/_init_utils.cpp
@@ -10,42 +10,4 @@ void init_utils(py::module utils_module){
 
     auto utils_collections_module = utils_module.def_submodule("collections");
     init_utils_collections(utils_collections_module);
-
-    utils_module.def(
-        "__getattr__",
-        [utils_module](py::object name) -> py::object {
-            if (
-                name == py::str("block_coords_to_chunk_coords") ||
-                name == py::str("chunk_coords_to_region_coords") ||
-                name == py::str("region_coords_to_chunk_coords") ||
-                name == py::str("blocks_slice_to_chunk_slice") ||
-                name == py::str("from_nibble_array")
-            ){
-                py::object attr = py::module::import("amulet.utils.world_utils").attr(name);
-                utils_module.attr(name) = attr;
-                return attr;
-            } else if (
-                name == py::str("check_all_exist") ||
-                name == py::str("check_one_exists") ||
-                name == py::str("load_leveldat")
-            ){
-                py::object attr = py::module::import("amulet.utils.format_utils").attr(name);
-                utils_module.attr(name) = attr;
-                return attr;
-            } else {
-                throw py::attribute_error("module amulet.utils has no attribute " + py::repr(name).cast<std::string>());
-            }
-        }
-    );
-
-    py::list all;
-    all.append("block_coords_to_chunk_coords");
-    all.append("chunk_coords_to_region_coords");
-    all.append("region_coords_to_chunk_coords");
-    all.append("blocks_slice_to_chunk_slice");
-    all.append("from_nibble_array");
-    all.append("check_all_exist");
-    all.append("check_one_exists");
-    all.append("load_leveldat");
-    utils_module.attr("__all__") = all;
 }
