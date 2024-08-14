394053c3fa4cb58dd9f4b50d80600695edc499cc
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2024-08-14 16:16:30 +0100

    Moved init to C++

--- src/amulet/palette/__init__.py
+++
@@ -1,2 +0,0 @@
-from .biome_palette import BiomePalette
-from .block_palette import BlockPalette
--- src/amulet/utils/__init__.py
+++
@@ -1,8 +0,0 @@
-from .world_utils import (
-    block_coords_to_chunk_coords,
-    chunk_coords_to_region_coords,
-    region_coords_to_chunk_coords,
-    blocks_slice_to_chunk_slice,
-    from_nibble_array,
-)
-from .format_utils import *
