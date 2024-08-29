790d36657bfa18144f67e45035d228ff251974ed
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2024-08-29 14:08:33 +0100

    Fixed some issues with the holder class

--- src/amulet/collections/holder.py.hpp
+++ src/amulet/collections/holder.py.hpp
@@ -1,20 +1,25 @@
 #pragma once
 
+#include <memory>
+#include <pybind11/pybind11.h>
+
+namespace py = pybind11;
+
 namespace Amulet {
 namespace collections {
 
 	// A class to allow python to hold a reference to a smart pointer
 	class Holder {
 	public:
-		virtual ~Holder() {};
+		virtual ~Holder() {}
 	};
 
 	template <typename T>
-	class HolderTemplate {
+	class HolderTemplate: public Holder {
 	private:
 		T value;
 	public:
-		HolderTemplate(T value): value(value){}
+		HolderTemplate(T&& value): value(std::move(value)){}
 	};
 
 }
