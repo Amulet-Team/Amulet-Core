70e1d385f13d4e0cb384cb6eeed51b0d144b15b9
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2024-09-02 15:34:22 +0100

    Fixed module name

--- src/amulet/level/java/__init_java.py.cpp
+++ src/amulet/level/java/__init_java.py.cpp
@@ -12,7 +12,7 @@ void init_java(py::module m_parent) {
     py::def_deferred(
         m,
         {
-            py::deferred_package_path(m_parent, m, "level")//,
+            py::deferred_package_path(m_parent, m, "java")//,
             //py::deferred_import("amulet.level.java._level", "JavaLevel")
         }
     );
