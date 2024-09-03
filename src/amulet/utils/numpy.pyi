1cc3a0e433d3b91a43412fc2b8421e43e7510026
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2024-09-03 14:08:52 +0100

    Fixed unique_inverse stub

--- src/amulet/utils/numpy.pyi
+++ src/amulet/utils/numpy.pyi
@@ -1,9 +1,10 @@
 from __future__ import annotations
 import numpy
+import numpy.typing
 import typing_extensions
 
 __all__ = ["unique_inverse"]
 
 def unique_inverse(
-    arg0: typing_extensions.Buffer,
-) -> tuple[numpy.ndarray[numpy.uint32], numpy.ndarray[numpy.uint32]]: ...
+    array: typing_extensions.Buffer,
+) -> tuple[numpy.typing.NDArray[numpy.uint32], numpy.typing.NDArray[numpy.uint32]]: ...
