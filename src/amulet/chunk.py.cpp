0129b5c6c7c26ded4d76b8392c37a3a74d4c0aa0
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2024-09-05 12:55:06 +0100

    Added chunk pickling

--- src/amulet/chunk.py.cpp
+++ src/amulet/chunk.py.cpp
@@ -21,37 +21,55 @@ void init_chunk(py::module m_parent) {
             "component_ids",
             &Amulet::Chunk::get_component_ids
         );
+        auto py_serialise = [](const Amulet::Chunk& self) -> py::typing::Dict<py::str, py::typing::Optional<py::bytes>> {
+            py::dict data;
+            for (const auto& [k, v] : self.serialise_chunk()) {
+                if (v) {
+                    data[py::str(k)] = py::bytes(v.value());
+                }
+                else {
+                    data[py::str(k)] = py::none();
+                }
+            }
+            return data;
+        };
         Chunk.def(
             "serialise_chunk",
-            [](const Amulet::Chunk& self) -> py::typing::Dict<py::str, py::typing::Optional<py::bytes>> {
-                py::dict data;
-                for (const auto& [k, v] : self.serialise_chunk()) {
-                    if (v) {
-                        data[py::str(k)] = py::bytes(v.value());
-                    }
-                    else {
-                        data[py::str(k)] = py::none();
-                    }
+            py_serialise,
+            py::doc("This is private. Do not use this. It will be removed in the future.")
+        );
+        auto py_deserialise = [](Amulet::Chunk& self, py::typing::Dict<py::str, py::typing::Optional<py::bytes>> data) {
+            Amulet::SerialisedComponents component_data;
+            for (const auto& [k, v] : data) {
+                if (v.is(py::none())) {
+                    component_data[k.cast<std::string>()];
                 }
-                return data;
-            },
+                else {
+                    component_data[k.cast<std::string>()] = v.cast<std::string>();
+                }
+            }
+            self.reconstruct_chunk(component_data);
+        };
+        Chunk.def(
+            "reconstruct_chunk",
+            py_deserialise,
             py::doc("This is private. Do not use this. It will be removed in the future.")
         );
         Chunk.def(
-            "reconstruct_chunk",
-            [](Amulet::Chunk& self, py::typing::Dict<py::str, py::typing::Optional<py::bytes>> data) {
-                Amulet::SerialisedComponents component_data;
-                for (const auto& [k, v] : data) {
-                    if (v.is(py::none())) {
-                        component_data[k.cast<std::string>()];
-                    }
-                    else {
-                        component_data[k.cast<std::string>()] = v.cast<std::string>();
-                    }
+            py::pickle(
+                [py_serialise](const Amulet::Chunk& self) {
+                    return py::make_tuple(
+                        self.get_chunk_id(),
+                        py_serialise(self)
+                    );
+                },
+                [py_deserialise](py::tuple state) {
+                    if (state.size() != 2) { throw std::runtime_error("Invalid state!"); }
+                    auto self = Amulet::get_null_chunk(state[0].cast<std::string>());
+                    py_deserialise(*self, state[1]);
+                    return self;
                 }
-                self.reconstruct_chunk(component_data);
-            },
-            py::doc("This is private. Do not use this. It will be removed in the future.")
+            )
         );
 
     m.def(
