e6e9869ef8f2486093f22ecd461a4ffda6168140
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2024-09-13 17:07:48 +0100

    Removed old long array function

--- src/amulet/utils/world_utils.pyi
+++ src/amulet/utils/world_utils.pyi
@@ -12,8 +12,6 @@ __all__ = [
     "blocks_slice_to_chunk_slice",
     "chunk_coords_to_block_coords",
     "chunk_coords_to_region_coords",
-    "decode_long_array",
-    "encode_long_array",
     "from_nibble_array",
     "math",
     "ndarray",
@@ -74,44 +72,6 @@ def chunk_coords_to_region_coords(cx: int, cz: int) -> ChunkCoordinates:
     :param cx: The x coordinate of the chunk
     :param cz: The z coordinate of the chunk
     :return: The resulting region coordinates in (x, z) order
-
-    """
-
-def decode_long_array(
-    long_array: numpy.ndarray,
-    size: int,
-    bits_per_entry: int,
-    dense: bool = True,
-    signed: bool = False,
-) -> numpy.ndarray:
-    """
-
-    Decode a long array (from BlockStates or Heightmaps)
-
-    :param long_array: Encoded long array
-    :param size: The expected size of the returned array
-    :param bits_per_entry: The number of bits per entry in the encoded array.
-    :param dense: If true the long arrays will be treated as a bit stream. If false they are distinct values with padding
-    :param signed: Should the returned array be signed.
-    :return: Decoded array as numpy array
-
-    """
-
-def encode_long_array(
-    array: numpy.ndarray,
-    bits_per_entry: int | None = None,
-    dense: bool = True,
-    min_bits_per_entry: int = 1,
-) -> numpy.ndarray:
-    """
-
-    Encode a long array (from BlockStates or Heightmaps)
-
-    :param array: A numpy array of the data to be encoded.
-    :param bits_per_entry: The number of bits to use to store each value. If left as None will use the smallest bits per entry.
-    :param dense: If true the long arrays will be treated as a bit stream. If false they are distinct values with padding
-    :param min_bits_per_entry: The mimimum value that bits_per_entry can be. If it is less than this it will be capped at this value.
-    :return: Encoded array as numpy array
 
     """
 
