1ba32fabf5c42c50b5db3b92c187e92e5695eb43
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2024-09-17 14:05:54 +0100

    Refactor py::equals to use PyObject_RichCompare

--- src/amulet/pybind11/python.hpp
+++ src/amulet/pybind11/python.hpp
@@ -6,20 +6,9 @@ namespace py = pybind11;
 
 namespace pybind11 {
 	inline bool equals(
-	    py::object l,
-	    py::object r
+	    const py::handle l,
+	    const py::handle r
 	){
-		py::object l_result = l.attr("__eq__")(r);
-		py::object NotImplemented = py::module::import("builtins").attr("NotImplemented");
-		if (l_result.is(NotImplemented)){
-		    py::object r_result = r.attr("__eq__")(l);
-		    if (r_result.is(NotImplemented)){
-                return false;
-            } else {
-                return r_result.cast<bool>();
-            }
-		} else {
-		    return l_result.cast<bool>();
-		}
+		return PyObject_RichCompare(l.ptr(), r.ptr(), Py_EQ);
 	}
 }
