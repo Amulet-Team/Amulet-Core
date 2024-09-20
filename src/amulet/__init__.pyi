1911418abe5d55c9f8704752be738ee3cf96fc46
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2024-09-20 11:03:57 +0100

    Removed root init module

---
+++ src/amulet/__init__.pyi
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+from . import (
+    biome,
+    block,
+    block_entity,
+    chunk,
+    chunk_components,
+    collections,
+    level,
+    palette,
+    utils,
+    version,
+)
+
+__all__ = [
+    "biome",
+    "block",
+    "block_entity",
+    "chunk",
+    "chunk_components",
+    "collections",
+    "level",
+    "palette",
+    "utils",
+    "version",
+]
+__version__: str
