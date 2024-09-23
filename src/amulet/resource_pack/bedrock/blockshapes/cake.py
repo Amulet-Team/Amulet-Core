552ca6b3c59088152214ab12c701c6347d29dca4
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2024-09-23 09:40:33 +0100

    Fixed mypy error

--- src/amulet/resource_pack/bedrock/blockshapes/cake.py
+++ src/amulet/resource_pack/bedrock/blockshapes/cake.py
@@ -2,12 +2,12 @@ from amulet.block import Block
 from amulet.resource_pack.bedrock.blockshapes.partial_block import (
     PartialBlock,
 )
-import amulet_nbt
+from amulet_nbt import AbstractBaseIntTag, IntTag
 
 
 class Cake(PartialBlock):
     def is_valid(self, block: Block) -> bool:
-        return isinstance(block.properties.get("bite_counter"), amulet_nbt.TAG_Int)
+        return isinstance(block.properties.get("bite_counter"), IntTag)
 
     @property
     def blockshape(self) -> str:
@@ -16,15 +16,16 @@ class Cake(PartialBlock):
     def bounds(
         self, block: Block
     ) -> tuple[tuple[float, float], tuple[float, float], tuple[float, float]]:
+        bite_count = block.properties["bite_counter"]
         return (
-            (1 / 16 + block.properties["bite_counter"].py_int * 2 / 16, 15 / 16),
+            (1 / 16 + (bite_count.py_int if isinstance(bite_count, AbstractBaseIntTag) else 0) * 2 / 16, 15 / 16),
             (0, 0.5),
             (1 / 16, 15 / 16),
         )
 
     def texture_index(self, block: Block, aux_value: int) -> int:
         bite_counter = block.properties["bite_counter"]
-        assert isinstance(bite_counter, amulet_nbt.AbstractBaseIntTag)
+        assert isinstance(bite_counter, AbstractBaseIntTag)
         return min(1, bite_counter.py_int)
 
     @property
