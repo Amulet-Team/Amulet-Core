44b27560904c3c192963b65686c83e94b6ea88fe
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2024-11-29 11:18:03 +0000

    Added compiler metadata and validator
    The libraries must be compiled with the same version of the compiler and pybind11.
    If they are compiled with different versions bad things happen.
    Adding this data at compile time allows dependent libraries to validate compatibility.
    
    Added code to validate that the nbt and leveldb libraries are compatible.
    This produces a sane error message at import.

--- src/amulet/__init__.py.cpp
+++ src/amulet/__init__.py.cpp
@@ -17,13 +17,60 @@ void init_chunk_components(py::module);
 void init_level(py::module);
 void init_block_mesh(py::module);
 
+static std::string get_version_top(std::string version){
+    return version.substr(0, version.find('.', version.find('.') + 1));
+}
 
+static void check_compatibility(py::module a, std::string a_name, py::module b, std::string b_name){
+    py::dict a_config = a.attr("compiler_config");
+    py::dict b_config = b.attr("compiler_config");
 
+    std::string a_pybind11_version = a_config["pybind11_version"].cast<std::string>();
+    std::string b_pybind11_version = b_config["pybind11_version"].cast<std::string>();
+    if (a_pybind11_version != b_pybind11_version){
+        throw std::runtime_error(
+            "pybind11 version mismatch. " +
+            a_name + " is compiled for pybind11==" + a_pybind11_version +
+            " and " +
+            b_name + " is compiled for pybind11==" + b_pybind11_version
+        );
+    }
 
+    std::string a_compiler_id = a_config["compiler_id"].cast<std::string>();
+    std::string b_compiler_id = b_config["compiler_id"].cast<std::string>();
+    if (a_compiler_id != b_compiler_id){
+        throw std::runtime_error(
+            "compiler mismatch. " +
+            a_name + " is compiled by " + a_compiler_id +
+            " and " +
+            b_name + " is compiled by " + b_compiler_id
+        );
+    }
+
+    std::string a_compiler_version = a_config["compiler_version"].cast<std::string>();
+    std::string b_compiler_version = b_config["compiler_version"].cast<std::string>();
+    if (get_version_top(a_compiler_version) != get_version_top(b_compiler_version)){
+        throw std::runtime_error(
+            "compiler version mismatch. " +
+            a_name + " is compiled by " + a_compiler_id + " " + a_compiler_version +
+            " and " +
+            b_name + " is compiled by " + b_compiler_id + " " + b_compiler_version
+        );
+    }
+}
 
 void init_module(py::module m){
     auto amulet_nbt = py::module::import("amulet_nbt");
     auto leveldb = py::module::import("leveldb");
+
+    py::dict compiler_config;
+    compiler_config["pybind11_version"] = PYBIND11_VERSION;
+    compiler_config["compiler_id"] = COMPILER_ID;
+    compiler_config["compiler_version"] = COMPILER_VERSION;
+    m.attr("compiler_config") = compiler_config;
+
+    check_compatibility(amulet_nbt, "amulet_nbt", m, "amulet");
+    check_compatibility(leveldb, "leveldb", m, "amulet");
 
     // Submodules
     init_collections(m);
