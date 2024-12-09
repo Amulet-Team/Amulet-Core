1604aff5163bc7d4b0cbfb9665127f9ca0615287
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2024-12-09 13:52:54 +0000

    Added missing load

--- src/amulet/level/java/anvil/_region.cpp
+++ src/amulet/level/java/anvil/_region.cpp
@@ -148,6 +148,7 @@ void AnvilRegion::load()
 AMULET_CORE_DLLX std::vector<std::pair<std::int64_t, std::int64_t>> AnvilRegion::all_coords()
 {
     std::lock_guard lock(mutex);
+    load();
     std::vector<std::pair<std::int64_t, std::int64_t>> coords;
     coords.reserve(_chunk_locations.size());
     for (const auto& it : _chunk_locations) {
