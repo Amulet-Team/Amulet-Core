b4518d3b61b308563e2f402f9929884011c6efbe
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-01-08 10:51:28 +0000

    Added docstrings

--- src/amulet/level/abc/chunk_handle.pyi
+++ src/amulet/level/abc/chunk_handle.pyi
@@ -5,13 +5,62 @@ import amulet.chunk
 __all__ = ["ChunkHandle"]
 
 class ChunkHandle:
-    def delete_chunk(self) -> None: ...
-    def exists(self) -> bool: ...
-    def get_chunk(self) -> amulet.chunk.Chunk: ...
-    def set_chunk(self, chunk: amulet.chunk.Chunk) -> None: ...
+    def delete_chunk(self) -> None:
+        """
+        Delete the chunk from the level.
+        You must acquire the chunk lock before deleting.
+
+        :raises:
+            LockNotAcquired: If the chunk is already locked by another thread.
+        """
+
+    def exists(self) -> bool:
+        """
+        Does the chunk exist. This is a quick way to check if the chunk exists without loading it.
+
+        This state may change if the lock is not acquired.
+
+        :return: True if the chunk exists. Calling get on this chunk handle may still throw ChunkLoadError
+        """
+
+    def get_chunk(self) -> amulet.chunk.Chunk:
+        """
+        Get a unique copy of the chunk data.
+
+        If you want to edit the chunk, use :meth:`edit` instead.
+
+        If you only want to access/modify parts of the chunk data you can specify the components you want to load.
+        This makes it faster because you don't need to load unneeded parts.
+
+        :param components: None to load all components or an iterable of component strings to load.
+        :return: A unique copy of the chunk data.
+        """
+
+    def set_chunk(self, chunk: amulet.chunk.Chunk) -> None:
+        """
+        Overwrite the chunk data.
+        You must acquire the chunk lock before setting.
+        If you want to edit the chunk, use :meth:`edit` instead.
+
+        :param chunk: The chunk data to set.
+        :raises:
+            LockNotAcquired: If the chunk is already locked by another thread.
+        """
+
     @property
-    def cx(self) -> int: ...
+    def cx(self) -> int:
+        """
+        The chunk x coordinate.
+        """
+
     @property
-    def cz(self) -> int: ...
+    def cz(self) -> int:
+        """
+        The chunk z coordinate.
+        """
+
     @property
-    def dimension_id(self) -> str: ...
+    def dimension_id(self) -> str:
+        """
+        The dimension identifier this chunk is from.
+        """
--- src/amulet/level/abc/dimension.pyi
+++ src/amulet/level/abc/dimension.pyi
@@ -7,10 +7,28 @@ import amulet.level.abc.chunk_handle
 __all__ = ["Dimension"]
 
 class Dimension:
-    def default_biome(self) -> amulet.biome.Biome: ...
-    def default_block(self) -> amulet.block.BlockStack: ...
+    def default_biome(self) -> amulet.biome.Biome:
+        """
+        The default biome for this dimension
+        """
+
+    def default_block(self) -> amulet.block.BlockStack:
+        """
+        The default block for this dimension
+        """
+
     def get_chunk_handle(
         self, cx: int, cz: int
-    ) -> amulet.level.abc.chunk_handle.ChunkHandle: ...
+    ) -> amulet.level.abc.chunk_handle.ChunkHandle:
+        """
+        Get the chunk handle for the given chunk in this dimension.
+
+        :param cx: The chunk x coordinate to load.
+        :param cz: The chunk z coordinate to load.
+        """
+
     @property
-    def dimension_id(self) -> str: ...
+    def dimension_id(self) -> str:
+        """
+        The dimension identifier this chunk is from.
+        """
