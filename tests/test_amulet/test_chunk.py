93fe6df6da32dd132e8abc4a91835b37b0101042
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-01-14 12:40:39 +0000

    Added chunk exception tests

---
+++ tests/test_amulet/test_chunk.py
@@ -0,0 +1,30 @@
+import unittest
+
+from amulet.chunk import ChunkLoadError, ChunkDoesNotExist
+
+
+class ChunkTestCase(unittest.TestCase):
+    def test_exceptions(self) -> None:
+        with self.assertRaises(ChunkDoesNotExist):
+            raise ChunkDoesNotExist
+        with self.assertRaises(ChunkLoadError):
+            raise ChunkLoadError
+        with self.assertRaises(ChunkLoadError):
+            raise ChunkDoesNotExist
+
+        try:
+            raise ChunkLoadError
+        except ChunkDoesNotExist:
+            self.assertTrue(False)
+        except ChunkLoadError:
+            self.assertTrue(True)
+
+        try:
+            raise ChunkDoesNotExist
+        except ChunkDoesNotExist:
+            self.assertTrue(True)
+        except ChunkLoadError:
+            self.assertTrue(False)
+
+if __name__ == "__main__":
+    unittest.main()
