3dc31cda9e33e0a93afb2b9cdaf432c1fb497d8f
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-01-14 11:32:38 +0000

    Updated stubs

--- src/amulet/level/java/anvil/dimension.pyi
+++ src/amulet/level/java/anvil/dimension.pyi
@@ -9,37 +9,109 @@ import amulet_nbt
 __all__ = ["AnvilDimension", "AnvilDimensionLayer", "RawChunkType"]
 
 class AnvilDimension:
+    """
+    A class to manage the data for a dimension.
+    This can consist of multiple layers. Eg the region layer which contains chunk data and the entities layer which contains entities.
+    """
+
     def __init__(
         self,
         directory: str,
         layer_names: collections.abc.Iterable[str],
         mcc: bool = False,
     ) -> None: ...
-    def all_chunk_coords(self) -> typing.Iterator[tuple[int, int]]: ...
-    def compact(self) -> None: ...
-    def delete_chunk(self, cx: int, cz: int) -> None: ...
-    def get_chunk_data(self, cx: int, cz: int) -> dict[str, amulet_nbt.NamedTag]: ...
-    def get_layer(self, layer_name: str) -> AnvilDimensionLayer: ...
-    def has_chunk(self, cx: int, cz: int) -> bool: ...
-    def has_layer(self, layer_name: str) -> bool: ...
+    def all_chunk_coords(self) -> typing.Iterator[tuple[int, int]]:
+        """
+        Get an iterator for all the chunks that exist in this dimension.
+        """
+
+    def compact(self) -> None:
+        """
+        Defragment the region files and remove unused region files.
+        """
+
+    def delete_chunk(self, cx: int, cz: int) -> None:
+        """
+        Delete all data for the given chunk.
+        """
+
+    def get_chunk_data(self, cx: int, cz: int) -> dict[str, amulet_nbt.NamedTag]:
+        """
+        Get the data for a chunk
+        """
+
+    def get_layer(self, layer_name: str) -> AnvilDimensionLayer:
+        """
+        Get the AnvilDimensionLayer for a specific layer. The returned value must not be stored long-term.
+        """
+
+    def has_chunk(self, cx: int, cz: int) -> bool:
+        """
+        Check if a chunk exists.
+        """
+
+    def has_layer(self, layer_name: str) -> bool:
+        """
+        Check if this dimension has the requested layer.
+        """
+
     def set_chunk_data(
         self,
         cx: int,
         cz: int,
         data_layers: collections.abc.Iterable[tuple[str, amulet_nbt.NamedTag]],
-    ) -> None: ...
+    ) -> None:
+        """
+        Set the data for a chunk.
+        """
 
 class AnvilDimensionLayer:
+    """
+    A class to manage a directory of region files.
+    """
+
     def __init__(self, directory: str, mcc: bool = False) -> None: ...
-    def all_region_coords(self) -> typing.Iterator[tuple[int, int]]: ...
-    def compact(self) -> None: ...
-    def delete_chunk(self, cx: int, cz: int) -> None: ...
-    def get_chunk_data(self, cx: int, cz: int) -> amulet_nbt.NamedTag: ...
+    def all_region_coords(self) -> typing.Iterator[tuple[int, int]]:
+        """
+        An iterator of all region coordinates in this layer.
+        """
+
+    def compact(self) -> None:
+        """
+        Defragment the region files and remove unused region files.
+        """
+
+    def delete_chunk(self, cx: int, cz: int) -> None:
+        """
+        Delete the chunk data from this layer.
+        """
+
+    def get_chunk_data(self, cx: int, cz: int) -> amulet_nbt.NamedTag:
+        """
+        Get a NamedTag of a chunk from the database.
+        Will raise ChunkDoesNotExist if the region or chunk does not exist
+        """
+
     def get_region(
         self, rx: int, rz: int, create: bool = False
-    ) -> amulet.level.java.anvil.region.AnvilRegion: ...
-    def has_chunk(self, cx: int, cz: int) -> bool: ...
-    def has_region(self, rx: int, rz: int) -> bool: ...
-    def set_chunk_data(self, cx: int, cz: int, tag: amulet_nbt.NamedTag) -> None: ...
+    ) -> amulet.level.java.anvil.region.AnvilRegion:
+        """
+        Get an AnvilRegion instance. This must not be stored long-term.
+        """
+
+    def has_chunk(self, cx: int, cz: int) -> bool:
+        """
+        Check if the chunk has data in this layer.
+        """
+
+    def has_region(self, rx: int, rz: int) -> bool:
+        """
+        Check if a region file exists in this layer.
+        """
+
+    def set_chunk_data(self, cx: int, cz: int, tag: amulet_nbt.NamedTag) -> None:
+        """
+        Set the chunk data for this layer.
+        """
 
 RawChunkType: typing.TypeAlias = dict[str, amulet_nbt.NamedTag]
