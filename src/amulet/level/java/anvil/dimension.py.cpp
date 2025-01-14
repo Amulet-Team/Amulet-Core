67b101acf6e0070f6f7878c0ae85047dc7c3e50a
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-01-14 13:04:45 +0000

    Added all_chunk_coords to layer

--- src/amulet/level/java/anvil/dimension.py.cpp
+++ src/amulet/level/java/anvil/dimension.py.cpp
@@ -41,6 +41,14 @@ py::module init_anvil_dimension(py::module m_parent)
         py::arg("create") = false,
         py::doc("Get an AnvilRegion instance. This must not be stored long-term."));
     AnvilDimensionLayer.def(
+        "all_chunk_coords",
+        [](std::shared_ptr<Amulet::AnvilDimensionLayer> self) {
+            return py::make_iterator(
+                Amulet::AnvilChunkCoordIterator(std::move(self)),
+                Amulet::AnvilChunkCoordIterator());
+        },
+        py::doc("An iterator of all chunk coordinates in this layer."));
+    AnvilDimensionLayer.def(
         "has_chunk",
         &Amulet::AnvilDimensionLayer::has_chunk,
         py::arg("cx"),
