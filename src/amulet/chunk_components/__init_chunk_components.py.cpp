cee7d6f9ee88f34b63eaa04bf2040f7b78351b6a
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-01-14 16:22:23 +0000

    Converted chunk_component modules to packages
    It makes sense to group the python stub file with the source files

--- src/amulet/chunk_components/__init_chunk_components.py.cpp
+++ src/amulet/chunk_components/__init_chunk_components.py.cpp
@@ -1,11 +1,12 @@
 #include <pybind11/pybind11.h>
+#include <pybind11_extensions/py_module.hpp>
 namespace py = pybind11;
 
 void init_section_array_map(py::module);
 void init_block_component(py::module);
 
 void init_chunk_components(py::module m_parent) {
-    auto m = m_parent.def_submodule("chunk_components");
+    auto m = pybind11_extensions::def_subpackage(m_parent, "chunk_components");
     init_section_array_map(m);
     init_block_component(m);
 }
--- src/amulet/level/java/chunk_components/__init_java_chunk_components.py.cpp
+++ src/amulet/level/java/chunk_components/__init_java_chunk_components.py.cpp
@@ -1,13 +1,14 @@
 #include <string>
 #include <vector>
 #include <pybind11/pybind11.h>
+#include <pybind11_extensions/py_module.hpp>
 namespace py = pybind11;
 
 void init_data_version_component(py::module);
 void init_java_raw_chunk_component(py::module);
 
 void init_java_chunk_components(py::module m_parent) {
-    auto m = m_parent.def_submodule("chunk_components");
+    auto m = pybind11_extensions::def_subpackage(m_parent, "chunk_components");
     init_data_version_component(m);
     init_java_raw_chunk_component(m);
 }
