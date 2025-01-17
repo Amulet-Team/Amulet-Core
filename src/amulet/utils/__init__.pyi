c537349ce512b94a8e48c7c97367fea9aece665f
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-01-17 10:30:54 +0000

    Added mutex bindings

--- src/amulet/utils/__init__.pyi
+++ src/amulet/utils/__init__.pyi
@@ -1,5 +1,5 @@
 from __future__ import annotations
 
-from . import numpy, task_manager
+from . import mutex, numpy, task_manager
 
-__all__ = ["numpy", "task_manager"]
+__all__ = ["mutex", "numpy", "task_manager"]
---
+++ src/amulet/utils/mutex.pyi
@@ -0,0 +1,48 @@
+from __future__ import annotations
+
+import datetime
+
+import amulet.utils.task_manager.cancel_manager
+
+__all__ = ["OrderedSharedMutex", "OrderedSharedTimedMutex"]
+
+class OrderedSharedMutex:
+    @staticmethod
+    def __repr__() -> str: ...
+    def __init__(self) -> None: ...
+    def lock(
+        self,
+        cancel_manager: amulet.utils.task_manager.cancel_manager.AbstractCancelManager = ...,
+    ) -> None: ...
+    def lock_shared(
+        self,
+        cancel_manager: amulet.utils.task_manager.cancel_manager.AbstractCancelManager = ...,
+    ) -> None: ...
+    def try_lock(self) -> bool: ...
+    def try_lock_shared(self) -> bool: ...
+    def unlock(self) -> None: ...
+    def unlock_shared(self) -> None: ...
+
+class OrderedSharedTimedMutex(OrderedSharedMutex):
+    @staticmethod
+    def __repr__() -> str: ...
+    def try_lock_for(
+        self,
+        timeout_duration: datetime.timedelta,
+        cancel_manager: amulet.utils.task_manager.cancel_manager.AbstractCancelManager = ...,
+    ) -> bool: ...
+    def try_lock_shared_for(
+        self,
+        timeout_duration: datetime.timedelta,
+        cancel_manager: amulet.utils.task_manager.cancel_manager.AbstractCancelManager = ...,
+    ) -> bool: ...
+    def try_lock_shared_until(
+        self,
+        timeout_time: datetime.datetime,
+        cancel_manager: amulet.utils.task_manager.cancel_manager.AbstractCancelManager = ...,
+    ) -> bool: ...
+    def try_lock_until(
+        self,
+        timeout_time: datetime.datetime,
+        cancel_manager: amulet.utils.task_manager.cancel_manager.AbstractCancelManager = ...,
+    ) -> bool: ...
