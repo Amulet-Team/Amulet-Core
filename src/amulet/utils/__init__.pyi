4331b9bed58ca47ac4611c8640dd718f4e01946d
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-01-20 10:45:47 +0000

    Added lock stub file

--- src/amulet/utils/__init__.pyi
+++ src/amulet/utils/__init__.pyi
@@ -1,5 +1,5 @@
 from __future__ import annotations
 
-from . import mutex, numpy, task_manager
+from . import lock, mutex, numpy, task_manager
 
-__all__ = ["mutex", "numpy", "task_manager"]
+__all__ = ["lock", "mutex", "numpy", "task_manager"]
