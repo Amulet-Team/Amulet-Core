62e1e86c0e78878a886de1cec2c213d95619010e
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-01-20 15:58:04 +0000

    Added Deadlock docstring

--- src/amulet/utils/mutex.py.cpp
+++ src/amulet/utils/mutex.py.cpp
@@ -15,7 +15,9 @@ void init_mutex(py::module m_parent)
 
     std::string module_name = m.attr("__name__").cast<std::string>();
 
-    py::register_exception<Amulet::Deadlock>(m, "Deadlock", PyExc_RuntimeError);
+    auto Deadlock = py::register_exception<Amulet::Deadlock>(m, "Deadlock", PyExc_RuntimeError);
+    Deadlock.doc() = "This exception signals that a deadlock occurred when locking a mutex.\n"
+                     "Not all deadlock cases raise an exception.";
 
     py::class_<Amulet::OrderedSharedMutex, std::shared_ptr<Amulet::OrderedSharedMutex>> OrderedSharedMutex(m, "OrderedSharedMutex");
     OrderedSharedMutex.def(py::init<>());
