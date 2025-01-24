6ac722cb5d545e4fbca0568e454586fa0145284b
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-01-24 14:25:27 +0000

    Added text constructor

--- tests/test_amulet/test_chunk_.py.cpp
+++ tests/test_amulet/test_chunk_.py.cpp
@@ -7,6 +7,6 @@ namespace py = pybind11;
 
 void init_test_chunk(py::module m_parent){
     auto m = m_parent.def_submodule("test_chunk_");
-    m.def("throw_chunk_does_not_exist", [](){ throw Amulet::ChunkDoesNotExist(); });
-    m.def("throw_chunk_load_error", [](){ throw Amulet::ChunkLoadError(); });
+    m.def("throw_chunk_does_not_exist", [](){ throw Amulet::ChunkDoesNotExist("ChunkDoesNotExist"); });
+    m.def("throw_chunk_load_error", [](){ throw Amulet::ChunkLoadError("ChunkLoadError"); });
 }
