b16d28a9c6ffaa610e94adc0e68420a3b6e7f049
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-01-27 11:25:01 +0000

    Reformatted

--- tests/test_amulet/test_level/test_java/test_anvil/test_region.py
+++ tests/test_amulet/test_level/test_java/test_anvil/test_region.py
@@ -11,7 +11,9 @@ from amulet.chunk import ChunkDoesNotExist
 from amulet.level.java.anvil import AnvilRegion, RegionDoesNotExist
 import tests.data.worlds_src
 import tests.data.region
-from tests.test_amulet.test_level.test_java.test_anvil.test_region_ import throw_region_does_not_exist
+from tests.test_amulet.test_level.test_java.test_anvil.test_region_ import (
+    throw_region_does_not_exist,
+)
 
 
 class AnvilRegionTestCase(unittest.TestCase):
--- tests/test_amulet/test_util/test_task_manager.py
+++ tests/test_amulet/test_util/test_task_manager.py
@@ -11,7 +11,10 @@ from amulet.utils.task_manager import (
     VoidProgressManager,
 )
 
-from tests.test_amulet.test_util.test_task_manager_ import throw_task_cancelled, cpp_test_task_cancelled
+from tests.test_amulet.test_util.test_task_manager_ import (
+    throw_task_cancelled,
+    cpp_test_task_cancelled,
+)
 
 
 class CancelManagerTestCase(TestCase):
