052d94fab864ba7a65f3e235a7a71a56f2e180bc
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-02-07 09:35:55 +0000

    Added WIP level class

---
+++ src/amulet/level/java/raw_level.cpp
@@ -0,0 +1,188 @@
+#include <bit>
+#include <filesystem>
+#include <fstream>
+#include <memory>
+#include <stdexcept>
+#include <variant>
+
+#include <amulet_nbt/nbt_encoding/binary.hpp>
+#include <amulet_nbt/string_encoding.hpp>
+#include <amulet_nbt/tag/copy.hpp>
+#include <amulet_nbt/zlib.hpp>
+
+#include "raw_level.hpp"
+
+namespace Amulet {
+
+std::unique_ptr<JavaRawLevel> JavaRawLevel::load(const std::filesystem::path& path)
+{
+    if (!std::filesystem::is_directory(path)) {
+        throw std::invalid_argument("path must be a directory.");
+    }
+    auto self = std::unique_ptr<JavaRawLevel>(new JavaRawLevel(path));
+    self->reload_metadata();
+    return self;
+}
+
+// Create a new Java level and create a JavaRawLevel instance for it.
+std::unique_ptr<JavaRawLevel> JavaRawLevel::create(const JavaCreateArgsV1&)
+{
+    throw std::runtime_error("NotImplementedError");
+}
+
+OrderedMutex& JavaRawLevel::mutex()
+{
+    return _public_mutex;
+}
+
+bool JavaRawLevel::is_open() const
+{
+    return bool(_raw_open_data);
+}
+
+void JavaRawLevel::reload_metadata()
+{
+    if (is_open()) {
+        throw std::runtime_error("Cannot reload metadata while the level is open.");
+    }
+
+    // Load the level.dat
+    auto level_dat_path = _path / "level.dat";
+    // Open the file
+    std::ifstream level_dat_f(level_dat_path, std::ios::in | std::ios::binary);
+    if (!level_dat_f) {
+        throw std::runtime_error("Could not open file " + level_dat_path.string());
+    }
+    // Find the file length
+    level_dat_f.seekg(0, std::ios::end);
+    size_t level_dat_size = level_dat_f.tellg();
+    level_dat_f.seekg(0);
+    // Read the file
+    std::string level_dat(level_dat_size, 0);
+    level_dat_f.read(&level_dat[0], level_dat_size);
+    // Decompress the file
+    std::string decompressed_level_dat;
+    AmuletNBT::decompress_zlib_gzip(level_dat, decompressed_level_dat);
+    // Decode the binary NBT.
+    _level_dat = AmuletNBT::decode_nbt(decompressed_level_dat, std::endian::big, AmuletNBT::mutf8_to_utf8);
+
+    // Set the data version.
+    try {
+        auto& root = std::get<AmuletNBT::CompoundTagPtr>(_level_dat.tag_node);
+        auto& data = std::get<AmuletNBT::CompoundTagPtr>(root->at("Data"));
+        auto& data_version = std::get<AmuletNBT::IntTag>(root->at("DataVersion"));
+        _data_version = { data_version.value };
+    } catch (...) {
+        _data_version = { -1 };
+    }
+}
+
+void JavaRawLevel::open()
+{
+    throw std::runtime_error("NotImplementedError");
+}
+
+void JavaRawLevel::close()
+{
+    throw std::runtime_error("NotImplementedError");
+}
+
+const std::filesystem::path& JavaRawLevel::get_path() const
+{
+    return _path;
+}
+
+AmuletNBT::NamedTag JavaRawLevel::get_level_dat() const
+{
+    return AmuletNBT::deep_copy(_level_dat);
+}
+
+void JavaRawLevel::set_level_dat(const AmuletNBT::NamedTag&)
+{
+    throw std::runtime_error("NotImplementedError");
+}
+
+std::string JavaRawLevel::get_platform() const
+{
+    return "java";
+}
+
+const VersionNumber& JavaRawLevel::get_data_version() const
+{
+    return _data_version;
+}
+
+void JavaRawLevel::set_data_version()
+{
+    throw std::runtime_error("NotImplementedError");
+}
+
+std::chrono::system_clock::time_point JavaRawLevel::modified_time() const
+{
+
+    throw std::runtime_error("NotImplementedError");
+}
+
+std::string JavaRawLevel::level_name() const
+{
+    throw std::runtime_error("NotImplementedError");
+}
+
+void JavaRawLevel::set_level_name(const std::string&)
+{
+    throw std::runtime_error("NotImplementedError");
+}
+
+JavaRawLevelOpenData& JavaRawLevel::_find_dimensions()
+{
+    auto& raw_open = _get_raw_open();
+    std::unique_lock lock(raw_open.dimensions_mutex);
+    return raw_open;
+}
+
+std::vector<std::string> JavaRawLevel::get_dimension_ids()
+{
+    auto& raw_open = _find_dimensions();
+    std::shared_lock lock(raw_open.dimensions_mutex);
+    std::vector<DimensionID> dimension_ids;
+    dimension_ids.reserve(raw_open.dimension_ids.size());
+    for (auto& [dimension_id, _] : raw_open.dimension_ids) {
+        dimension_ids.push_back(dimension_id);
+    }
+    return dimension_ids;
+}
+
+std::shared_ptr<JavaRawDimension> JavaRawLevel::get_dimension(const DimensionID& dimension_id)
+{
+    auto& raw_open = _find_dimensions();
+    std::shared_lock lock(raw_open.dimensions_mutex);
+    auto it = raw_open.dimension_ids.find(dimension_id);
+    JavaInternalDimensionID internal_dimension_id = (it == raw_open.dimension_ids.end()) ? dimension_id : it->second;
+    auto it2 = raw_open.dimensions.find(internal_dimension_id);
+    if (it2 == raw_open.dimensions.end()) {
+        throw std::invalid_argument("Dimension " + dimension_id + " does not exist.");
+    }
+    return it2->second;
+}
+
+void JavaRawLevel::compact()
+{
+    auto& raw_open = _find_dimensions();
+    std::shared_lock dimensions_lock(raw_open.dimensions_mutex);
+    for (const auto& [dimension_id, dimension] : raw_open.dimensions) {
+        std::unique_lock dimesion_lock(dimension->mutex());
+        dimension->compact();
+    }
+}
+
+std::shared_ptr<IdRegistry> JavaRawLevel::get_block_id_override()
+{
+    return _get_raw_open().block_id_override;
+}
+
+std::shared_ptr<IdRegistry> JavaRawLevel::get_biome_id_override()
+{
+    return _get_raw_open().biome_id_override;
+}
+
+} // namespace Amulet
--- src/amulet/level/java/raw_level.hpp
+++ src/amulet/level/java/raw_level.hpp
@@ -2,14 +2,17 @@
 
 #include <chrono>
 #include <filesystem>
+#include <map>
 #include <memory>
+#include <shared_mutex>
+#include <stdexcept>
 
 #include <amulet_nbt/tag/named_tag.hpp>
 
 #include <amulet/dll.hpp>
+#include <amulet/level/abc/registry.hpp>
 #include <amulet/utils/mutex.hpp>
 #include <amulet/version.hpp>
-#include <amulet/level/abc/registry.hpp>
 
 #include "dimension.hpp"
 #include "raw_dimension.hpp"
@@ -23,32 +26,134 @@ struct JavaCreateArgsV1 {
     std::string level_name;
 };
 
+class JavaRawLevelOpenData {
+public:
+    // TODO: lock_file
+    // TODO: lock_time
+    // TODO: data_pack
+    std::shared_mutex dimensions_mutex;
+    std::map<JavaInternalDimensionID, std::shared_ptr<JavaRawDimension>> dimensions;
+    std::map<DimensionID, JavaInternalDimensionID> dimension_ids;
+    std::shared_ptr<IdRegistry> block_id_override;
+    std::shared_ptr<IdRegistry> biome_id_override;
+};
+
 class JavaRawLevel {
 private:
-    Amulet::OrderedSharedTimedMutex mutex;
+    OrderedMutex _public_mutex;
+    std::filesystem::path _path;
+    AmuletNBT::NamedTag _level_dat;
+    VersionNumber _data_version;
+
+    // Data that is only valid when the level is open.
+    // The external unique lock must be held to change this pointer.
+    std::unique_ptr<JavaRawLevelOpenData> _raw_open_data;
+
+    // Construct a new instance. Path is the directory containing the level.dat file.
+    JavaRawLevel(const std::filesystem::path path)
+        : _path(path)
+        , _level_dat("", std::make_shared<AmuletNBT::CompoundTag>())
+        , _data_version({})
+    {
+    }
+
+    // Validate _raw_open_data is valid and return a reference.
+    // External shared read lock required.
+    JavaRawLevelOpenData& _get_raw_open()
+    {
+        if (!_raw_open_data) {
+            throw std::runtime_error("The level is not open.");
+        }
+        return *_raw_open_data;
+    }
+
+    JavaRawLevelOpenData& _find_dimensions();
 
 public:
+    JavaRawLevel() = delete;
+    JavaRawLevel(const JavaRawLevel&) = delete;
+    JavaRawLevel(JavaRawLevel&&) = default;
+
+    // Create a new JavaRawLevel instance from the data at the path.
     AMULET_CORE_EXPORT static std::unique_ptr<JavaRawLevel> load(const std::filesystem::path&);
+
+    // Create a new Java level and create a JavaRawLevel instance for it.
     AMULET_CORE_EXPORT static std::unique_ptr<JavaRawLevel> create(const JavaCreateArgsV1&);
 
-    AMULET_CORE_EXPORT bool is_open() const;
-    AMULET_CORE_EXPORT void reload();
-    AMULET_CORE_EXPORT void open();
-    AMULET_CORE_EXPORT void close();
-    AMULET_CORE_EXPORT const std::filesystem::path path() const;
-    AMULET_CORE_EXPORT AmuletNBT::NamedTag get_level_dat() const;
-    AMULET_CORE_EXPORT void set_level_dat(const AmuletNBT::NamedTag&);
-    AMULET_CORE_EXPORT std::string platform();
-    AMULET_CORE_EXPORT const VersionNumber& data_version();
-    AMULET_CORE_EXPORT std::chrono::system_clock::time_point modified_time();
-    AMULET_CORE_EXPORT std::string level_name();
-    AMULET_CORE_EXPORT void set_level_name(const std::string&);
-    AMULET_CORE_EXPORT std::vector<std::string> dimension_ids();
-    AMULET_CORE_EXPORT std::shared_ptr<JavaRawDimension> get_dimension();
-    AMULET_CORE_EXPORT void compact();
-    AMULET_CORE_EXPORT std::shared_ptr<IdRegistry> get_block_id_override();
-    AMULET_CORE_EXPORT std::shared_ptr<IdRegistry> get_biome_id_override();
+    // External mutex
+    // Thread safe.
+    AMULET_CORE_EXPORT OrderedMutex& mutex();
 
+    // Is the level open.
+    // External shared read lock required.
+    AMULET_CORE_EXPORT bool is_open() const;
+
+    // Reload the metadata. This can only be called when the level is closed.
+    // External unique lock required.
+    AMULET_CORE_EXPORT void reload_metadata();
+
+    // Open the level.
+    // External unique lock required.
+    AMULET_CORE_EXPORT void open();
+
+    // Close the level.
+    // External unique lock required.
+    AMULET_CORE_EXPORT void close();
+
+    // The path to the level directory.
+    // Thread safe.
+    AMULET_CORE_EXPORT const std::filesystem::path& get_path() const;
+
+    // The NamedTag stored in the level.dat file. Returns a unique copy.
+    // External shared read lock required.
+    AMULET_CORE_EXPORT AmuletNBT::NamedTag get_level_dat() const;
+
+    // Set the level.dat NamedTag
+    // External unique lock required.
+    AMULET_CORE_EXPORT void set_level_dat(const AmuletNBT::NamedTag&);
+
+    // The platform identifier. "java"
+    // Thread safe.
+    AMULET_CORE_EXPORT std::string get_platform() const;
+
+    // The game data version that the level was last opened in.
+    // External shared read lock required.
+    const VersionNumber& get_data_version() const;
+
+    // Set the maximum game version.
+    // If the game version is different this will close and re-open the level.
+    // External unique lock required.
+    void set_data_version();
+
+    // The time when the level was lasted edited.
+    AMULET_CORE_EXPORT std::chrono::system_clock::time_point modified_time() const;
+
+    // The name of the level.
+    AMULET_CORE_EXPORT std::string level_name() const;
+
+    // Set the level name.
+    AMULET_CORE_EXPORT void set_level_name(const std::string&);
+
+    // The identifiers for all dimensions in this level.
+    // External shared read lock required.
+    // External shared read-only lock optional.
+    AMULET_CORE_EXPORT std::vector<std::string> get_dimension_ids();
+
+    // Get the raw dimension object for a specific dimension.
+    // External shared read lock required.
+    AMULET_CORE_EXPORT std::shared_ptr<JavaRawDimension> get_dimension(const DimensionID&);
+
+    // Compact the level.
+    // Thread safe.
+    AMULET_CORE_EXPORT void compact();
+
+    // Overridden block ids.
+    // External shared read lock required.
+    AMULET_CORE_EXPORT std::shared_ptr<IdRegistry> get_block_id_override();
+
+    // Overridden biome ids.
+    // External shared read lock required.
+    AMULET_CORE_EXPORT std::shared_ptr<IdRegistry> get_biome_id_override();
 };
 
 } // namespace Amulet
