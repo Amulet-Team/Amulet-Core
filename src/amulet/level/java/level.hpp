fd7b9b16e6bb57116be24aae4f5fae583e25c089
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-02-19 13:21:20 +0000

    Added load and create declarations

--- src/amulet/level/java/level.hpp
+++ src/amulet/level/java/level.hpp
@@ -1,11 +1,21 @@
 #pragma once
 
+#include <filesystem>
+
 #include <amulet/level/abc/level.hpp>
+#include "raw_level.hpp"
 
 namespace Amulet {
 
 class JavaLevel : public Level {
+public:
+    // Load an existing Java level from the given directory.
+    // Thread safe.
+    AMULET_CORE_EXPORT static std::unique_ptr<JavaLevel> load(const std::filesystem::path&);
 
+    // Create a new Java level at the given directory.
+    // Thread safe.
+    AMULET_CORE_EXPORT static std::unique_ptr<JavaLevel> create(const JavaCreateArgsV1&);
 };
 
 } // namespace Amulet
