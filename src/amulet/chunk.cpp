eb8af129da3be75557b406487c26766e7f062de0
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-02-19 15:23:17 +0000

    Fixed chunk constructor initialisation

--- src/amulet/chunk.cpp
+++ src/amulet/chunk.cpp
@@ -9,10 +9,15 @@
 
 namespace Amulet {
 namespace detail {
-    std::unordered_map<std::string, std::function<std::shared_ptr<Chunk>()>> chunk_constructors;
+    std::unordered_map<std::string, std::function<std::shared_ptr<Chunk>()>>& get_chunk_constructors()
+    {
+        static std::unordered_map<std::string, std::function<std::shared_ptr<Chunk>()>> chunk_constructors;
+        return chunk_constructors;
+    }
+
 }
 std::shared_ptr<Chunk> get_null_chunk(std::string chunk_id)
 {
-    return detail::chunk_constructors.at(chunk_id)();
+    return detail::get_chunk_constructors().at(chunk_id)();
 }
 }
