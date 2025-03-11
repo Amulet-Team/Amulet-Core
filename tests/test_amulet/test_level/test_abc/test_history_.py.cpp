8d015f2b62b0aa8f3db440ddf21beddd8c084ec2
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-03-11 15:54:57 +0000

    Added history tests

--- tests/test_amulet/test_level/test_abc/test_history_.py.cpp
+++ tests/test_amulet/test_level/test_abc/test_history_.py.cpp
@@ -10,7 +10,7 @@ private:
 
 public:
     TestResourceId(const std::string& key)
-        : key(key) { };
+        : key(key) {};
     auto operator<=>(const TestResourceId&) const = default;
     operator std::string() const
     {
@@ -19,11 +19,31 @@ public:
 };
 
 template <typename T>
-void assert_equal(const T& a, const T& b, const std::string& msg) {
+void assert_equal(const T& a, const T& b, size_t line)
+{
     if (a != b) {
-        throw std::runtime_error("Values are not equal " + msg);
+        throw std::runtime_error("Values are not equal. Line: " + std::to_string(line));
     }
 }
+
+#define ASSERT_EQUAL(CLS, A, B)                                                                                    \
+    {                                                                                                              \
+        CLS a;                                                                                                     \
+        try {                                                                                                      \
+            a = A;                                                                                                 \
+        } catch (const std::exception& e) {                                                                        \
+            throw std::runtime_error("Failed evaluating A at line " + std::to_string(__LINE__) + ". " + e.what()); \
+        }                                                                                                          \
+        CLS b;                                                                                                     \
+        try {                                                                                                      \
+            b = B;                                                                                                 \
+        } catch (const std::exception& e) {                                                                        \
+            throw std::runtime_error("Failed evaluating B at line " + std::to_string(__LINE__) + ". " + e.what()); \
+        }                                                                                                          \
+        if (a != b) {                                                                                              \
+            throw std::runtime_error("Values are not equal. Line: " + std::to_string(__LINE__));                   \
+        }                                                                                                          \
+    }
 
 void init_test_history(py::module m_parent)
 {
@@ -31,15 +51,15 @@ void init_test_history(py::module m_parent)
     m.def("test_history", []() {
         // Create the history manager.
         Amulet::HistoryManager history_manager;
-        
+
         // Create two layers.
         auto layer_1 = history_manager.new_layer<TestResourceId>();
         auto layer_2 = history_manager.new_layer<TestResourceId>();
-        
+
         // Create two keys.
         TestResourceId key_1("key_1");
         TestResourceId key_2("key_2");
-        
+
         // Set initial values.
         layer_1->set_initial_value(key_1, "value_1_1");
         layer_1->set_initial_value(key_2, "value_1_2");
@@ -47,9 +67,55 @@ void init_test_history(py::module m_parent)
         layer_2->set_initial_value(key_2, "value_2_2");
 
         // Get initial values.
-        assert_equal<std::string>("value_1_1", layer_1->get_value(key_1), "value_1_1");
-        assert_equal<std::string>("value_1_2", layer_1->get_value(key_2), "value_1_2");
-        assert_equal<std::string>("value_2_1", layer_2->get_value(key_1), "value_2_1");
-        assert_equal<std::string>("value_2_2", layer_2->get_value(key_2), "value_2_2");
+        ASSERT_EQUAL(std::string, "value_1_1", layer_1->get_value(key_1));
+        ASSERT_EQUAL(std::string, "value_1_2", layer_1->get_value(key_2));
+        ASSERT_EQUAL(std::string, "value_2_1", layer_2->get_value(key_1));
+        ASSERT_EQUAL(std::string, "value_2_2", layer_2->get_value(key_2));
+
+        // Create a new undo point.
+        history_manager.create_undo_bin();
+
+        // Overwrite values
+        layer_1->set_value(key_2, "value_1_2b");
+        layer_2->set_value(key_2, "value_2_2b");
+
+        // Set new values
+        TestResourceId key_3("key_3");
+        layer_1->set_initial_value(key_3, "value_1_3");
+        layer_2->set_initial_value(key_3, "value_2_3");
+
+        // Validate
+        ASSERT_EQUAL(std::string, "value_1_1", layer_1->get_value(key_1));
+        ASSERT_EQUAL(std::string, "value_1_2b", layer_1->get_value(key_2));
+        ASSERT_EQUAL(std::string, "value_1_3", layer_1->get_value(key_3));
+        ASSERT_EQUAL(std::string, "value_2_1", layer_2->get_value(key_1));
+        ASSERT_EQUAL(std::string, "value_2_2b", layer_2->get_value(key_2));
+        ASSERT_EQUAL(std::string, "value_2_3", layer_2->get_value(key_3));
+
+        // Undo and validate
+        history_manager.undo();
+        ASSERT_EQUAL(std::string, "value_1_1", layer_1->get_value(key_1));
+        ASSERT_EQUAL(std::string, "value_1_2", layer_1->get_value(key_2));
+        ASSERT_EQUAL(std::string, "value_2_1", layer_2->get_value(key_1));
+        ASSERT_EQUAL(std::string, "value_2_2", layer_2->get_value(key_2));
+
+        // Redo and validate
+        history_manager.redo();
+        ASSERT_EQUAL(std::string, "value_1_1", layer_1->get_value(key_1));
+        ASSERT_EQUAL(std::string, "value_1_2b", layer_1->get_value(key_2));
+        ASSERT_EQUAL(std::string, "value_1_3", layer_1->get_value(key_3));
+        ASSERT_EQUAL(std::string, "value_2_1", layer_2->get_value(key_1));
+        ASSERT_EQUAL(std::string, "value_2_2b", layer_2->get_value(key_2));
+        ASSERT_EQUAL(std::string, "value_2_3", layer_2->get_value(key_3));
+
+        // Undo and create a new undo point
+        history_manager.undo();
+        history_manager.create_undo_bin();
+
+        // Validate
+        ASSERT_EQUAL(std::string, "value_1_1", layer_1->get_value(key_1));
+        ASSERT_EQUAL(std::string, "value_1_2", layer_1->get_value(key_2));
+        ASSERT_EQUAL(std::string, "value_2_1", layer_2->get_value(key_1));
+        ASSERT_EQUAL(std::string, "value_2_2", layer_2->get_value(key_2));
     });
 }
