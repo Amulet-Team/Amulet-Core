c8845f10f4f46a52ef9abada8fa89d1f9eec7278
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-03-13 15:24:03 +0000

    Added docstrings to temp.hpp

--- src/amulet/utils/temp.hpp
+++ src/amulet/utils/temp.hpp
@@ -8,6 +8,9 @@
 
 namespace Amulet {
 
+// Get the directory in which temporary directories will be created.
+// This is configurable by setting the "CACHE_DIR" environment variable.
+// Thread safe.
 std::filesystem::path get_temp_dir();
 
 // A temporary directory to do with as you wish.
@@ -17,8 +20,16 @@ private:
     std::unique_ptr<Amulet::LockFile> _lock;
 
 public:
+    // Construct a new temporary directory.
+    // Thread safe.
     TempDir(const std::string& group);
+
+    // TempDir destructor.
+    // This automatically deletes the temporary directory.
     ~TempDir();
+
+    // Get the path of the temporary directory.
+    // Thread safe.
     const std::filesystem::path& get_path() const;
 };
 
