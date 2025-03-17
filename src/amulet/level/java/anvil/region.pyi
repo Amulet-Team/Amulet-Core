976324bbae277b4df79b6f3374e7cf719b35a08a
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-03-17 13:33:51 +0000

    Update stubs

--- src/amulet/level/java/anvil/region.pyi
+++ src/amulet/level/java/anvil/region.pyi
@@ -16,16 +16,35 @@ class AnvilRegion:
     """
 
     class FileCloser:
-        pass
+        """
+        A class to manage closing the region file.
+        When the instance is deleted the region file will be closed.
+        The region file can be manually closed before this is deleted.
+        """
 
     @typing.overload
     def __init__(
         self, directory: str, file_name: str, rx: int, rz: int, mcc: bool = False
-    ) -> None: ...
+    ) -> None:
+        """
+        Construct from the directory path, name of the file and region coordinates.
+        """
+
     @typing.overload
-    def __init__(self, directory: str, rx: int, rz: int, mcc: bool = False) -> None: ...
+    def __init__(self, directory: str, rx: int, rz: int, mcc: bool = False) -> None:
+        """
+        Construct from the directory path and region coordinates.
+        File name is computed from region coordinates.
+        """
+
     @typing.overload
-    def __init__(self, path: str, mcc: bool = False) -> None: ...
+    def __init__(self, path: str, mcc: bool = False) -> None:
+        """
+        Construct from the path to the region file.
+        Coordinates are computed from the file name.
+        File name must match "r.X.Z.mca".
+        """
+
     def close(self) -> None:
         """
         Close the file object if open.
@@ -46,21 +65,21 @@ class AnvilRegion:
         Is the coordinate in the region.
         This returns true even if there is no value for the coordinate.
         Coordinates are in world space.
-        Thread safe.
+        External Read:SharedRead lock optional.
         """
 
     def delete_batch(self, coords: list[tuple[int, int]]) -> None:
         """
         Delete multiple chunk's data.
         Coordinates are in world space.
-        External shared read-write lock required.
+        External ReadWrite:SharedReadWrite lock required.
         """
 
     def delete_value(self, cx: int, cz: int) -> None:
         """
         Delete the chunk data.
         Coordinates are in world space.
-        External shared read-write lock required.
+        External ReadWrite:SharedReadWrite lock required.
         """
 
     def destroy(self) -> None:
@@ -68,15 +87,15 @@ class AnvilRegion:
         Destroy the instance.
         Calls made after this will fail.
         This may only be called by the owner of the instance.
-        External unique lock required.
+        External ReadWrite:UniqueLock required.
         """
 
     def get_coords(self) -> list[tuple[int, int]]:
         """
         Get the coordinates of all values in the region file.
         Coordinates are in world space.
-        External shared read lock required.
-        External shared read-only lock optional.
+        External Read:SharedReadWrite lock required.
+        External Read:SharedRead lock optional.
         """
 
     def get_file_closer(self) -> AnvilRegion.FileCloser:
@@ -92,22 +111,22 @@ class AnvilRegion:
         """
         Get the value for this coordinate.
         Coordinates are in world space.
-        External shared read lock required.
+        External Read:SharedReadWrite lock required.
         """
 
     def has_value(self, cx: int, cz: int) -> bool:
         """
         Is there a value stored for this coordinate.
         Coordinates are in world space.
-        External shared read lock required.
-        External shared read-only lock optional.
+        External Read:SharedReadWrite lock required.
+        External Read:SharedRead lock optional.
         """
 
     def set_value(self, cx: int, cz: int, tag: amulet_nbt.NamedTag) -> None:
         """
         Set the value for this coordinate.
         Coordinates are in world space.
-        External shared read-write lock required.
+        External ReadWrite:SharedReadWrite lock required.
         """
 
     @property
