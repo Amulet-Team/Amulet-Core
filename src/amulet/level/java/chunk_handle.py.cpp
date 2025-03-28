9a557427c5077d0362bd7b3b30b3c52eea6887e5
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-03-28 17:04:57 +0000

    Added set_chunk overload
    pybind11 stubgen does not add overloads for methods defined in the base class

--- src/amulet/level/java/chunk_handle.py.cpp
+++ src/amulet/level/java/chunk_handle.py.cpp
@@ -24,13 +24,34 @@ py::module init_java_chunk_handle(py::module m_parent)
         py::is_method(JavaChunkHandle),
         py::call_guard<py::gil_scoped_release>(),
         py::doc("Get a unique copy of the chunk data."));
-    JavaChunkHandle.attr("set_chunk") = py::cpp_function(
+    JavaChunkHandle.def(
+        "set_chunk",
         &Amulet::JavaChunkHandle::set_java_chunk,
-        py::name("set_chunk"),
-        py::is_method(JavaChunkHandle),
+        py::arg("chunk"),
+        py::prepend(),
+        py::call_guard<py::gil_scoped_release>(),
+        py::doc(
+            "Overwrite the chunk data.\n"
+            "You must acquire the chunk lock before setting.\n"
+            "If you want to edit the chunk, use :meth:`edit` instead.\n"
+            "\n"
+            ":param chunk: The chunk data to set.\n"
+            ":raises:\n"
+            "    LockNotAcquired: If the chunk is already locked by another thread."));
+    // This is here to appease mypy.
+    JavaChunkHandle.def(
+        "set_chunk",
+        &Amulet::JavaChunkHandle::set_chunk,
         py::arg("chunk"),
         py::call_guard<py::gil_scoped_release>(),
-        py::doc("Overwrite the chunk data."));
+        py::doc(
+            "Overwrite the chunk data.\n"
+            "You must acquire the chunk lock before setting.\n"
+            "If you want to edit the chunk, use :meth:`edit` instead.\n"
+            "\n"
+            ":param chunk: The chunk data to set.\n"
+            ":raises:\n"
+            "    LockNotAcquired: If the chunk is already locked by another thread."));
 
     return m;
 }
--- src/amulet/level/java/chunk_handle.pyi
+++ src/amulet/level/java/chunk_handle.pyi
@@ -1,5 +1,8 @@
 from __future__ import annotations
 
+import typing
+
+import amulet.chunk
 import amulet.level.abc.chunk_handle
 import amulet.level.java.chunk
 
@@ -11,7 +14,26 @@ class JavaChunkHandle(amulet.level.abc.chunk_handle.ChunkHandle):
         Get a unique copy of the chunk data.
         """
 
+    @typing.overload
     def set_chunk(self, chunk: amulet.level.java.chunk.JavaChunk) -> None:
         """
         Overwrite the chunk data.
+        You must acquire the chunk lock before setting.
+        If you want to edit the chunk, use :meth:`edit` instead.
+
+        :param chunk: The chunk data to set.
+        :raises:
+            LockNotAcquired: If the chunk is already locked by another thread.
+        """
+
+    @typing.overload
+    def set_chunk(self, chunk: amulet.chunk.Chunk) -> None:
+        """
+        Overwrite the chunk data.
+        You must acquire the chunk lock before setting.
+        If you want to edit the chunk, use :meth:`edit` instead.
+
+        :param chunk: The chunk data to set.
+        :raises:
+            LockNotAcquired: If the chunk is already locked by another thread.
         """
