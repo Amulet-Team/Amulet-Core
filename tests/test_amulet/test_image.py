e24a7b779ddfa6208e5813c28280a8e254f6ce01
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-03-31 13:39:51 +0100

    Added tests

--- tests/test_amulet/test_image.py
+++ tests/test_amulet/test_image.py
@@ -1,8 +1,16 @@
 from unittest import TestCase
+import os
 
 from PIL import Image
 
-from amulet.image import get_missing_no_icon, get_missing_pack_icon, get_missing_world_icon
+from amulet.image import (
+    get_missing_no_icon,
+    get_missing_pack_icon,
+    get_missing_world_icon,
+    missing_no_icon_path,
+    missing_world_icon_path,
+    missing_pack_icon_path,
+)
 
 
 class ImageTestCase(TestCase):
@@ -24,3 +32,11 @@ class ImageTestCase(TestCase):
             self.assertIsInstance(missing_world_icon, Image.Image)
         finally:
             missing_world_icon.close()
+
+    def test_image_paths(self) -> None:
+        self.assertIsInstance(missing_no_icon_path, str)
+        self.assertIsInstance(missing_world_icon_path, str)
+        self.assertIsInstance(missing_pack_icon_path, str)
+        self.assertTrue(os.path.isfile(missing_no_icon_path))
+        self.assertTrue(os.path.isfile(missing_world_icon_path))
+        self.assertTrue(os.path.isfile(missing_pack_icon_path))
