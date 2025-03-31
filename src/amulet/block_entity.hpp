42b9598cc1bdaf0ead559d6e8c4d32043dbfbcb9
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-03-31 09:00:32 +0100

    Moved block entity implementation into cpp file

--- src/amulet/block_entity.hpp
+++ src/amulet/block_entity.hpp
@@ -19,35 +19,25 @@ private:
     std::shared_ptr<AmuletNBT::NamedTag> _nbt;
 
 public:
-    const std::string& get_namespace() const { return _namespace; }
-    void set_namespace(const std::string& namespace_) { _namespace = namespace_; }
+    AMULET_CORE_EXPORT const std::string& get_namespace() const;
+    AMULET_CORE_EXPORT void set_namespace(const std::string& namespace_);
 
-    const std::string& get_base_name() const { return _base_name; }
-    void set_base_name(const std::string& base_name) { _base_name = base_name; }
+    AMULET_CORE_EXPORT const std::string& get_base_name() const;
+    AMULET_CORE_EXPORT void set_base_name(const std::string& base_name);
 
-    std::shared_ptr<AmuletNBT::NamedTag> get_nbt() const { return _nbt; }
-    void set_nbt(std::shared_ptr<AmuletNBT::NamedTag> nbt) { _nbt = nbt; }
+    AMULET_CORE_EXPORT std::shared_ptr<AmuletNBT::NamedTag> get_nbt() const;
+    AMULET_CORE_EXPORT void set_nbt(std::shared_ptr<AmuletNBT::NamedTag> nbt);
 
-    BlockEntity(
+    AMULET_CORE_EXPORT BlockEntity(
         const PlatformType& platform,
         const VersionNumber& version,
         const std::string& namespace_,
         const std::string& base_name,
-        std::shared_ptr<AmuletNBT::NamedTag> nbt)
-        : PlatformVersionContainer(platform, version)
-        , _namespace(namespace_)
-        , _base_name(base_name)
-        , _nbt(nbt)
-    {
-    }
+        std::shared_ptr<AmuletNBT::NamedTag> nbt);
 
     AMULET_CORE_EXPORT void serialise(BinaryWriter&) const;
     AMULET_CORE_EXPORT static BlockEntity deserialise(BinaryReader&);
 
-    bool operator==(const BlockEntity& other) const
-    {
-        return (
-            PlatformVersionContainer::operator==(other) && _namespace == other._namespace && _base_name == other._base_name && AmuletNBT::NBTTag_eq(*_nbt, *other._nbt));
-    }
+    AMULET_CORE_EXPORT bool operator==(const BlockEntity& other) const;
 };
 }
