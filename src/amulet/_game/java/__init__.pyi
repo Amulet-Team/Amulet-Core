020e479755d06bba0d10adbf36624cfd386a39b8
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-04-23 09:42:55 +0100

    Remove non-core source

--- src/amulet/_game/java/__init__.pyi
+++
@@ -1,5 +0,0 @@
-from __future__ import annotations
-
-from . import block
-
-__all__ = ["block"]
--- src/amulet/resource_pack/abc/__init__.py
+++
@@ -1,2 +0,0 @@
-from .resource_pack import BaseResourcePack
-from .resource_pack_manager import BaseResourcePackManager
