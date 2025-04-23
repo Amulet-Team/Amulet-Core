e46e6badd32d954494121623060a0bdc78000b8c
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-04-23 09:39:59 +0100

    Fix editable install

--- build_requires.py
+++ build_requires.py
@@ -12,3 +12,12 @@ def get_requires_for_build_wheel(
     wheel_requirements.extend(build_meta.get_requires_for_build_wheel(config_settings))
     wheel_requirements.extend(requirements.get_compile_dependencies(config_settings))
     return wheel_requirements
+
+
+def get_requires_for_build_editable(
+    config_settings: Union[Mapping[str, Union[str, list[str], None]], None] = None,
+) -> list[str]:
+    editable_requirements = []
+    editable_requirements.extend(build_meta.get_requires_for_build_editable(config_settings))
+    editable_requirements.extend(requirements.get_compile_dependencies(config_settings))
+    return editable_requirements
