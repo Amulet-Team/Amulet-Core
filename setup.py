5d51df3536186208ff34b02b25a0db2637fc91ce
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-05-12 09:01:57 +0100

    Fix nbt and io dependencies and improve setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def fix_path(path: str) -> str:
 dependencies = requirements.get_fixed_runtime_dependencies()
 setup_args = {}
 
-def add_cpp_dependency(lib_name: str, version_str: str) -> None:
+def add_dependency(lib_name: str, version_str: str) -> None:
     version = Version(version_str)
     if version.is_prerelease:
         # Breaking API changes can be made between pre-release versions. Pin to this exact release.
@@ -32,53 +32,41 @@ def add_cpp_dependency(lib_name: str, version_str: str) -> None:
         # Fix - API unchanged. Dependents do not need to be recompiled.
         dependencies.append(f"{lib_name}~={major}.{minor}.{patch}.0")
 
-def add_default_requirements():
-    dependencies.append(
-        f"amulet-compiler-version{requirements.AMULET_COMPILER_VERSION_REQUIREMENT}"
-    )
-    dependencies.append(f"amulet_nbt{requirements.AMULET_NBT_REQUIREMENT}")
 
 if os.environ.get("AMULET_FREEZE_COMPILER", None):
     try:
         import amulet_compiler_version
-        import amulet.nbt
     except ImportError:
-        add_default_requirements()
+        dependencies.append(
+            f"amulet-compiler-version{requirements.AMULET_COMPILER_VERSION_REQUIREMENT}"
+        )
     else:
         dependencies.append(
             f"amulet-compiler-version=={amulet_compiler_version.__version__}"
         )
-
-        def add_dependency(lib_name: str, version_str: str) -> None:
-            version = Version(version_str)
-            if version.is_prerelease:
-                # Breaking ABI changes can be made between pre-release versions.
-                # Pin to this exact release.
-                dependencies.append(f"{lib_name}=={version_str}")
-            else:
-                # Breaking ABI changes can be made in major and minor changes.
-                # Require the same major and minor version.
-                match = re.fullmatch(
-                    r"(?P<major>\d+)(\.(?P<minor>\d+)(\.(?P<patch>\d+))?)?", version_str
-                )
-                if match is None:
-                    raise RuntimeError(
-                        f"Unsupported version number {lib_name}=={version_str}"
-                    )
-                major = match.group("major")
-                minor = match.group("minor") or 0
-                patch = match.group("patch") or 0
-                dependencies.append(f"{lib_name}~={major}.{minor}.{patch}")
-
-        add_dependency("amulet_nbt", amulet.nbt.__version__)
-
         setup_args["options"] = {
             "bdist_wheel": {
                 "build_number": f"1.{amulet_compiler_version.compiler_id}.{amulet_compiler_version.compiler_version}"
             }
         }
 else:
-    add_default_requirements()
+    dependencies.append(
+        f"amulet-compiler-version{requirements.AMULET_COMPILER_VERSION_REQUIREMENT}"
+    )
+
+try:
+    import amulet.io
+except ImportError:
+    dependencies.append(f"amulet_io{requirements.AMULET_IO_REQUIREMENT}")
+else:
+    add_dependency("amulet_io", amulet.io.__version__)
+
+try:
+    import amulet.nbt
+except ImportError:
+    dependencies.append(f"amulet_nbt{requirements.AMULET_NBT_REQUIREMENT}")
+else:
+    add_dependency("amulet_nbt", amulet.nbt.__version__)
 
 
 cmdclass: dict[str, type[Command]] = versioneer.get_cmdclass()
