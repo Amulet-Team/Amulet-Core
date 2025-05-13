621e5447a6b64e2735ba204f080dbe57d001a316
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-05-13 10:40:39 +0100

    Redesign biome 3d storage

--- src/amulet/core/chunk/component/biome_3d_component.hpp
+++ src/amulet/core/chunk/component/biome_3d_component.hpp
@@ -20,12 +20,31 @@ private:
     std::shared_ptr<SectionArrayMap> _sections;
 
 public:
-    AMULET_CORE_EXPORT Biome3DComponentData(
-        const VersionRange& version_range,
+    template <typename PaletteT, typename SectionsT>
+    Biome3DComponentData(
+        PaletteT&& palette,
+        SectionsT&& sections)
+        : _palette(std::forward<PaletteT>(palette))
+        , _sections(std::forward<SectionsT>(sections))
+    {
+    }
+
+    template <typename VersionRangeT>
+    Biome3DComponentData(
+        VersionRangeT&& version_range,
         const SectionShape& array_shape,
-        const Biome& default_biome);
-    AMULET_CORE_EXPORT std::shared_ptr<BiomePalette> get_palette();
-    AMULET_CORE_EXPORT std::shared_ptr<SectionArrayMap> get_sections();
+        const Biome& default_biome)
+        : Biome3DComponentData(
+              std::make_shared<BiomePalette>(std::forward<VersionRangeT>(version_range)),
+              std::make_shared<SectionArrayMap>(array_shape, static_cast<std::uint32_t>(0)))
+    {
+        _palette->biome_to_index(default_biome);
+    }
+
+    BiomePalette& get_palette() { return *_palette; }
+    std::shared_ptr<BiomePalette> get_palette_ptr() { return _palette; }
+    SectionArrayMap& get_sections() { return *_sections; }
+    std::shared_ptr<SectionArrayMap> get_sections_ptr() { return _sections; }
 };
 
 class Biome3DComponent {
@@ -35,11 +54,19 @@ private:
 protected:
     // Null constructor
     AMULET_CORE_EXPORT Biome3DComponent() = default;
+
     // Default constructor
+    template <typename VersionRangeT>
     AMULET_CORE_EXPORT void init(
-        const VersionRange& version_range,
+        VersionRangeT&& version_range,
         const SectionShape& array_shape,
-        const Biome& default_biome);
+        const Biome& default_biome)
+    {
+        _value = std::make_shared<Biome3DComponentData>(
+            std::forward<VersionRangeT>(version_range),
+            array_shape,
+            default_biome);
+    }
 
     // Serialise the component data
     AMULET_CORE_EXPORT std::optional<std::string> serialise() const;
