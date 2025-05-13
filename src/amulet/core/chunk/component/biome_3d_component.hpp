4849e4a98a3b281c30035cf41fefd574998ec89c
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-05-13 13:05:30 +0100

    Add Biome3DComponentData ctor from reference

--- src/amulet/core/chunk/component/biome_3d_component.hpp
+++ src/amulet/core/chunk/component/biome_3d_component.hpp
@@ -24,8 +24,22 @@ public:
     Biome3DComponentData(
         PaletteT&& palette,
         SectionsT&& sections)
-        : _palette(std::forward<PaletteT>(palette))
-        , _sections(std::forward<SectionsT>(sections))
+        : _palette(
+              [&palette] {
+                  if constexpr (std::is_same_v<std::shared_ptr<BiomePalette>, std::decay_t<PaletteT>>) {
+                      return std::forward<PaletteT>(palette);
+                  } else {
+                      return std::make_shared<BiomePalette>(palette);
+                  }
+              }())
+        , _sections(
+              [&sections] {
+                  if constexpr (std::is_same_v<std::shared_ptr<SectionArrayMap>, std::decay_t<SectionsT>>) {
+                      return std::forward<SectionsT>(sections);
+                  } else {
+                      return std::make_shared<SectionArrayMap>(sections);
+                  }
+              }())
     {
     }
 
