995b85ff7d1011d8a244a03c67417b1c0df6b7a2
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-05-27 16:27:03 +0100

    Fix build (#399)
    * Replace build tag with version suffix
    
    Pip needs to see the different versions as distinct versions which it doesn't with build tags.
    
    * Refactor requirements
    
    * Move unittests from source to composite action
    
    * Move install dependencies to composite action
    
    * Update build workflow
    
    * Fix version
    
    * Create directory
    
    * Try hyphens for action name
    
    * Remove requirements import
    
    * Move checkout back to main workflow
    
    * Fix argument name
    
    * Fix src unittests
    
    * Shorten job name
    
    * Pass GITHUB_TOKEN to composite actions
    
    * Fix secret variable

--- requirements.py
+++ requirements.py
@@ -1,46 +1,78 @@
-from typing import Union, Mapping
 import os
+import amulet_compiler_version
+from packaging.version import Version
 
-AMULET_COMPILER_VERSION_REQUIREMENT = "==1.3.0"
-# AMULET_NBT_REQUIREMENT = "~=4.0"
-AMULET_NBT_REQUIREMENT = "==4.0a20"
+AMULET_COMPILER_TARGET_REQUIREMENT = "==1.0"
+AMULET_COMPILER_VERSION_REQUIREMENT = "==3.0.0"
+
+PYBIND11_REQUIREMENT = "==2.13.6"
 AMULET_PYBIND11_EXTENSIONS_REQUIREMENT = "~=1.0"
+
 AMULET_IO_REQUIREMENT = "~=1.0"
-
-_compile_dependencies: dict[str, str] = {
-    "wheel": "",
-    "pybind11": "==2.13.6",
-    "amulet_pybind11_extensions": AMULET_PYBIND11_EXTENSIONS_REQUIREMENT,
-    "amulet_nbt": AMULET_NBT_REQUIREMENT,
-    "amulet_io": AMULET_IO_REQUIREMENT,
-}
-
-fixed_runtime_dependencies_data: dict[str, str] = {
-    "amulet-compiler-target": "==1.0",
-    "numpy": "~=2.0",
-}
+# AMULET_NBT_REQUIREMENT = "~=4.0"
+AMULET_NBT_REQUIREMENT = "==4.0a24"
+NUMPY_REQUIREMENT = "~=2.0"
 
 
-def get_compile_dependencies_data(
-    config_settings: Union[Mapping[str, Union[str, list[str], None]], None] = None,
-) -> dict[str, str]:
-    reqs = _compile_dependencies.copy()
-    if (
-        config_settings and config_settings.get("AMULET_FREEZE_COMPILER")
-    ) or os.environ.get("AMULET_FREEZE_COMPILER", None):
-        reqs["amulet-compiler-version"] = (
-            "@git+https://github.com/Amulet-Team/Amulet-Compiler-Version.git@1.0"
+def get_specifier_set(version_str: str, compiler_suffix_: str = "") -> str:
+    """
+    version_str: The PEP 440 version number of the library.
+    compiler_suffix_: Only specified if it is a compiled library and the compiler is being frozen.
+    """
+    version = Version(version_str)
+    if version.epoch != 0 or version.is_devrelease or version.is_postrelease:
+        raise RuntimeError(f"Unsupported version format. {version_str}")
+
+    major, minor, patch, fix, *_ = version.release + (0, 0, 0, 0)
+
+    if version.is_prerelease:
+        # Pre-releases can make breaking changes. Pin to this exact release.
+        if compiler_suffix_:
+            return f"=={major}.{minor}.{patch}.{fix}{compiler_suffix_}{''.join(map(str, version.pre))}"
+        else:
+            return f"=={version_str}"
+    else:
+        # Require an ABI compatible build.
+        return f"~={major}.{minor}.{patch}.{fix}"
+
+
+if os.environ.get("AMULET_FREEZE_COMPILER", None):
+    AMULET_COMPILER_VERSION_REQUIREMENT = f"=={amulet_compiler_version.__version__}"
+
+    compiler_suffix = f".{'.'.join(amulet_compiler_version.__version__.split('.')[3:])}"
+
+    try:
+        import amulet.io
+    except ImportError:
+        pass
+    else:
+        AMULET_IO_REQUIREMENT = get_specifier_set(amulet.io.__version__)
+
+    try:
+        import amulet.nbt
+    except ImportError:
+        pass
+    else:
+        AMULET_NBT_REQUIREMENT = get_specifier_set(
+            amulet.nbt.__version__, compiler_suffix
         )
-    return reqs
 
 
-def get_compile_dependencies(
-    config_settings: Union[Mapping[str, Union[str, list[str], None]], None] = None,
-) -> list:
+def get_build_dependencies() -> list:
     return [
-        f"{k}{v}" for k, v in get_compile_dependencies_data(config_settings).items()
+        f"pybind11{PYBIND11_REQUIREMENT}",
+        f"amulet_pybind11_extensions{AMULET_PYBIND11_EXTENSIONS_REQUIREMENT}",
+        f"amulet_nbt{AMULET_NBT_REQUIREMENT}",
+        f"amulet_io{AMULET_IO_REQUIREMENT}",
+        f"amulet-compiler-version{AMULET_COMPILER_VERSION_REQUIREMENT}",
     ]
 
 
-def get_fixed_runtime_dependencies() -> list[str]:
-    return [f"{k}{v}" for k, v in fixed_runtime_dependencies_data.items()]
+def get_runtime_dependencies() -> list[str]:
+    return [
+        f"amulet-compiler-target{AMULET_COMPILER_TARGET_REQUIREMENT}",
+        f"numpy{NUMPY_REQUIREMENT}",
+        f"amulet-compiler-version{AMULET_COMPILER_VERSION_REQUIREMENT}",
+        f"amulet-io{AMULET_IO_REQUIREMENT}",
+        f"amulet-nbt{AMULET_NBT_REQUIREMENT}",
+    ]
--- setup.py
+++ setup.py
@@ -2,83 +2,23 @@ import os
 import subprocess
 import sys
 from pathlib import Path
-import requirements
 
 from setuptools import setup, Extension, Command
 from setuptools.command.build_ext import build_ext
+
 from packaging.version import Version
 
 import versioneer
+
+import requirements
+import amulet_compiler_version
 
 
 def fix_path(path: str) -> str:
     return os.path.realpath(path).replace(os.sep, "/")
 
 
-dependencies = requirements.get_fixed_runtime_dependencies()
-setup_args = {}
-
-
-def add_dependency(lib_name: str, version_str: str) -> None:
-    version = Version(version_str)
-    if version.is_prerelease:
-        # Breaking API changes can be made between pre-release versions. Pin to this exact release.
-        dependencies.append(f"{lib_name}=={version_str}")
-    else:
-        # Major - breaking API change. Dependents must be updated and recompiled.
-        major = version.major
-        # Minor - backwards compatible API change. Dependents must be recompiled.
-        minor = version.minor
-        # Patch - API unchanged. Dependents must be recompiled.
-        patch = version.micro
-        # Fix - API unchanged. Dependents do not need to be recompiled.
-        dependencies.append(f"{lib_name}~={major}.{minor}.{patch}.0")
-
-
-try:
-    import amulet_compiler_version
-except ImportError:
-    dependencies.append(
-        f"amulet-compiler-version{requirements.AMULET_COMPILER_VERSION_REQUIREMENT}"
-    )
-else:
-    if os.environ.get("AMULET_FREEZE_COMPILER", None):
-        dependencies.append(
-            f"amulet-compiler-version=={amulet_compiler_version.__version__}"
-        )
-        setup_args["options"] = {
-            "bdist_wheel": {
-                "build_number": f"1.{amulet_compiler_version.compiler_id}.{amulet_compiler_version.compiler_version}"
-            }
-        }
-    else:
-        dependencies.append(
-            f"amulet-compiler-version{requirements.AMULET_COMPILER_VERSION_REQUIREMENT}"
-        )
-
-try:
-    import amulet.pybind11_extensions
-except ImportError:
-    dependencies.append(
-        f"amulet_pybind11_extensions{requirements.AMULET_PYBIND11_EXTENSIONS_REQUIREMENT}"
-    )
-else:
-    add_dependency("amulet_pybind11_extensions", amulet.pybind11_extensions.__version__)
-
-try:
-    import amulet.io
-except ImportError:
-    dependencies.append(f"amulet_io{requirements.AMULET_IO_REQUIREMENT}")
-else:
-    add_dependency("amulet_io", amulet.io.__version__)
-
-try:
-    import amulet.nbt
-except ImportError:
-    dependencies.append(f"amulet_nbt{requirements.AMULET_NBT_REQUIREMENT}")
-else:
-    add_dependency("amulet_nbt", amulet.nbt.__version__)
-
+dependencies = requirements.get_runtime_dependencies()
 
 cmdclass: dict[str, type[Command]] = versioneer.get_cmdclass()
 
@@ -138,10 +78,31 @@ class CMakeBuild(cmdclass.get("build_ext", build_ext)):
 cmdclass["build_ext"] = CMakeBuild
 
 
+def _get_version() -> str:
+    version_str: str = versioneer.get_version()
+
+    if os.environ.get("AMULET_FREEZE_COMPILER", None):
+        # Add the compiler version to the library version so that pip sees it as a distinct version.
+        compiler_version_str = ".".join(
+            amulet_compiler_version.__version__.split(".")[3:]
+        )
+        if compiler_version_str:
+            version = Version(version_str)
+            if version.epoch != 0 or version.is_devrelease or version.is_postrelease:
+                raise RuntimeError(f"Unsupported version format. {version_str}")
+            major, minor, patch, fix, *_ = version.release + (0, 0, 0, 0)
+            pre = "".join(map(str, version.pre)) if version.is_prerelease else ""
+            local = f"+{version.local}" if version.local else ""
+            version_str = (
+                f"{major}.{minor}.{patch}.{fix}.{compiler_version_str}{pre}{local}"
+            )
+
+    return version_str
+
+
 setup(
-    version=versioneer.get_version(),
+    version=_get_version(),
     cmdclass=cmdclass,
     ext_modules=[Extension("amulet.core._amulet_core", [])],
     install_requires=dependencies,
-    **setup_args,
 )
