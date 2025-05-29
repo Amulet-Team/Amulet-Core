5a4364bf92e8e1d1a3ccb77de14d813c5bb40acc
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-05-29 09:42:08 +0100

    Switch to timestamp post release (#403)
    * Move action step
    
    * Remove unused import
    
    * Switch to post release
    
    * Revert "Move action step"
    
    This reverts commit cd1ddc59f207fe4b1f1a61c29f2b214abad5a8f6.
    
    * Bump nbt requirement
    
    * Cache timestamp
    
    The timestamp can change between executions of setup.py leading to errors.
    Cache the timestamp in a file so the same value is used.

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ import subprocess
 import sys
 from pathlib import Path
 import platform
+import datetime
 
 from setuptools import setup, Extension, Command
 from setuptools.command.build_ext import build_ext
@@ -12,7 +13,6 @@ from packaging.version import Version
 import versioneer
 
 import requirements
-import amulet_compiler_version
 
 
 if (
@@ -94,20 +94,27 @@ def _get_version() -> str:
     version_str: str = versioneer.get_version()
 
     if os.environ.get("AMULET_FREEZE_COMPILER", None):
-        # Add the compiler version to the library version so that pip sees it as a distinct version.
-        compiler_version_str = ".".join(
-            amulet_compiler_version.__version__.split(".")[3:]
-        )
-        if compiler_version_str:
-            version = Version(version_str)
-            if version.epoch != 0 or version.is_devrelease or version.is_postrelease:
-                raise RuntimeError(f"Unsupported version format. {version_str}")
-            major, minor, patch, fix, *_ = version.release + (0, 0, 0, 0)
-            pre = "".join(map(str, version.pre)) if version.is_prerelease else ""
-            local = f"+{version.local}" if version.local else ""
-            version_str = (
-                f"{major}.{minor}.{patch}.{fix}.{compiler_version_str}{pre}{local}"
-            )
+        date_format = "%y%m%d%H%M%S"
+        try:
+            with open("build/timestamp.txt", "r") as f:
+                timestamp = datetime.datetime.strptime(f.read(), date_format)
+            print("using cached timestamp")
+        except Exception:
+            timestamp = datetime.datetime(1, 1, 1)
+        if datetime.timedelta(minutes=10) < datetime.datetime.now() - timestamp:
+            print("get timestamp")
+            timestamp = datetime.datetime.now()
+            os.makedirs("build", exist_ok=True)
+            with open("build/timestamp.txt", "w") as f:
+                f.write(timestamp.strftime(date_format))
+
+        version = Version(version_str)
+        epoch = f"{version.epoch}!" if version.epoch else ""
+        release = ".".join(map(str, version.release))
+        pre = "".join(map(str, version.pre)) if version.is_prerelease else ""
+        post = f".post{timestamp.strftime(date_format)}"
+        local = f"+{version.local}" if version.local else ""
+        version_str = f"{epoch}{release}{pre}{post}{local}"
 
     return version_str
 
