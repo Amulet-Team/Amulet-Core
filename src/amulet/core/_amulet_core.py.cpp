24b178fe57892e8f216a00f4da57f365c0a4c22a
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-06-27 15:58:36 +0100

    Update build tools
    Require pre-compiled libraries when installing for tests.
    Other fixes

--- src/amulet/core/_amulet_core.py.cpp
+++ src/amulet/core/_amulet_core.py.cpp
@@ -1,7 +1,6 @@
 #include <pybind11/pybind11.h>
 
 #include <amulet/pybind11_extensions/compatibility.hpp>
-#include <amulet/pybind11_extensions/py_module.hpp>
 
 namespace py = pybind11;
 namespace pyext = Amulet::pybind11_extensions;
@@ -18,6 +17,7 @@ void init_chunk(py::module);
 void init_module(py::module m)
 {
     pyext::init_compiler_config(m);
+    pyext::check_compatibility(py::module::import("amulet.zlib"), m);
     pyext::check_compatibility(py::module::import("amulet.nbt"), m);
 
     // Submodules
