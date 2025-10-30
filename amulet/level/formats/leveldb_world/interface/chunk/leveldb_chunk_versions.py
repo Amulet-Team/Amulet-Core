00b6e9914167b87b0434f215f89e2e958580eaf9
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-10-30 09:35:19 +0000

    Add Bedrock v42 interface

--- amulet/level/formats/leveldb_world/interface/chunk/leveldb_chunk_versions.py
+++ amulet/level/formats/leveldb_world/interface/chunk/leveldb_chunk_versions.py
@@ -50,7 +50,8 @@ chunk_version_to_max_version: Dict[
     # continue without experimental gameplay
     39: ((1, 18, 0, 0), (1, 18, 29, 999)),
     40: ((1, 18, 30, 0), (1, 21, 31, 999)),
-    41: ((1, 21, 40, 0), (999, 999, 999, 999)),
+    41: ((1, 21, 40, 0), (1, 21, 139, 999)),
+    42: ((1, 21, 120, 0), (999, 999, 999, 999)),
 }  # TODO: fill this list with the actual last game version number each chunk version was last used in
 
 
