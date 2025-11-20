3d03243852147d494a021d416c9e739de07a3bec
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-11-20 14:59:19 +0000

    Improve module constructor

--- src/amulet/core/_amulet_core.py.cpp
+++ src/amulet/core/_amulet_core.py.cpp
@@ -34,8 +34,5 @@ void init_module(py::module m)
 
 PYBIND11_MODULE(_amulet_core, m)
 {
-    py::options options;
-    options.disable_function_signatures();
-    m.def("init", &init_module, py::doc("init(arg0: types.ModuleType) -> None"));
-    options.enable_function_signatures();
+    m.def("init", &init_module, py::arg("m"));
 }
--- tests/test_amulet_core/_test_amulet_core.py.cpp
+++ tests/test_amulet_core/_test_amulet_core.py.cpp
@@ -31,8 +31,5 @@ void init_module(py::module m){
 }
 
 PYBIND11_MODULE(_test_amulet_core, m) {
-    py::options options;
-    options.disable_function_signatures();
-    m.def("init", &init_module, py::doc("init(arg0: types.ModuleType) -> None"));
-    options.enable_function_signatures();
+    m.def("init", &init_module, py::arg("m"));
 }
