de89a896fd4da2722bfb2dcc503c8499c4ae06d1
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2026-04-21 11:44:51 +0100

    Switch to cmake find python

--- tools/compile_tests.py
+++ tools/compile_tests.py
@@ -39,7 +39,7 @@ def main() -> None:
         [
             "cmake",
             *platform_args,
-            f"-DPYTHON_EXECUTABLE={sys.executable}",
+            f"-DPython3_EXECUTABLE={fix_path(sys.executable)}",
             f"-Dpybind11_DIR={fix_path(pybind11.get_cmake_dir())}",
             f"-Damulet_pybind11_extensions_DIR={fix_path(amulet.pybind11_extensions.__path__[0])}",
             f"-Damulet_io_DIR={fix_path(amulet.io.__path__[0])}",
