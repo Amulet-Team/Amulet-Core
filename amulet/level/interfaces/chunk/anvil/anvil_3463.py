077395a1b434c697f9fc5054e06dfc060b035934
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2026-09-18 14:47:31 +0100

    Bump Java format version

--- amulet/level/interfaces/chunk/anvil/anvil_3463.py
+++ amulet/level/interfaces/chunk/anvil/anvil_3463.py
@@ -13,7 +13,7 @@ class Anvil3463Interface(ParentInterface):
 
     @staticmethod
     def minor_is_valid(key: int):
-        return 3454 <= key <= 4950
+        return 3454 <= key <= 5100
 
 
 export = Anvil3463Interface
