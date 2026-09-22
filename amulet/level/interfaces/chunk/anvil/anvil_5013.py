72fb4770a4fdf87ee4b8d047fba747519eeb9038
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2026-09-22 11:59:12 +0100

    Register new status format in interface 5013

--- amulet/level/interfaces/chunk/anvil/anvil_5013.py
+++ amulet/level/interfaces/chunk/anvil/anvil_5013.py
@@ -1,11 +1,17 @@
 from __future__ import annotations
 
+from amulet.api.chunk import StatusFormats
+
 from .anvil_5006 import (
     Anvil5006Interface as ParentInterface,
 )
 
 
 class Anvil5013Interface(ParentInterface):
+    def __init__(self):
+        super().__init__()
+        self._set_feature("status", StatusFormats.Java_5013)
+
     @staticmethod
     def minor_is_valid(key: int):
         return 5013 <= key <= 5100
